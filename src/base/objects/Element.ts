13db95d66929c0629cf46367976b5001bb9ec067
Kornelius Sendy <kornelius.sendy@esb.co.id>
2024-07-20 22:41:29 +0700

    [ADD] element class

---
+++ src/base/objects/Element.ts
@@ -0,0 +1,27 @@
+export default class Element {
+    selector?: string;
+    value?: string;
+    type: ElementType;
+
+    private constructor(selector: string, value: string, type: ElementType) {
+        this.selector = selector;
+        this.value = value;
+        this.type = type;
+    }
+
+    public static of(selector: string, value: string): Element {
+        return new Element(selector, value, ElementType.KEY_VALUE);
+    }
+
+    static ofSelector(selector: string): Element {
+        return new Element(selector, null, ElementType.ELEMENT)
+    }
+
+    static ofText(text: string): Element {
+        return new Element(null, text, ElementType.TEXT);
+    }
+}
+
+enum ElementType {
+    TEXT, ELEMENT, KEY_VALUE
+}
