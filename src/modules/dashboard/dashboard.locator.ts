e792525da3a466345494b1e21ce4745b2aa45cfb
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-07-26 14:29:53 +0700

    [ADD] Add Change User name changes
    + Add locator for performLoginSubs test case
    + Add profile.locator.ts
    + Add profile.page.ts
    + Add profile.scenario.ts
    + Add profile.spec.ts
    + Add test data for performLoginSubs in .evn
    + Add profile url

--- src/modules/dashboard/dashboard.locator.ts
+++ src/modules/dashboard/dashboard.locator.ts
@@ -3,5 +3,8 @@ import BaseLocator from "../../base/base-locator";
 export default class DashboardLocator extends BaseLocator {
     // Locator for the "Later" button on the dashboard
     static buttonLater: string = "button[class='button-later']";
-
+    static buttonLater2: string = "//button[contains(@class, 'esb-btn-md-outline-secondary')]";
+    static dropdownProfile: string = "//div[@nztrigger='click']"; //request dev for better ID
+    static buttonProfile: string = "//li[@tabindex='0']"; //request dev for better ID
+    static buttonLogout: string = "//li[.//img[@src='assets/img/icons/ic-logout.svg']]"; //request dev for better ID
 }
---
+++ src/modules/profile/profile.page.ts
@@ -0,0 +1,32 @@
+import BasePage from "../../base/base-page";
+import Urls from "../../configs/urls";
+import Element from "../../base/objects/Element";
+import ProfileScenario from "./profile.scenario";
+import DashboardLocator from "../dashboard/dashboard.locator";
+import ProfileLocator from "./profile.locator";
+
+
+export default class ProfilePage extends BasePage implements ProfileScenario {
+    private testname = "test QA1";
+
+    pageUrl = (): string => Urls.profile;
+
+    shouldHave(): Element[] {
+        return [];
+    }
+
+    async changeUsername(): Promise<void> {
+        await this.expectVisible(DashboardLocator.dropdownProfile);
+        await this.click(DashboardLocator.dropdownProfile);
+        await this.expectTextVisible("Profil");
+        await this.click(DashboardLocator.buttonProfile);
+        await this.waitForUrl(`${process.env.BASE_URL}${Urls.profile}`);
+        await this.expectTextVisible("Informasi Pengguna");
+        await this.click(ProfileLocator.editUsername);
+        await this.clear(ProfileLocator.namaUser);
+        await this.fill(ProfileLocator.namaUser, this.testname);
+        await this.click(ProfileLocator.saveButton);
+    }
+
+
+}
