0749f4c1f1fe2513959decdb0a6be37fe24ed462
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-07-29 02:06:22 +0700

    [ADD] Add pengaturan cabang side bar locator and url

--- src/configs/urls.ts
+++ src/configs/urls.ts
@@ -6,7 +6,8 @@ export default class Urls {
     static profile: string = "/esb-core-lite/profile";
     static menu: string = "/esb-core-lite/catalog/menu/index";
     static singlemenu: string = "/esb-core-lite/catalog/menu/create";
-    static printer: string ="/esb-core-lite/printer-setting/station/index";
+    static printer: string = "/esb-core-lite/printer-setting/station/index";
     static salessum: string = "/esb-core-lite/report/sales-summary/index";
+    static accbranch: string = "https://dev7.esb.co.id/esb-core-lite/account-setting/branch/index"
 
 }
