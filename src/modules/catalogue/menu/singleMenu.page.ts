9b3698c7b714c05939293e22fc335e650551ea60
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-07-29 15:54:55 +0700

    [MOD] refactor singlemenu to singleMenu

--- src/modules/catalogue/menu/singleMenu.page.ts
+++ src/modules/catalogue/menu/singleMenu.page.ts
@@ -3,10 +3,10 @@ import Urls from "../../../configs/urls";
 import Element from "../../../base/objects/Element";
 import MenuScenario from "./menu.scenario";
 import MenuLocator from "./menu.locator";
-import SinglemenuScenario from "./singlemenu.scenario";
+import SingleMenuScenario from "./singleMenu.scenario";
 
 
-export default class SinglemenuPage extends BasePage implements SinglemenuScenario {
+export default class SingleMenuPage extends BasePage implements SingleMenuScenario {
 
 
     pageUrl = (): string => Urls.singlemenu;
