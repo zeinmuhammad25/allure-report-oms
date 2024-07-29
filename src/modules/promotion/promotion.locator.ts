be1fa3563af4fd0ba63462819629229f9580e465
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-07-29 13:49:24 +0700

    [MOD] Updated variable names in PromotionLocator class to follow camel case convention for consistency and readability

--- src/modules/promotion/promotion.locator.ts
+++ src/modules/promotion/promotion.locator.ts
@@ -1,21 +1,21 @@
 import BaseLocator from "../../base/base-locator";
 
 export default class PromotionLocator extends BaseLocator {
-    static discnotepercent: string = "(//span[@class='ant-radio-button ant-radio-button-checked'])[1]";
-    static discnoterupiah: string = "(//span[@class='ant-radio-button'])[1]";
-    static discmenurupiah: string = "(//span[@class='ant-radio-button'])[2]";
-    static disccustompercent: string = "(//span[@class='ant-radio-button'])[3]";
-    static discsavebutton: string = "(//button[normalize-space()='Lanjut'])[1]";
-    static disccancelbutton: string = "(//button[normalize-space()='Batal'])[1]";
+    static discPercent: string = "(//span[@class='ant-radio-button ant-radio-button-checked'])[1]";
+    static discNoteRupiah: string = "(//span[@class='ant-radio-button'])[1]";
+    static discMenuRupiah: string = "(//span[@class='ant-radio-button'])[2]";
+    static discCustomPercent: string = "(//span[@class='ant-radio-button'])[3]";
+    static discSaveButton: string = "(//button[normalize-space()='Lanjut'])[1]";
+    static discCancelButton: string = "(//button[normalize-space()='Batal'])[1]";
 
     //Detail Promo
-    static discnamefield: string = "(//input[@id='notes'])[1]";
-    static discdiscfield: string = "(//input[@id='discount'])[1]";
-    static discbranchfield: string = "(//input[@id='discount'])[1]";
-    static discpercentfield: string = "(//input[@id='discount'])[1]";
-    static mintransfield: string = "(//input[@id='minSalesPrice'])[1]";
-    static maxdiscfield: string = "(//input[@id='maxSalesPrice'])[1]";
-    static paymentmethodfield: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[2]";
+    static discNameField: string = "(//input[@id='notes'])[1]";
+    static discDiscField: string = "(//input[@id='discount'])[1]";
+    static discBranchField: string = "(//input[@id='discount'])[1]";
+    static discPercentField: string = "(//input[@id='discount'])[1]";
+    static minTransField: string = "(//input[@id='minSalesPrice'])[1]";
+    static maxDiscField: string = "(//input[@id='maxSalesPrice'])[1]";
+    static paymentMethodField: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[2]";
 
 
 }
--- src/modules/promotion/promotion.page.ts
+++ src/modules/promotion/promotion.page.ts
@@ -10,12 +10,19 @@ export default class PromotionPage extends BasePage implements PromotionScenario
     shouldHave(): Element[] {
         return [
             Element.ofText("Tipe Promosi"),
-            Element.ofSelector(PromotionLocator.discnotepercent),
-            Element.ofSelector(PromotionLocator.discnoterupiah),
-            Element.ofSelector(PromotionLocator.discmenurupiah),
-            Element.ofSelector(PromotionLocator.disccustompercent),
-            Element.ofSelector(PromotionLocator.discsavebutton),
-            Element.ofSelector(PromotionLocator.disccancelbutton),
+            Element.ofSelector(PromotionLocator.discPercent),
+            Element.ofSelector(PromotionLocator.discNoteRupiah),
+            Element.ofSelector(PromotionLocator.discMenuRupiah),
+            Element.ofSelector(PromotionLocator.discCustomPercent),
+            Element.ofSelector(PromotionLocator.discSaveButton),
+            Element.ofSelector(PromotionLocator.discCancelButton),
+            Element.ofSelector(PromotionLocator.discNameField),
+            Element.ofSelector(PromotionLocator.discDiscField),
+            Element.ofSelector(PromotionLocator.discBranchField),
+            Element.ofSelector(PromotionLocator.discPercentField),
+            Element.ofSelector(PromotionLocator.minTransField),
+            Element.ofSelector(PromotionLocator.maxDiscField),
+            Element.ofSelector(PromotionLocator.paymentMethodField),
 
         ];
     }
