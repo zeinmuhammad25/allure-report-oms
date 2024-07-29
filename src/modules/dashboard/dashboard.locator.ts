8db338aba5986f8433b8fb343dddc11e53d7570a
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-07-29 16:32:10 +0700

    [MOD] refactor dashboard.locator.ts

--- src/modules/dashboard/dashboard.locator.ts
+++ src/modules/dashboard/dashboard.locator.ts
@@ -7,16 +7,15 @@ export default class DashboardLocator extends BaseLocator {
     static dropdownProfile: string = "//div[@nztrigger='click']"; //request dev for better ID
     static buttonProfile: string = "//li[@tabindex='0']"; //request dev for better ID
     static buttonLogout: string = "//li[.//img[@src='assets/img/icons/ic-logout.svg']]"; //request dev for better ID
-    static buttonsubstatus: string = "(//li[@id='mnu-langganan-sekarang'])[1]"; //request dev for better ID for Status Langganan
-    static buttongrouplist: string = "(//li[@class='d-flex align-items-center dropdown-account-item ant-dropdown-menu-item'])[2]"; //request dev for better ID for Daftar Grup
-    static buttonpromocode: string = "(//li[@class='d-flex align-items-center dropdown-account-item ant-dropdown-menu-item'])[4]";//request dev for better ID for Kode Promo
-    static findcompanyfield: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[1]";
-    static findbrandfield: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[2]";
-    static findbranchfield: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[3]";
-    static findbutton: string = "(//button[normalize-space()='Cari'])[1]";
-    static daybuttonchecked: string = "(//span[@class='ant-radio-button ant-radio-button-checked'])[1]";
-    static monthbuttonunchecked: string = "(//label[@class='ant-radio-button-wrapper'])[1]";
-
+    static subscriptionStatusButton: string = "(//li[@id='mnu-langganan-sekarang'])[1]"; //request dev for better ID for Status Langganan
+    static groupListButton: string = "(//li[@class='d-flex align-items-center dropdown-account-item ant-dropdown-menu-item'])[2]"; //request dev for better ID for Daftar Grup
+    static promoCodeButton: string = "(//li[@class='d-flex align-items-center dropdown-account-item ant-dropdown-menu-item'])[4]";//request dev for better ID for Kode Promo
+    static findCompanyField: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[1]";
+    static findBrandField: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[2]";
+    static findBranchField: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[3]";
+    static findButton: string = "(//button[normalize-space()='Cari'])[1]";
+    static dayButtonChecked: string = "(//span[@class='ant-radio-button ant-radio-button-checked'])[1]";
+    static monthButtonChecked: string = "(//label[@class='ant-radio-button-wrapper'])[1]";
 
 
 }
