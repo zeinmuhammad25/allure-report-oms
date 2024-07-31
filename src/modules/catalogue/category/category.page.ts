c4a972561bb9c3576bc6814a43596297802fddca
Muhammad Anugerah Basri <muhammad.basri@esb.co.id>
2024-07-31 21:01:18 +0700

    [MOD] Reformat Files Category Page

--- src/modules/catalogue/category/category.page.ts
+++ src/modules/catalogue/category/category.page.ts
@@ -13,15 +13,13 @@ export default class CategoryPage extends BasePage implements CategoryScenario {
     // Real URL = https://dev7.esb.co.id/esb-core-lite/catalog/menu-category/index
     shouldHave(): Element[] {
         return [
-
             Element.ofSelector(CategoryLocator.categoryTab),
             Element.ofSelector(CategoryLocator.subCategoryTab),
             Element.ofSelector(CategoryLocator.categoryNameColumn),
             Element.ofSelector(CategoryLocator.statusNameColumn),
             Element.ofSelector(CategoryLocator.categoryNameSearch),
             Element.ofSelector(CategoryLocator.statusSearch),
-            Element.ofSelector(CategoryLocator.addCategoryButton),
-
+            Element.ofSelector(CategoryLocator.addCategoryButton)
         ];
     }
 
