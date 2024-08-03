24c6da0451605fbf35e45fbda43e0e85bfc67af7
Muhammad Anugerah Basri <muhammad.basri@esb.co.id>
2024-08-03 13:49:51 +0700

    [ADD] Add Sub Module bookkeepingCategory Page

---
+++ src/modules/bookkeeping/bookkeepingCategory/bookkeepingCategory.page.ts
@@ -0,0 +1,28 @@
+import BasePage from "../../../base/base-page";
+import Urls from "../../../configs/urls";
+import Element from "../../../base/objects/Element";
+import BookkeepingCategoryLocator from "./bookkeepingCategory.locator"
+import BookkeepingCategoryScenario from "./bookkeepingCategory.scenario"
+
+
+export default class CategoryPage extends BasePage implements BookkeepingCategoryScenario {
+
+
+    pageUrl = (): string => Urls.menu;
+
+    // Real URL = https://dev7.esb.co.id/esb-core-lite/finance/charts-of-account/index
+    shouldHave(): Element[] {
+        return [
+            Element.ofSelector(BookkeepingCategoryLocator.guideButton),
+            Element.ofSelector(BookkeepingCategoryLocator.addBookkeepingCategoryButton),
+            Element.ofSelector(BookkeepingCategoryLocator.archiveBookkeepingCategoryButton),
+            Element.ofSelector(BookkeepingCategoryLocator.bookkeepingCategorySearch),
+            Element.ofSelector(BookkeepingCategoryLocator.bookkeepingTypeSearch),
+            Element.ofSelector(BookkeepingCategoryLocator.numberColumn),
+            Element.ofSelector(BookkeepingCategoryLocator.bookkeepingNameColumn),
+            Element.ofSelector(BookkeepingCategoryLocator.bookkeepingTypeColumn),
+        ];
+    }
+
+
+}
