48ae0f32ff23fd5c2a5d563ca34c92818a3c929f
Muhammad Anugerah Basri <muhammad.basri@esb.co.id>
2024-08-10 00:53:04 +0700

    [ADD] Add Sub Module bannerMarketing Page

---
+++ src/modules/esbOrder/bannerMarketing/bannerMarketing.page.ts
@@ -0,0 +1,33 @@
+import BasePage from "../../../base/base-page";
+import Urls from "../../../configs/urls";
+import Element from "../../../base/objects/Element";
+import BannerMarketingScenario from "./bannerMarketing.scenario";
+import BannerMarketingLocator from "./bannerMarketing.locator";
+
+
+export default class BannerMarketingPage extends BasePage implements BannerMarketingScenario {
+
+
+    pageUrl = (): string => Urls.menu;
+
+    // Real URL = https://dev7.esb.co.id/esb-core-lite/esb-order/banner/index
+    shouldHave(): Element[] {
+        return [
+            Element.ofSelector(BannerMarketingLocator.bannerMarketingTab),
+            Element.ofSelector(BannerMarketingLocator.bannerNameSearch),
+            Element.ofSelector(BannerMarketingLocator.branchSearch),
+            Element.ofSelector(BannerMarketingLocator.startDateSearch),
+            Element.ofSelector(BannerMarketingLocator.endDateSearch),
+            Element.ofSelector(BannerMarketingLocator.statusSearch),
+            Element.ofSelector(BannerMarketingLocator.bannerNameColumn),
+            Element.ofSelector(BannerMarketingLocator.branchColumn),
+            Element.ofSelector(BannerMarketingLocator.startDateColumn),
+            Element.ofSelector(BannerMarketingLocator.endDateColumn),
+            Element.ofSelector(BannerMarketingLocator.statusNameColumn),
+            Element.ofSelector(BannerMarketingLocator.addBannerMarketingButton),
+
+        ];
+    }
+
+
+}
