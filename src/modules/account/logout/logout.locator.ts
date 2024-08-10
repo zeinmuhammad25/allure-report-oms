14d50549b52edca1032a9fb8bdc0cd1346892864
Muhammad Anugerah Basri <muhammad.basri@esb.co.id>
2024-08-10 17:48:19 +0700

    [ADD] Add Sub Module logout Locator

---
+++ src/modules/account/logout/logout.locator.ts
@@ -0,0 +1,9 @@
+import BaseLocator from "../../../base/base-locator";
+
+export default class LogoutLocator extends BaseLocator {
+    static accountDropdown: string = "//div[@nzoverlayclassname='dropdown-account']";
+    static logoutButton: string = "//span[text()='Keluar']/parent::li";
+    static logoutConfirmationPopUp: string = "//app-modal-confirm";
+    static cancelLogoutButton: string = "//button[text()=' Keluar ']";
+    static logoutConfirmationButton: string = "//button[text()=' Batal ']";
+}
