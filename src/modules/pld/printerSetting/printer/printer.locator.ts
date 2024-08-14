12b4761af883f36d27bc778ddae2b11cc8abe7e9
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-14 19:13:56 +0700

    [MOD] printer.locator.ts

--- src/modules/pld/printerSetting/printer/printer.locator.ts
+++ src/modules/pld/printerSetting/printer/printer.locator.ts
@@ -8,14 +8,19 @@ export default class PrinterLocator extends BaseLocator {
     static findPrinterField: string = "(//nz-input-group[@class='ant-input-affix-wrapper ant-input-affix-wrapper-lg'])[1]";
     static printerArchiveButton: string = "(//button[@class='button button-outline-grey button-small'])[1]";
     static addPrinterButton: string = "(//button[normalize-space()='Printer'])[1]";
+    static printerButtonSidebar: string = "//a[@ng-reflect-router-link='printer-setting/station']";
+
 
 
     // Add Printer Locator
 
     static stationNameField: string = "(//input[@id='stationName'])[1]";
+    static suggestionStationName: string = "//button[normalize-space()='Dapur']";
     static printerBranchField: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[1]";
     static printerViewDropdown: string = "(//nz-select-item[@title='Standar'])[1]";
-    static printerViewOption1: string = "(//div[normalize-space()='Per Jumlah Menu'])[1]";
+    static printerViewOption1: string = "//div[normalize-space()='Ini Cabang 6 Bulan']";
+    static printerDeleteButton: string = "//button[@class='button button-red button-x-small button-icon']";
+    static printerConfirmDeleteButton: string = "//div[@class='cdk-overlay-container']//div[@class='ant-row']//div[1]";
     static printerViewOption2: string = "(//nz-option-item[@title='Per Menu'])[1]";
     static printerView: string = "(//div[contains(text(),'Standar')])[1]";
     static printerCancelButton: string = "(//button[normalize-space()='Batal'])[1]";
