e3e65d75049ec73f0f5d81149880213e87f063e8
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-14 19:52:25 +0700

    [Add] TC-PrinterSetting to smokeTC.spec.ts

--- tests/pld/smokeTC.spec.ts
+++ tests/pld/smokeTC.spec.ts
@@ -1,5 +1,6 @@
 import {test} from "@playwright/test";
 import LoginPage from "../../src/modules/pld/login/login.page";
+import PrinterPage from "../../src/modules/pld/printerSetting/printer/printer.page";
 
 test.describe('TC-login', { tag: '@login' }, ()   => {
     test(`Verify if user can't log in with wrong email`, async ({page}) => {
@@ -21,6 +22,15 @@ test.describe('TC-login', { tag: '@login' }, ()   => {
 
 })
 
+test.describe('TC-printerSetting', { tag: '@print' }, ()   => {
+    test(`Verify if user can't log in with wrong email`, async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigateHere();
+        await loginPage.performLoginSubs();
+        const printerPage = new PrinterPage(page);
+        await printerPage.performAddPrinter();
+    })
+})
 
 
 
