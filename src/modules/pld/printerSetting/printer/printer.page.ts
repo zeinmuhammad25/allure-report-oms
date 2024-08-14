cd96869ed6d6ff1582d905ebb041cb4accadc9ed
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-14 19:14:08 +0700

    [MOD] printer.page.ts

--- src/modules/pld/printerSetting/printer/printer.page.ts
+++ src/modules/pld/printerSetting/printer/printer.page.ts
@@ -1,14 +1,16 @@
 import BasePosLitePage from "../../base-pos-lite-page";
-import Urls from "../../../../configs/urls";
 import Element from "../../../../base/objects/Element";
 import PrinterScenario from "./printer.scenario";
 import PrinterLocator from "./printer.locator";
 
 
 export default class PrinterPage extends BasePosLitePage implements PrinterScenario {
+    private stationName = "test Printer";
+    private stationNameEmpty = "";
+    private stationNameSpecialChar = "test Printer@!#";
+    private stationNameNumber = "test Printer0123";
 
-
-    pageUrl = (): string => Urls.printer;
+    pageUrl = (): string => this.urls.get.printerSetting.printer;
 
     shouldHave(): Element[] {
         return [
@@ -17,6 +19,38 @@ export default class PrinterPage extends BasePosLitePage implements PrinterScena
             Element.ofSelector(PrinterLocator.printerArchiveButton),
             Element.ofSelector(PrinterLocator.addPrinterButton)
         ];
+    }
+
+    async performUiPrinterCheck(): Promise<void> {
+        await this.expectTextVisible("Pengaturan Printer");
+        console.log("Verified text 'Pengaturan Printer' is visible");
+        await this.clickText("Pengaturan Printer");
+        await this.expectVisible(PrinterLocator.printerButtonSidebar);
+        await this.click(PrinterLocator.printerButtonSidebar);
+        await this.click(PrinterLocator.addPrinterButton);
+        await this.clear(PrinterLocator.stationNameField);
+        await this.fill(PrinterLocator.stationNameField, this.stationName);
+    }
+
+    async performAddPrinter(): Promise<void> {
+        await this.expectTextVisible("Pengaturan Printer");
+        console.log("Verified text 'Pengaturan Printer' is visible");
+        await this.clickText("Pengaturan Printer");
+        await this.expectVisible(PrinterLocator.printerButtonSidebar);
+        await this.click(PrinterLocator.printerButtonSidebar);
+        await this.click(PrinterLocator.addPrinterButton);
+        await this.click(PrinterLocator.suggestionStationName);
+        await this.clear(PrinterLocator.stationNameField);
+        await this.fill(PrinterLocator.stationNameField, this.stationName);
+        await this.click(PrinterLocator.printerBranchField);
+        await this.click(PrinterLocator.printerViewOption1);
+        await this.clickButtonByText("Simpan");
+        await this.click(PrinterLocator.printerDeleteButton);
+        await this.expectTextVisible("Ya, Hapus");
+        await this.clickButtonByText("Ya, Hapus");
+        await this.expectTextInvisible("Ya, Hapus");
+        await this.expectTextInvisible("Test Printer");
+
     }
 
 
