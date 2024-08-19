143ef39cbab7e26f6f5d73853f0e1c5b7ec41d28
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-19 19:58:37 +0700

    [MOD] printer.page.ts

--- src/modules/pld/printerSetting/printer/printer.page.ts
+++ src/modules/pld/printerSetting/printer/printer.page.ts
@@ -5,71 +5,45 @@ import PrinterLocator from "./printer.locator";
 
 
 export default class PrinterPage extends BasePosLitePage implements PrinterScenario {
-    private stationName = "Test Printer Edit";
-    private stationNameEdit = "Test Printer Edit";
-    private stationNameEmpty = "";
-    private stationNameSpecialChar = "test Printer@!#";
-    private stationNameNumber = "test Printer0123";
-
+    private stationName = "Test Printer 01";
 
     pageUrl = (): string => this.urls.get.printerSetting.printer;
 
     shouldHave(): Element[] {
         return [
-            Element.ofSelector(PrinterLocator.printerFilter),
-            Element.ofSelector(PrinterLocator.findPrinterField),
-            Element.ofSelector(PrinterLocator.printerArchiveButton),
-            Element.ofSelector(PrinterLocator.addPrinterButton),
-            Element.ofText("Pengaturan Printer")
+            Element.ofSelector(PrinterLocator.printerDeleteButton),
+            Element.ofSelector(PrinterLocator.printerSearchBar),
+            Element.ofSelector(PrinterLocator.printerSearchDropdown),
         ];
     }
 
 
-    async performAddPrinter(): Promise<void> {
-        await this.expectTextVisible("Pengaturan Printer");
-        await this.clickText("Pengaturan Printer");
-        await this.expectVisible(PrinterLocator.printerButtonSidebar);
-        await this.click(PrinterLocator.printerButtonSidebar);
-        await this.click(PrinterLocator.addPrinterButton);
-        await this.click(PrinterLocator.suggestionStationName);
-        await this.clear(PrinterLocator.stationNameField);
-        await this.fill(PrinterLocator.stationNameField, this.stationName);
-        await this.click(PrinterLocator.printerBranchField);
-        await this.click(PrinterLocator.printerViewOption1);
-        await this.clickButtonByText("Simpan");
-        await this.expectTextVisible("Data Berhasil disimpan!");
-        await this.click(PrinterLocator.printerDeleteButton);
-        await this.clickButtonByText("Ya, Hapus");
-        await this.expectTextVisible("Data berhasil dipindahkan ke");
+    async navigateToPrinter(): Promise<void> {
+        await this.expectVisible(PrinterLocator.printerSettingSidebar);
+        await this.click(PrinterLocator.printerSettingSidebar);
+        await this.click(PrinterLocator.printerSidebar);
 
+    }
+
+    async performAddPrinter(): Promise<void> {
+        await this.expectVisible(PrinterLocator.printerButtonAdd);
+        await this.click(PrinterLocator.printerButtonAdd);
 
     }
 
     async performEditPrinter(): Promise<void> {
-        await this.expectTextVisible("Pengaturan Printer");
-        await this.clickText("Pengaturan Printer");
-        await this.expectVisible(PrinterLocator.printerButtonSidebar);
-        await this.click(PrinterLocator.printerButtonSidebar);
-        await this.click(PrinterLocator.addPrinterButton);
-        await this.click(PrinterLocator.suggestionStationName);
-        await this.clear(PrinterLocator.stationNameField);
-        await this.fill(PrinterLocator.stationNameField, this.stationName);
-        await this.click(PrinterLocator.printerBranchField);
-        await this.click(PrinterLocator.printerViewOption1);
-        await this.clickButtonByText("Simpan");
-        await this.expectTextVisible("Data Berhasil disimpan!");
-        await this.click(PrinterLocator.printerEditButton);
-        await this.clear(PrinterLocator.stationNameField);
-        await this.fill(PrinterLocator.stationNameField, this.stationNameEdit);
-        await this.click(PrinterLocator.printerBranchFilled);
-        await this.click(PrinterLocator.printerViewOption2);
-        await this.click(PrinterLocator.printerSaveButton);
-        await this.expectTextVisible("Data Berhasil disimpan!");
-        await this.click(PrinterLocator.printerDeleteButton);
-        await this.clickButtonByText("Ya, Hapus");
-        await this.expectTextVisible("Data berhasil dipindahkan ke");
+        await this.expectVisible(PrinterLocator.printerEditButton);
+    }
 
+    async performDeletePrinter(): Promise<void> {
+        await this.expectVisible(PrinterLocator.printerDeleteButton);
+    }
 
+    async performSearchPrinter(): Promise<void> {
+        await this.expectTextVisible(PrinterLocator.printerSearchBar);
+        // await this.click(PrinterLocator.printerSearchBar);
+        // await this.fill(PrinterLocator.printerSearchBar, this.stationName);
+        // await this.click(PrinterLocator.printerSearchButton);
     }
 
 
