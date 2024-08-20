9808c12cec66c615fec1b7a872456438a07168e5
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-20 19:27:07 +0700

    [ADD] Added menumanagementEdit sub-modules

---
+++ src/modules/pld/printerSetting/menuManagement/menuManagementEdit/menuManagementEdit.locator.ts
@@ -0,0 +1,9 @@
+import BaseLocator from "../../../../../base/base-locator";
+
+export default class MenuManagementEditLocator extends BaseLocator {
+
+    static menuManagementRadioBtn: string = "//button[@class='ant-switch ant-switch-small']";
+    static menuManagementSaveBtn: string = "//button[@id='c-branchmenu3']";
+    static menuManagementCancelBtn: string = "//button[@class='btn btn-warning mr-2 ng-star-inserted']";
+
+}
