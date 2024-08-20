2de0c00f82eb428c840213cbbdcaf59aaa9e6a8a
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-20 10:36:58 +0700

    [MOD] Adjust locator for delete button

--- src/modules/pld/printerSetting/printer/printer.locator.ts
+++ src/modules/pld/printerSetting/printer/printer.locator.ts
@@ -12,7 +12,7 @@ export default class PrinterLocator extends BaseLocator {
     static printerButtonAdd: string = "//button[@id='i-station1']";
     static printerSearchBar: string = "//input[@placeholder='Cari Berdasarkan Printer']";
     static printerSearchDropdown: string = "//nz-select-item[@title='Nama Printer']";
-    static printerDeleteButton: string = "//button[@class='button button-red button-x-small button-icon']";
+    static printerDeleteButton: string = "(//button[img[@alt='trash']])[1]";
     static printerPopupDelete: string = "//button[@class='button button-orange button-small button-block']";
     static printerPopupCancel: string = "//button[@class='button button-outline-red button-small button-block']";
     static printerEditButton: string = "//button[@class='button button-blue button-x-small button-icon']";
