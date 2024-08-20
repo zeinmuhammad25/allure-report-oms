10ad675966b9d313b42fe439ffdebcfb5aeafe05
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-20 18:19:12 +0700

    [MOD] adjust locators

--- src/modules/pld/profile/profile.locator.ts
+++ src/modules/pld/profile/profile.locator.ts
@@ -2,12 +2,13 @@ import BaseLocator from "../../../base/base-locator";
 
 export default class ProfileLocator extends BaseLocator {
 
-    static infoProfileSelected: string = "//div[@aria-selected='true']"; //request for better locator name
-    static infoProfileUnselected: string = "//div[@aria-selected='false']"; //Request for better locator name
+    static infoProfileSelected: string = "//div[@aria-selected='true']";
+    static infoProfileUnselected: string = "//div[@aria-selected='false']";
     static infoButton: string = "//button[@class='button button-info']"
-    static editUsername: string = "//nz-input-group/span/div\n"; // request for better locator name: button Ubah in Nama User field
-    static editPhonenumber: string = ""; // request for better locator name: button Ubah in No. Telepon
-    static namaUser: string = "(//input[@id='fullName'])[2]";
+    static userNameEditButton: string = "//div[contains(@class, 'esb-caption-1') and contains(@class, 'c-pointer') and contains(@class, 'ng-star-inserted') and text()='Ubah']";
+    static userNameEditFieldPopup: string = "(//input[@id='fullName'])[2]";
+    static editPhonenumber: string = "";
+    static userNameField: string = "//input[@id='fullName']";
     static cancelButton: string = "//button[@type='button']";
     static saveButton: string = "//button[normalize-space()='Simpan']";
     static buttonBack: string = "//button[@class='button back']";
