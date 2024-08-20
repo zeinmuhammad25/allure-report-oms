f7d1cfd195fca30b47a032d12547eb23d037614c
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-20 11:06:35 +0700

    [MOD] printer.locator.ts for delete button

--- src/modules/pld/printerSetting/printer/printer.locator.ts
+++ src/modules/pld/printerSetting/printer/printer.locator.ts
@@ -12,9 +12,10 @@ export default class PrinterLocator extends BaseLocator {
     static printerButtonAdd: string = "//button[@id='i-station1']";
     static printerSearchBar: string = "//input[@placeholder='Cari Berdasarkan Printer']";
     static printerSearchDropdown: string = "//nz-select-item[@title='Nama Printer']";
-    static printerDeleteButton: string = "(//button[img[@alt='trash']])[1]";
-    static printerPopupDelete: string = "//button[@class='button button-orange button-small button-block']";
-    static printerPopupCancel: string = "//button[@class='button button-outline-red button-small button-block']";
+    static printerStationData: string = "//tr[contains(@class, 'ant-table-row')][1]//td[1]"
+    static printerDeleteButton: string = "//tr[contains(@class, 'ant-table-row')][1]//button[contains(@class, 'button-red')]";
+    static printerPopupDelete: string = "//button[@class='button button-outline-red button-small button-block']";
+    static printerPopupCancel: string = "//button[@class='button button-orange button-small button-block']";
     static printerEditButton: string = "//button[@class='button button-blue button-x-small button-icon']";
     static printerSearchButton: string = "//i[@class='anticon anticon-search ng-star-inserted']//*[name()='svg']";
 
