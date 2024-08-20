7fe1f6f398552a967dfec982ed262c72a62e6d70
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-20 18:19:41 +0700

    [MOD] adjust profile.spec.ts scenario

--- tests/pld/profileTC/profile.spec.ts
+++ tests/pld/profileTC/profile.spec.ts
@@ -2,14 +2,25 @@ import {test} from "@playwright/test";
 import LoginPage from "../../../src/modules/pld/login/login.page";
 import ProfilePage from "../../../src/modules/pld/profile/profile.page";
 
-test(`User can change user name`, async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigateHere();
-    await loginPage.performLoginSubs();
 
-    const profilePage = new ProfilePage(page);
-    await profilePage.changeUsername();
+test.describe.serial('Test Profile', () => {
+    let loginPage: LoginPage;
+
+
+    test.beforeEach(async ({page}) => {
+        loginPage = new LoginPage(page);
+        await loginPage.navigateHere();
+        await loginPage.performLoginSubs();
+    });
+
+    test(`Verify if user can change username on by click Ubah button`, async ({page}) => {
+        const profilePage = new ProfilePage(page);
+        await profilePage.userNameChange();
+    });
+
+    test(`Verify if user can change username directly on the field`, async ({page}) => {
+        const profilePage = new ProfilePage(page);
+        await profilePage.userNameChangeDirect();
+    });
+
 });
-
-
-
