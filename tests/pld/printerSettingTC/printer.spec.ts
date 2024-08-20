a82ea8e28ee56c85e27bc6b8b5ea1c32aa8b7a12
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-20 11:13:51 +0700

    [MOD] add tags in printer.spec.ts

--- tests/pld/printerSettingTC/printer.spec.ts
+++ tests/pld/printerSettingTC/printer.spec.ts
@@ -5,31 +5,39 @@ import LogoutPage from "../../../src/modules/pld/account/logout/logout.page";
 import PrinterCreatePage from "../../../src/modules/pld/printerSetting/printer/printerCreate/printerCreate.page";
 
 
-test(`Verify if user can Add Printer`,{tag: '@smokeTest'},  async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigateHere();
-    await loginPage.performLoginSubs();
-    const printerPage = new PrinterPage(page);
-    await printerPage.navigateToPrinter();
-    await printerPage.performAddPrinter();
-    const printerCreatePage = new PrinterCreatePage(page);
-    await printerCreatePage.printerFormFill();
-    const logoutPage = new LogoutPage(page);
-    await logoutPage.performLogout();
-})
-test(`Verify if user can delete Printer`, {tag: '@smokeTest'}, async ({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.navigateHere();
-    await loginPage.performLoginSubs();
-    const printerPage = new PrinterPage(page);
-    await printerPage.navigateToPrinter();
-    await printerPage.performSearchPrinter();
-    await printerPage.performDeletePrinter();
+test.describe.serial('printerTC', () => {
 
-    const logoutPage = new LogoutPage(page);
-    await logoutPage.performLogout();
+    test('Verify if user can Add Printer', {tag: '@smokeTest, @printerSetting'}, async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigateHere();
+        await loginPage.performLoginSubs();
 
-})
+        const printerPage = new PrinterPage(page);
+        await printerPage.navigateToPrinter();
+        await printerPage.performAddPrinter();
+
+        const printerCreatePage = new PrinterCreatePage(page);
+        await printerCreatePage.printerFormFill();
+
+        const logoutPage = new LogoutPage(page);
+        await logoutPage.performLogout();
+    });
+
+    test('Verify if user can delete Printer', {tag: '@smokeTest, @printerSetting'}, async ({page}) => {
+        const loginPage = new LoginPage(page);
+        await loginPage.navigateHere();
+        await loginPage.performLoginSubs();
+
+        const printerPage = new PrinterPage(page);
+        await printerPage.navigateToPrinter();
+        await printerPage.performSearchPrinter();
+        await printerPage.performDeletePrinter();
+
+        const logoutPage = new LogoutPage(page);
+        await logoutPage.performLogout();
+    });
+
+});
 
 
 
