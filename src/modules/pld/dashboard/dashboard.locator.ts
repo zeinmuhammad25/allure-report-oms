2122fcaa885eb662a60c242545ecef3e3ccb7ae5
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-22 20:08:42 +0700

    [ADD] refactor locator for easier to locate

--- src/modules/pld/dashboard/dashboard.locator.ts
+++ src/modules/pld/dashboard/dashboard.locator.ts
@@ -6,16 +6,5 @@ export default class DashboardLocator extends BaseLocator {
     static buttonLater2: string = "//button[contains(@class, 'esb-btn-md-outline-secondary')]";
     static dropdownProfile: string = "//div[@nztrigger='click']"; //request dev for better ID
     static buttonProfile: string = "//li[@tabindex='0']"; //request dev for better ID
-    static buttonLogout: string = "//li[.//img[@src='assets/img/icons/ic-logout.svg']]"; //request dev for better ID
-    static subscriptionStatusButton: string = "(//li[@id='mnu-langganan-sekarang'])[1]"; //request dev for better ID for Status Langganan
-    static groupListButton: string = "(//li[@class='d-flex align-items-center dropdown-account-item ant-dropdown-menu-item'])[2]"; //request dev for better ID for Daftar Grup
-    static promoCodeButton: string = "(//li[@class='d-flex align-items-center dropdown-account-item ant-dropdown-menu-item'])[4]";//request dev for better ID for Kode Promo
-    static findCompanyField: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[1]";
-    static findBrandField: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[2]";
-    static findBranchField: string = "(//input[@class='ant-select-selection-search-input ng-untouched ng-pristine ng-valid'])[3]";
-    static findButton: string = "(//button[normalize-space()='Cari'])[1]";
-    static dayButtonChecked: string = "(//span[@class='ant-radio-button ant-radio-button-checked'])[1]";
-    static monthButtonChecked: string = "(//label[@class='ant-radio-button-wrapper'])[1]";
-
 
 }
--- src/modules/pld/dashboard/dashboard.page.ts
+++ src/modules/pld/dashboard/dashboard.page.ts
@@ -1,7 +1,5 @@
 import Element from "../../../base/objects/Element";
-import Urls from "../../../configs/urls";
 import DashboardScenario from "./dashboard.scenario";
-import DashboardLocator from "./dashboard.locator";
 import BasePosLitePage from "../base-pos-lite-page";
 
 
@@ -9,16 +7,7 @@ export default class DashboardPage extends BasePosLitePage implements DashboardS
     pageUrl = (): string => this.urls.get.dashboard.dashboardIndex
 
     shouldHave(): Element[] {
-        return [
-            Element.ofSelector(DashboardLocator.buttonProfile),
-            Element.ofSelector(DashboardLocator.findButton),
-            Element.ofSelector(DashboardLocator.findCompanyField),
-            Element.ofSelector(DashboardLocator.findBrandField),
-            Element.ofSelector(DashboardLocator.findBranchField),
-            Element.ofSelector(DashboardLocator.dayButtonChecked),
-            Element.ofSelector(DashboardLocator.monthButtonChecked),
-
-        ];
+        return [];
     }
 
 
