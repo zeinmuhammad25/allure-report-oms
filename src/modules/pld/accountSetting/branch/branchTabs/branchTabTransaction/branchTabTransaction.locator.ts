d8cbfea22fabb3b827c140236d449e46c672ecbb
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-22 16:12:51 +0700

    [MOD] group locator based on type

--- src/modules/pld/accountSetting/branch/branchTabs/branchTabTransaction/branchTabTransaction.locator.ts
+++ src/modules/pld/accountSetting/branch/branchTabs/branchTabTransaction/branchTabTransaction.locator.ts
@@ -4,19 +4,33 @@ export default class BranchTabTransactionLocator extends BaseLocator {
 
     static branchTransactionTab: string = "//div[contains(text(),'Transaksi')]";
     static branchMenuBookDropdown: string = "//nz-select[@id='menuTemplateID']";
-    static branchRadioBtnBeforeDisc: string = "//label[.//span[text()=' Sebelum Diskon ']]//input[@type='radio']";
-    static branchRadioBtnAfterDisc: string = "//label[.//span[text()=' Setelah Diskon ']]//input[@type='radio']";
     static branchTaxValueField: string = "//input[@formcontrolname='taxValue' and @placeholder='Nilai Pajak (%)']";
     static branchServiceChargeField: string = "//input[@placeholder='Nilai Service Charge']";
-    static branchRadioBtnOtherTax: string = "//nz-form-control//nz-switch[@id='flagOtherTaxVat']";
+
+    //toggle
+    static branchToggleOtherTax: string = "//nz-form-control//nz-switch[@id='flagOtherTaxVat']";
+
+    //radioButton
+    static branchRadioBtnBeforeDisc: string = "//label[.//span[text()=' Sebelum Diskon ']]//input[@type='radio']";
+    static branchRadioBtnAfterDisc: string = "//label[.//span[text()=' Setelah Diskon ']]//input[@type='radio']";
+    static branchRadioBtnRoundingYes: string = "//label[span[text()=' Ya ']]/span/input";
+    static branchRadioBtnRoundingNo: string = "//nz-radio-group[@formcontrolname='activateRounding']//label[span[text()=' Tidak ']]/span/input";
 
 
+
+
+    //checkBox
     static paymentCashCheckBox: string = "//label[@class='ant-checkbox-wrapper ng-valid ng-dirty ng-touched cdk-focused cdk-mouse-focused']//input[@type='checkbox']";
-    static branchTransactionSaveButton: string = "//button[@class='button button-orange button-small ng-star-inserted']";
-    static branchTransactionCancelButton: string = "//button[@class='button button-outline-red button-small ng-star-inserted']";
     static branchTabTransactionCheckBoxCash: string = "//label[.//p[@class='child-title' and contains(text(), 'Cash')]]//input[@type='checkbox']";
     static branchTabTransactionCheckBoxDana: string = "//label[.//p[@class='child-title' and contains(text(), 'Dana')]]//input[@type='checkbox']";
     static branchTabTransactionCheckBoxOVO: string = "//label[.//p[@class='child-title' and contains(text(), 'OVO')]]//input[@type='checkbox']";
     static branchTabTransactionCheckBoxGoPay: string = "//label[.//p[@class='child-title' and contains(text(), 'GoPay')]]//input[@type='checkbox']";
     static branchTabTransactionCheckBoxQRIS: string = "//label[.//p[@class='child-title' and contains(text(), 'QRIS')]]//input[@type='checkbox']"
+
+    //button
+    static branchTransactionButtonAddPayment: string = "//button[normalize-space()='Tambah']";
+    static branchTransactionButtonActivate: string = "//button[normalize-space()='Aktifkan']";
+    static branchTransactionButtonSave: string = "//button[@class='button button-orange button-small ng-star-inserted']";
+    static branchTransactionButtonCancel: string = "//button[@class='button button-outline-red button-small ng-star-inserted']";
+
 }
