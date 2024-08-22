0c8031802254b6f0b557b3134f0f269f91521f69
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-22 14:26:25 +0700

    [ADD] performBranchElementCheck test case

--- tests/testDemo/demoTC.spec.ts
+++ tests/testDemo/demoTC.spec.ts
@@ -1,9 +1,10 @@
 import {test} from "@playwright/test";
 import LoginPage from "../../src/modules/pld/login/login.page";
 import LogoutPage from "../../src/modules/pld/account/logout/logout.page";
+import BranchPage from "../../src/modules/pld/accountSetting/branch/branch.page";
 
 
-test.describe.serial('accountPage', () => {
+test.describe.serial('Demo Test Case', () => {
     let loginPage: LoginPage;
 
 
@@ -13,13 +14,18 @@ test.describe.serial('accountPage', () => {
         await loginPage.performLoginSubs();
     });
 
-    test('Verify if user can access logout page', {tag: '@demoTest, @account'}, async ({page}) => {
+    test('Verify if user can access logout page', {tag: '@demoTest, @accountDemo'}, async ({page}) => {
         const logout = new LogoutPage(page);
         await logout.navigateToLogoutPage();
 
     });
 
+    test('Verify if user can access branch page ', {tag: '@demoTest, @branchDemo'}, async ({page}) => {
+        const branch = new BranchPage(page);
+        await branch.navigateToBranchSetting();
+        await branch.performBranchElementCheck();
 
+    });
 
 
 });
