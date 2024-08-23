75a1bf078051d57c02a43dd93c0721ed0bad4a8d
Kornelius Sendy <kornelius.sendy@esb.co.id>
2024-08-23 08:49:48 +0700

    [MOD] implement in base core page

--- src/modules/core/base-core-page.ts
+++ src/modules/core/base-core-page.ts
@@ -1,9 +1,10 @@
 import BasePage from "../../base/base-page";
 import CoreUrls from "../../configs/core/core-urls";
 import {Page} from "@playwright/test";
+import CoreConfigs from "../../configs/core/core-configs";
 
-export default abstract class BaseCorePage extends BasePage<CoreUrls> {
+export default abstract class BaseCorePage extends BasePage<CoreUrls, CoreConfigs> {
     public constructor(page: Page) {
-        super(page, CoreUrls.Instance);
+        super(page, CoreUrls.Instance, CoreConfigs.Instance);
     }
 }
