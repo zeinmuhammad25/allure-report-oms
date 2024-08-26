f4e9268c744cad43a8712615d05417c3c00a5d1a
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-26 16:40:35 +0700

    [MOD] refactor locator for platformIntegration

--- src/modules/pld/onlinePlatform/platformIntegration/platformIntegration.page.ts
+++ src/modules/pld/onlinePlatform/platformIntegration/platformIntegration.page.ts
@@ -12,20 +12,10 @@ export default class PlatformIntegrationPage extends BasePosLitePage implements 
 
     shouldHave(): Element[] {
         return [
-            Element.ofText("Integrasi Platform"),
-            Element.ofText("4 Keuntungan Integrasi Platform Anda:"),
             Element.ofSelector(PlatformIntegrationLocator.ruleOneImage),
-            Element.ofText("Pengaturan 1 Platform"),
-            Element.ofText("Atur menu dan harga secara bersamaan di POSLite Dashboard"),
             Element.ofSelector(PlatformIntegrationLocator.ruleTwoImage),
-            Element.ofText("Input Data Mudah"),
-            Element.ofText("Input transaksi otomatis dari GrabFood ke POSLite"),
             Element.ofSelector(PlatformIntegrationLocator.ruleThreeImage),
-            Element.ofText("Rangkuman Laporan"),
-            Element.ofText("Laporan harian & bulanan terangkum di POSLite dashboard"),
             Element.ofSelector(PlatformIntegrationLocator.ruleFourImage),
-            Element.ofText("Integrasi Transaksi"),
-            Element.ofText("Lihat pendapatan harian tanpa khawatir berbeda"),
 
         ];
     }
