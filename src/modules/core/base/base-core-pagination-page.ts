d714b913a34176e2a6b73846d7be8463ce94c794
Kornelius Sendy <kornelius.sendy@esb.co.id>
2024-08-26 19:39:33 +0700

    [MOD] fix inputFilterCurrencySign

--- src/modules/core/base/base-core-pagination-page.ts
+++ src/modules/core/base/base-core-pagination-page.ts
@@ -2,7 +2,7 @@ import BaseCorePage from "./base-core-page";
 import {CoreFilter} from "../objects/CoreFilter";
 
 export default abstract class BaseCorePaginationPage extends BaseCorePage {
-    private locatorLoading = "#w0-container[contains(@class, 'kv-grid-loading')]";
+    private locatorLoading = "//div[@class='kv-grid-loading' and @id='#w0-container']";
 
     abstract withFilters(): CoreFilter[];
 
--- src/modules/core/pages/currency/view/currency-view-locator.ts
+++ src/modules/core/pages/currency/view/currency-view-locator.ts
@@ -1,5 +1,5 @@
 export default class CurrencyViewLocator {
     public static inputFilterCurrencyName: string = "//input[@name='MsCurrencyForm[currencyName]']";
     public static inputFilterCurrencyRate: string = "#mscurrencyform-rate";//"//input[@name='MsCurrencyForm[rate]']";
-    public static inputFilterCurrencySign: string = "//input[@name='MsCurrencyForm[currencySign]']";
+    public static inputFilterCurrencySign: string = "//input[@name='MsCurrencyForm[currencySign]' and @type='text']";
 }
