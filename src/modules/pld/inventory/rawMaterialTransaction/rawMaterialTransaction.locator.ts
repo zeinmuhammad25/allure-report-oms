39a44296c1289137ae04939153d88a3714c85aec
Ryan Mustashim <ryan.mustashim@esb.co.id>
2024-08-26 16:34:34 +0700

    [MOD] refactor locator for rawMaterialTransaction

--- src/modules/pld/inventory/rawMaterialTransaction/rawMaterialTransaction.locator.ts
+++ src/modules/pld/inventory/rawMaterialTransaction/rawMaterialTransaction.locator.ts
@@ -4,7 +4,7 @@ export default class RawMaterialTransactionLocator extends BaseLocator {
 
     static rawMaterialTransactionDate: string = "(//input[@type='text'])[1]";
     static rawMaterialTransactionBranch: string = "(//nz-select-item[@title='Ini Cabang 6 Bulan'][normalize-space()='Ini Cabang 6 Bulan'])[1]";
-    static rawMaterialTransactionType: string = "(//nz-select-top-control[@class='ng-tns-c137-551 ant-select-selector'])[1]";
+    static rawMaterialTransactionType: string = "//nz-select[@id='transactionTypeID']";
     static rawMaterialTransactionCount: string = "//div[@class='page-total-container']";
     static rawMaterialTransactionAdd: string = "//button[normalize-space()='Tambah Transaksi']";
 
