733a4d51a8caab141e3736b53c05d75bc0cfaa3a
ryan witsqo <ryan.witsqo@esb.co.id>
2024-08-27 15:47:07 +0700

    [MOD] refactor for renaming sidebar locator to adhere namin convention

--- src/modules/pld/dashboard/sidebar.locator.ts
+++ src/modules/pld/dashboard/sidebar.locator.ts
@@ -10,34 +10,80 @@ export default class SidebarLocator extends BaseLocator {
     static sidebarMenu: string = "//a[normalize-space()='Menu']";
 
     //accountSetting
-    static accountSettingHead: string = "//div[contains(text(),'Pengaturan Cabang')]";
-    static accountSettingBranch: string = "//a[@ng-reflect-router-link='account-setting/branch']";
-    static accountSettingBrand: string = "//a[@ng-reflect-router-link='account-setting/company']";
-    static accountSettingCompany: string = "//a[@ng-reflect-router-link='account-setting/company']";
-    static accountSettingTableSetting: string = "//a[@ng-reflect-router-link='account-setting/table']";
-    static accountSettingPaymentMethod: string = "//a[@ng-reflect-router-link='account-setting/payment-method']";
-    static accountSettingGenerateOTP: string = "//a[@ng-reflect-router-link='account-setting/otp']";
-    static accountSettingEmailNotification: string = "//a[@ng-reflect-router-link='account-setting/email-recipien']";
+    static sidebarAccountSettingHead: string = "//div[contains(text(),'Pengaturan Cabang')]";
+    static sidebarAccountSettingBranch: string = "//a[@ng-reflect-router-link='account-setting/branch']";
+    static sidebarAccountSettingBrand: string = "//a[@ng-reflect-router-link='account-setting/company']";
+    static sidebarAccountSettingCompany: string = "//a[@ng-reflect-router-link='account-setting/company']";
+    static sidebarAccountSettingTableSetting: string = "//a[@ng-reflect-router-link='account-setting/table']";
+    static sidebarAccountSettingPayment: string = "//a[@ng-reflect-router-link='account-setting/payment-method']";
+    static sidebarAccountSettingGenerateOTP: string = "//a[@ng-reflect-router-link='account-setting/otp']";
+    static sidebarAccountSettingEmailNotification: string = "//a[@ng-reflect-router-link='account-setting/email-recipien']";
 
     //printerSetting
     static printerSettingHead: string = "//div[contains(text(),'Pengaturan Printer')]";
-    static printerSettingChild: string = "//a[@ng-reflect-router-link='printer-setting/station']";
-    static menuManagementSidebar: string = "//a[@ng-reflect-router-link='printer-setting/branch-menu']";
+    static sidebarPrinterSettingChild: string = "//a[@ng-reflect-router-link='printer-setting/station']";
+    static sidebarMenuManagement: string = "//a[@ng-reflect-router-link='printer-setting/branch-menu']";
 
     //attendance
     static attendanceHead: string = "(//div[contains(text(),'Absensi')])[1]";
-    static employeeDataChild: string = "//a[@ng-reflect-router-link='attendance/employee']";
-    static attendanceDataChild: string = "//a[@ng-reflect-router-link='attendance/report']";
+    static sidebarEmployeeDataChild: string = "//a[@ng-reflect-router-link='attendance/employee']";
+    static sidebarAttendanceDataChild: string = "//a[@ng-reflect-router-link='attendance/report']";
 
     //report
-    static reportSidebarHead: string = "//div[contains(text(),'Laporan')]";
-    static cancelAndVoidSidebar: string = "//a[@ng-reflect-router-link='report/cancel-menu']";
-    static paymentReportSidebar: string = "//a[@ng-reflect-router-link='report/sales-payment']";
-    static salesDetailSidebar: string = "//a[@ng-reflect-router-link='report/sales-detail']";
-    static salesSummarySidebar: string = "//a[@ng-reflect-router-link='report/sales-summary']";
-    static reportPromotionSidebar: string = "//a[@ng-reflect-router-link='report/promotion']";
-    static profitAndLossSidebar: string = "//a[@ng-reflect-router-link='report/profit-loss']";
+    static sidebarReportHead: string = "//div[contains(text(),'Laporan')]";
+    static sidebarCancelAndVoid: string = "//a[@ng-reflect-router-link='report/cancel-menu']";
+    static sidebarPaymentReport: string = "//a[@ng-reflect-router-link='report/sales-payment']";
+    static sidebarSalesDetail: string = "//a[@ng-reflect-router-link='report/sales-detail']";
+    static sidebarSalesSummary: string = "//a[@ng-reflect-router-link='report/sales-summary']";
+    static sidebarReportPromotion: string = "//a[@ng-reflect-router-link='report/promotion']";
+    static sidebarProfitAndLoss: string = "//a[@ng-reflect-router-link='report/profit-loss']";
 
+    //catalogue
+    static sidebarCatalogueHead: string = "//div[contains(text(),'Pengaturan Menu')]";
+    static sidebarChildMenu: string = "//a[@ng-reflect-router-link='catalog/menu']";
+    static sidebarChildCategory: string = "//a[@ng-reflect-router-link='catalog/menu-category']";
+    static sidebarMenuBook: string = "//a[@ng-reflect-router-link='catalog/menu-template']";
+    static sidebarSalesMode: string = "//a[@ng-reflect-router-link='catalog/sales-mode']";
+    static sidebarCancelReason: string = "//a[@ng-reflect-router-link='catalog/cancel-reason']";
+    static sidebarMenuNote: string = "//a[@ng-reflect-router-link='catalog/notes-category']";
+    static sidebarSpecialPrice: string = "//a[@ng-reflect-router-link='catalog/special-price']";
+
+    //inventory
+    static sidebarInventoryHead: string = "//div[contains(text(),'Inventori')]";
+    static sidebarRawMaterial: string = "//a[@ng-reflect-router-link='inventory/product']";
+    static sidebarRawMaterialTransaction: string = "//a[@ng-reflect-router-link='inventory/transaction']";
+    static sidebarRawMaterialStock: string = "//a[@ng-reflect-router-link='inventory/product-inquiry']";
+    static sidebarRawMaterialReport: string = "//a[@ng-reflect-router-link='inventory/report']";
+
+    //userAccessControl
+    static sidebarUserAccessControlHead: string = "//div[contains(text(),'Hak Akses')]";
+    static sidebarUser: string = "//a[@ng-reflect-router-link='role/user']";
+    static sidebarUserAccessBackend: string = "//a[@ng-reflect-router-link='role/user-role']";
+    static sidebarUserAccessPOS: string = "//a[@ng-reflect-router-link='role/pos-user-role']";
+
+    //withdrawFunds
+    static sidebarWithdrawFundsHead: string = "//div[contains(text(),'Penarikan Dana')]";
+    static sidebarHistory: string = "//a[normalize-space()='Riwayat']";
+    static sidebarRemainingFunds: string = "//a[normalize-space()='Sisa Dana']";
+
+    //ESBOrder
+    static sidebarESBOrderHead: string = "//div[contains(text(),'ESB Order')]";
+    static sidebarESBOrderSetting: string = "//a[@ng-reflect-router-link='esb-order/setting']";
+    static sidebarESBOrderBannerMarketing: string = "//a[@ng-reflect-router-link='esb-order/banner']";
+
+    //promotion
+    static sidebarPromotionHead: string = "//a[@ng-reflect-router-link='promotion']";
+
+    //onlinePlatform
+    static sidebarOnlinePlatform: string = "//div[contains(text(),'Pesanan Online')]";
+    static sidebarPlatformIntegration: string = "//a[@ng-reflect-router-link='online-platform/integration']";
+    static sidebarManageOnlineMenu: string = "//a[@ng-reflect-router-link='online-platform/menu-setting']";
+
+    //bookkeeping
+    static sidebarBookkeeping: string = "//div[contains(text(),'Pembukuan')]";
+    static sidebarBookkeepingCategory: string = "//a[@ng-reflect-router-link='finance/charts-of-account']";
+    static sidebarBookkeepingInput: string = "//a[@ng-reflect-router-link='finance/input-finance']";
+    static sidebarBookkeepingReport: string = "//a[@ng-reflect-router-link='finance/profit-loss-report']";
 
 
 }
