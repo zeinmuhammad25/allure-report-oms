5213b9646aace796c3843d9bd761de6f8f4a2230
ryan witsqo <ryan.witsqo@esb.co.id>
2024-08-27 14:28:54 +0700

    [MOD] refactor sidebar locator for accountSettingHead

--- src/modules/pld/dashboard/sidebar.locator.ts
+++ src/modules/pld/dashboard/sidebar.locator.ts
@@ -8,8 +8,17 @@ export default class SidebarLocator extends BaseLocator {
     static sidebarClosed: string = "//img[contains(@src, 'right')]";
     static catalogue: string = "(//div[contains(text(),'Katalog')])[1]";
     static sidebarMenu: string = "//a[normalize-space()='Menu']";
+
+    //accountSetting
     static accountSettingHead: string = "//div[contains(text(),'Pengaturan Cabang')]";
     static accountSettingBranch: string = "//a[@ng-reflect-router-link='account-setting/branch']";
+    static accountSettingBrand: string = "//a[@ng-reflect-router-link='account-setting/company']";
+    static accountSettingCompany: string = "//a[@ng-reflect-router-link='account-setting/company']";
+    static accountSettingTableSetting: string = "//a[@ng-reflect-router-link='account-setting/table']";
+    static accountSettingPaymentMethod: string = "//a[@ng-reflect-router-link='account-setting/payment-method']";
+    static accountSettingGenerateOTP: string = "//a[@ng-reflect-router-link='account-setting/otp']";
+    static accountSettingEmailNotification: string = "//a[@ng-reflect-router-link='account-setting/email-recipien']";
+
     static attendanceHead: string = "(//div[contains(text(),'Absensi')])[1]";
     static employeeDataChild: string = "(//a[normalize-space()='Data Karyawan'])[1]";
     static attendanceDataChild: string = "(//a[normalize-space()='Daftar Kehadiran'])[1]";
