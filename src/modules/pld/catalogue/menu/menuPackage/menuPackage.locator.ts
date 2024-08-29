20adf2cc27fa64f6e33f35b8dc3075b1bd67f777
ryan witsqo <ryan.witsqo@esb.co.id>
2024-08-29 14:54:49 +0700

    [MOD] removed duplicate locators and page

---
+++ src/modules/pld/catalogue/menu/menuPackage/menuPackage.locator.ts
@@ -0,0 +1,8 @@
+import BaseLocator from "../../../../../base/base-locator";
+
+export default class MenuPackageLocator extends BaseLocator {
+    // Locator on Add Menu
+    static menuPackageCreateSaveButton: string = "//button[@id='btn-save']";
+    static menuPackageCreateCancelButton: string = "//button[@id='btn-cancel']";
+
+}
--- src/modules/pld/catalogue/menu/menuPackage/menuPackage.page.ts
+++ src/modules/pld/catalogue/menu/menuPackage/menuPackage.page.ts
@@ -1,30 +1,20 @@
 import BasePosLitePage from "../../../base-pos-lite-page";
-import Urls from "../../../../../configs/urls";
 import Element from "../../../../../base/objects/Element";
-import MenuLocator from "../menu.locator";
 import MenuPackageScenario from "./menuPackage.scenario";
+import MenuPackageLocator from "./menuPackage.locator";
 
 
 export default class MenuPackagePage extends BasePosLitePage implements MenuPackageScenario {
 
 
-    pageUrl = (): string => Urls.singlemenu;
-
+    pageUrl = (): string => this.urls.get.catalogue.menuPackageUrl;
 
 
     shouldHave(): Element[] {
         return [
 
-            Element.ofSelector(MenuLocator.newHeadPackageMenuRadio),
-            Element.ofSelector(MenuLocator.existingHeadPackageMenuRadio),
-            Element.ofSelector(MenuLocator.menuPackageNameField),
-            Element.ofSelector(MenuLocator.menuCodeField),
-            Element.ofSelector(MenuLocator.menuDescriptionField),
-
-            Element.ofSelector(MenuLocator.separatePrintYesRadio),
-            Element.ofSelector(MenuLocator.separatePrintNoRadio),
-            Element.ofSelector(MenuLocator.),
-            Element.ofSelector(MenuLocator.menuCategoryField),
+            Element.ofSelector(MenuPackageLocator.menuPackageCreateCancelButton),
+            Element.ofSelector(MenuPackageLocator.menuPackageCreateSaveButton),
 
 
         ];
