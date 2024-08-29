f2614ad22b9a584be0f072420a4feda9acfcd5fe
ryan witsqo <ryan.witsqo@esb.co.id>
2024-08-29 14:56:32 +0700

    [ADD] createPackageMenu scenario

--- src/modules/pld/catalogue/menu/menu.scenario.ts
+++ src/modules/pld/catalogue/menu/menu.scenario.ts
@@ -1,5 +1,11 @@
 import BaseScenario from "../../../../base/base-scenario";
+import MenuSinglePage from "./menuSingle/menuSingle.page";
+import MenuPackageLocator from "./menuPackage/menuPackage.locator";
+import MenuPackagePage from "./menuPackage/menuPackage.page";
 
 export default interface MenuScenario extends BaseScenario {
 
+    createSingleMenu(): Promise<MenuSinglePage>;
+    createPackageMenu(): Promise<MenuPackagePage>;
 }
+
