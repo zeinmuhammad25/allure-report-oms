6c6fcfb09e31977cc3015eb801f326c83102f624
ryan witsqo <ryan.witsqo@esb.co.id>
2024-08-29 14:40:33 +0700

    [MOD] Improve element checks for singleMenu.page.ts

--- src/modules/pld/catalogue/menu/singleMenu/singleMenu.page.ts
+++ src/modules/pld/catalogue/menu/singleMenu/singleMenu.page.ts
@@ -1,26 +1,29 @@
 import BasePosLitePage from "../../../base-pos-lite-page";
-import Urls from "../../../../../configs/urls";
 import Element from "../../../../../base/objects/Element";
-import MenuLocator from "../menu.locator";
 import SingleMenuScenario from "./singleMenu.scenario";
+import SingleMenuLocator from "./singleMenu.locator";
 
 
 export default class SingleMenuPage extends BasePosLitePage implements SingleMenuScenario {
 
 
-    pageUrl = (): string => this.urls.get.catalogue.singleMenuUrl;
+    pageUrl = (): string => this.urls.get.catalogue.singleMenUrl;
 
     shouldHave(): Element[] {
         return [
 
-            Element.ofSelector(MenuLocator.menuNameField),
-            Element.ofSelector(MenuLocator.menuCodeField),
-            Element.ofSelector(MenuLocator.menuCategoryButton),
-            Element.ofSelector(MenuLocator.menuDesciptionField),
-            Element.ofSelector(MenuLocator.menuCategoryField),
-            Element.ofSelector(MenuLocator.addMenuBookButton),
-            Element.ofSelector(MenuLocator.saveMenuButton),
-            Element.ofSelector(MenuLocator.cancelMenuButton),
+            Element.ofSelector(SingleMenuLocator.menuNameField),
+            Element.ofSelector(SingleMenuLocator.menuCodeField),
+            Element.ofSelector(SingleMenuLocator.menuCategoryField),
+            Element.ofSelector(SingleMenuLocator.menuDescriptionField),
+            Element.ofSelector(SingleMenuLocator.menuAddBookButton),
+            Element.ofSelector(SingleMenuLocator.menuCategoryButton),
+            Element.ofSelector(SingleMenuLocator.scRadioButton),
+            Element.ofSelector(SingleMenuLocator.TaxRadioButton),
+            Element.ofSelector(SingleMenuLocator.focRadioButton),
+            Element.ofSelector(SingleMenuLocator.replacementField),
+            Element.ofSelector(SingleMenuLocator.menuCreateSaveButton),
+            Element.ofSelector(SingleMenuLocator.menuCreateCancelButton),
 
         ];
     }
