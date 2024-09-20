b41f4e0937f97d7e0c5c64d18abc1b82792b6f4f
alvin <alvin.tutupary@esb.co.id>
2024-09-20 09:46:18 +0700

    [ADD] sales summary report test case

---
+++ tests/pld/reportTC/salesSummaryReport.spec.ts
@@ -0,0 +1,44 @@
+import {test} from "@playwright/test";
+import SalesSummaryPage from "../../../src/modules/pld/report/salesSummary/salesSummary.page";
+import LoginPage from "../../../src/modules/pld/login/login.page";
+
+test.describe.serial("Sales Summary Report Test", () => {
+    let loginPage: LoginPage
+    const tag = '@smokeTest @report @salesSummaryReport'
+
+    test.beforeEach(async ({page}) => {
+        loginPage = new LoginPage(page)
+        await loginPage.navigateHere()
+        await loginPage.performLoginSubs()
+    })
+
+    test("Verify Summary Report has Sales Statistic", {tag: tag}, async ({page}) => {
+        const salesSummaryPage = new SalesSummaryPage(page)
+        await salesSummaryPage.validateSalesStatistic()
+    })
+
+    test("Verify Summary Report has Data Sales Per Day", {tag: tag}, async ({page}) => {
+        const salesSummaryPage = new SalesSummaryPage(page)
+        await salesSummaryPage.validateDataSalesPerDay()
+    })
+
+    test("Verify Summary Report has Data Sales Per Time", {tag: tag}, async ({page}) => {
+        const salesSummaryPage = new SalesSummaryPage(page)
+        await salesSummaryPage.validateDataSalesPerTime()
+    })
+
+    test("Verify Summary Report has Data Sales", {tag: tag}, async ({page}) => {
+        const salesSummaryPage = new SalesSummaryPage(page)
+        await salesSummaryPage.validateDataSales()
+    })
+
+    test("Verify Summary Report has Data Fraud Controll", {tag: tag}, async ({page}) => {
+        const salesSummaryPage = new SalesSummaryPage(page)
+        await salesSummaryPage.validateDataSalesFraudControl()
+    })
+
+    test("Verify Summary Report can be download", {tag: tag}, async ({page}) => {
+        const salesSummaryPage = new SalesSummaryPage(page)
+        await salesSummaryPage.downloadSalesSummary()
+    })
+})
