6b1321a075fd73960244b621c9223b4d4ddfa591
alvin <alvin.tutupary@esb.co.id>
2024-09-23 02:46:44 +0700

    [ADD] profitAndLoss scenario

--- src/modules/pld/report/profitAndLoss/profitAndLoss.scenario.ts
+++ src/modules/pld/report/profitAndLoss/profitAndLoss.scenario.ts
@@ -1,5 +1,7 @@
 import BaseScenario from "../../../../base/base-scenario";
 
 export default interface ProfitAndLossScenario extends BaseScenario {
+    validateDataProfitAndLostByCompany(): Promise<void>
 
+    validateDataProfitAndLostByBranch(): Promise<void>
 }
