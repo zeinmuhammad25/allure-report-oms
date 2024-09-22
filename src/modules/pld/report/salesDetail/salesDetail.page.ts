0fe7d0461cc53e6c617dca6be49663d9e00c0e55
alvin <alvin.tutupary@esb.co.id>
2024-09-23 03:32:39 +0700

    [MOD] Report Sales Detail page

--- src/modules/pld/report/salesDetail/salesDetail.page.ts
+++ src/modules/pld/report/salesDetail/salesDetail.page.ts
@@ -2,11 +2,11 @@ import BasePosLitePage from "../../base-pos-lite-page";
 import Element from "../../../../base/objects/Element";
 import SalesDetailScenario from "./salesDetail.scenario";
 import SalesDetailLocator from "./salesDetail.locator";
+import ProfitAndLossLocator from "../profitAndLoss/profitAndLoss.locator";
+import SalesSummaryLocator from "../salesSummary/salesSummary.locator";
 
 
 export default class SalesDetailPage extends BasePosLitePage implements SalesDetailScenario {
-
-
     pageUrl = (): string => this.urls.get.report.salesDetailUrl;
 
     shouldHave(): Element[] {
@@ -22,10 +22,71 @@ export default class SalesDetailPage extends BasePosLitePage implements SalesDet
             Element.ofSelector(SalesDetailLocator.cashierName),
             Element.ofSelector(SalesDetailLocator.salesViewButton),
             Element.ofSelector(SalesDetailLocator.salesDownloadButton),
-
-
         ];
     }
 
+    private async inputSalesDate(): Promise<void> {
+
+    }
+
+    private async inputSalesCompany(): Promise<void> {
+
+    }
+
+    private async inputSalesBrand(): Promise<void> {
+
+    }
+
+    private async inputSalesBranch(): Promise<void> {
+
+    }
+
+    private async inputSalesMode(): Promise<void> {
+
+    }
+
+    private async inputSalesType(): Promise<void> {
+
+    }
+
+    private async inputPaymentMethod(): Promise<void> {
+
+    }
+
+    private async inputTransactionNumber(): Promise<void> {
+
+    }
+
+    private async inputCashierName(): Promise<void> {
+
+    }
+
+    private async fillFilterAndShow(withTransactionNo:boolean): Promise<void> {
+        await this.inputSalesDate()
+        await this.inputSalesCompany()
+        await this.inputSalesBrand()
+        await this.inputSalesBranch()
+        await this.inputSalesMode()
+        await this.inputSalesType()
+        await this.inputPaymentMethod()
+        await this.inputCashierName()
+        if(withTransactionNo){
+            await this.inputTransactionNumber()
+        }
+        await this.click(ProfitAndLossLocator.viewButton)
+    }
+
+    async validateFilterAndShowData(): Promise<void> {
+        await this.fillFilterAndShow(false)
+    }
+
+    async validateShowDataWithTransactionNumber(): Promise<void> {
+        await this.fillFilterAndShow(true)
+    }
+
+    async validateShowDataAndDownload(): Promise<void> {
+        await this.fillFilterAndShow(true)
+        await this.clickAndExpectDownloadedFile(SalesDetailLocator.salesDownloadButton, "LAPORAN_PROMOSI","xlsx");
+    }
 
 }
