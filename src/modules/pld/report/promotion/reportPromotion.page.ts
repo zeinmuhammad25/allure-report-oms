e251db1b2ac540df05ac04f353fb05a145a485a4
alvin <alvin.tutupary@esb.co.id>
2024-09-23 03:55:51 +0700

    [MOD] Report Promotion page

--- src/modules/pld/report/promotion/reportPromotion.page.ts
+++ src/modules/pld/report/promotion/reportPromotion.page.ts
@@ -5,7 +5,6 @@ import ReportPromotionLocator from "./reportPromotion.locator";
 
 export default class ReportPromotionPage extends BasePosLitePage implements ReportPromotionScenario {
 
-
     pageUrl = (): string => this.urls.get.report.promotionReportUrl;
 
     shouldHave(): Element[] {
@@ -24,5 +23,52 @@ export default class ReportPromotionPage extends BasePosLitePage implements Repo
         ];
     }
 
+private async inputSalesDate():Promise<void>{
 
+}
+private async inputSalesCompany():Promise<void>{
+
+}
+private async inputSalesBrand():Promise<void>{
+
+}
+private async inputSalesBranch():Promise<void>{
+
+}
+private async inputReportMode():Promise<void>{
+
+}
+private async inputPromotionType():Promise<void>{
+
+}
+private async inputPromotionName():Promise<void>{
+
+}
+private async inputTransactionNumber():Promise<void>{
+
+}
+
+private async fillFilterAndShow():Promise<void>{
+    await this.inputSalesDate()
+    await this.inputSalesCompany()
+    await this.inputSalesBrand()
+    await this.inputSalesBranch()
+    await this.inputReportMode()
+    await this.inputPromotionType()
+    await this.inputPromotionName()
+    await this.inputTransactionNumber()
+}
+
+    async validateListOfDataPromotion(): Promise<void> {
+        await this.fillFilterAndShow()
+    }
+
+    async validateDetailDataPromotion(): Promise<void> {
+        await this.fillFilterAndShow()
+    }
+
+    async validateDownloadDataPromotion(): Promise<void> {
+        await this.fillFilterAndShow()
+        await this.clickAndExpectDownloadedFile(ReportPromotionLocator.salesDownloadButton, 'LAPORAN_PROMOSI','xlsx')
+    }
 }
