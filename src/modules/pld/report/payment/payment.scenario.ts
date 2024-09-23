7c194f6fe12d796c196e9876d4b95c1b5d21e1eb
alvin <alvin.tutupary@esb.co.id>
2024-09-23 16:49:00 +0700

    [ADD] Report Payment scenario

--- src/modules/pld/report/payment/payment.scenario.ts
+++ src/modules/pld/report/payment/payment.scenario.ts
@@ -1,5 +1,8 @@
 import BaseScenario from "../../../../base/base-scenario";
 
 export default interface PaymentScenario extends BaseScenario {
-
+    validateFilterAndShowDataPaymentReport():Promise<void>
+    validateFilterAndShowDataDetailPaymentReport():Promise<void>
+    validateDownloadDataDetailPayment():Promise<void>
 }
+
