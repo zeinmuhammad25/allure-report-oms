fe96a91861174361b93b87489760ddc267c15aee
alvin <alvin.tutupary@esb.co.id>
2024-09-26 11:01:00 +0700

    [MOD] implement scenario in Dashboard Stock Menu page

--- src/modules/pld/dashboard/stockMenu/stockMenu.page.ts
+++ src/modules/pld/dashboard/stockMenu/stockMenu.page.ts
@@ -2,10 +2,15 @@ import BasePosLitePage from "../../base-pos-lite-page";
 import Element from "../../../../base/objects/Element";
 import StockMenuScenario from "./stockMenu.scenario";
 import StockMenuLocator from "./stockMenu.locator";
+import SidebarLocator from "../sidebar.locator";
+import {Keyboard} from "../../../../base/constants/Keyboard";
 
 
 export default class StockMenuPage extends BasePosLitePage implements StockMenuScenario {
+    private branch = "Test Cabang Edit"
+    private menuName = "Kopi Panas"
 
+    private apiStockMenu = "/stock-menu"
     pageUrl = (): string => this.urls.get.dashboard.stockMenuUrl;
 
     shouldHave(): Element[] {
@@ -23,7 +28,33 @@ export default class StockMenuPage extends BasePosLitePage implements StockMenuS
         ];
     }
 
+    private async navigateToStockMenu(): Promise<void> {
+        await this.expectVisible(SidebarLocator.sidebarStockMenu);
+        await this.click(SidebarLocator.sidebarStockMenu);
+        await this.waitForResponse(this.apiStockMenu)
+    }
+
     async validateDataOnDashboardStockMenu(): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.navigateToStockMenu()
+        await this.fillFilterAndShow()
+        await this.expectVisible(StockMenuLocator.firstDataNumber)
+    }
+
+    private async fillFilterAndShow() {
+        await this.inputBranch()
+        await this.inputMenu()
+    }
+
+    private async inputBranch() {
+        await this.expectVisible(StockMenuLocator.branchFilter)
+        await this.click(StockMenuLocator.branchFilter)
+        await this.expectVisible(StockMenuLocator.filterOptionItem(this.branch))
+        await this.click(StockMenuLocator.branchFilter)
+    }
+
+    private async inputMenu() {
+        await this.expectVisible(StockMenuLocator.inputFilter)
+        await this.fill(StockMenuLocator.inputFilter, this.menuName)
+        await this.pressKeyboard(Keyboard.ENTER)
     }
 }
