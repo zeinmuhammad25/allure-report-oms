b8b04bad49ad2779b7005b833aea340f7d8e6b89
alvin <alvin.tutupary@esb.co.id>
2024-09-26 16:27:12 +0700

    [MOD] Dashboard Book Keeping locator

--- src/modules/pld/dashboard/bookKeeping/bookKeeping.locator.ts
+++ src/modules/pld/dashboard/bookKeeping/bookKeeping.locator.ts
@@ -8,10 +8,21 @@ export default class BookKeepingLocator extends BaseLocator {
     static threeMonthTab: string = "(//button[normalize-space()='3 Bulan'])[1]";
     static sixMonthTab: string = "(//button[normalize-space()='6 Bulan'])[1]";
     static oneYearTab: string = "(//button[normalize-space()='1 Tahun'])[1]";
+    static filterOptionItem = (filterText: string): string => `//nz-option-item[@title='${filterText}']`
 
-    //Transaksi
+    // Book Keeping Statistical
+    static summaryIncome: string = "//div[contains(@class,'card-summary-income')]";
+    static summaryExpense: string = "//div[contains(@class,'card-summary-expense')]";
+    static summaryProfit: string = "//div[contains(@class,'card-summary-profit')]";
+    static summarySupplier: string = "//div[contains(@class,'card-summary-supplier')]";
+
+    //Transaction Chart
+    static chartTransaction = "//canvas[@id='myChart']"
     static incomeTab: string = "(//div[contains(text(),'Pendapatan')])[1]";
     static expenseTab: string = "(//div[contains(text(),'Pengeluaran')])[1]";
-    static profiTab: string = "(//div[contains(text(),'Keuntungan')])[1]";
+    static profitTab: string = "(//div[contains(text(),'Keuntungan')])[1]";
+
+    //Supplier Chart
+    static chartSupplier = "//canvas[@id='supplierChart']"
 
 }
