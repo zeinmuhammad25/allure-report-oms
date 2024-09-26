886683b4d95c41476e2494ba0935d045fdf92cc2
alvin <alvin.tutupary@esb.co.id>
2024-09-26 11:34:30 +0700

    [ADD] Dashboard Book Keeping scenario

--- src/modules/pld/dashboard/bookKeeping/bookKeeping.page.ts
+++ src/modules/pld/dashboard/bookKeeping/bookKeeping.page.ts
@@ -5,6 +5,27 @@ import BookKeepingLocator from "./bookKeeping.locator";
 
 
 export default class BookKeepingPage extends BasePosLitePage implements BookKeepingScenario {
+    validateStatisticalIncomeDataOnDashboardBookKeeping(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    validateStatisticalExpenditureDataOnDashboardBookKeeping(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    validateStatisticalProfitDataOnDashboardBookKeeping(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    validateTransactionIncomeChartOnDashboardBookKeeping(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    validateTransactionExpenditureChartOnDashboardBookKeeping(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    validateTransactionProfitChartOnDashboardBookKeeping(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    validateSupplierChartOnDashboardBookKeeping(): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
     pageUrl = (): string => this.urls.get.dashboard.bookKeepingUrl;
 
     shouldHave(): Element[] {
--- src/modules/pld/dashboard/bookKeeping/bookKeeping.scenario.ts
+++ src/modules/pld/dashboard/bookKeeping/bookKeeping.scenario.ts
@@ -1,6 +1,17 @@
 import BaseScenario from "../../../../base/base-scenario";
 
 export default interface BookKeepingScenario extends BaseScenario {
+    validateStatisticalIncomeDataOnDashboardBookKeeping(): Promise<void>
 
+    validateStatisticalExpenditureDataOnDashboardBookKeeping(): Promise<void>
 
+    validateStatisticalProfitDataOnDashboardBookKeeping(): Promise<void>
+
+    validateTransactionIncomeChartOnDashboardBookKeeping(): Promise<void>
+
+    validateTransactionExpenditureChartOnDashboardBookKeeping(): Promise<void>
+
+    validateTransactionProfitChartOnDashboardBookKeeping(): Promise<void>
+
+    validateSupplierChartOnDashboardBookKeeping(): Promise<void>
 }
