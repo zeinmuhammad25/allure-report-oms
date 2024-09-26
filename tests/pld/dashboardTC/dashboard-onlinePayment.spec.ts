99e365a95e729af10cfdf89ed40062604f5ab9b6
alvin <alvin.tutupary@esb.co.id>
2024-09-26 17:41:01 +0700

    [ADD] Dashboard Online Payment spec

--- tests/pld/dashboardTC/dashboard-onlinePayment.spec.ts
+++ tests/pld/dashboardTC/dashboard-onlinePayment.spec.ts
@@ -1,6 +1,6 @@
-import DashboardPage from "../../../src/modules/pld/dashboard/dashboard.page";
 import LoginPage from "../../../src/modules/pld/login/login.page";
 import {test} from "@playwright/test";
+import OnlinePaymentPage from "../../../src/modules/pld/dashboard/onlinePayment/onlinePayment.page";
 
 test.describe.serial("Dashboard Online Payment Test", () => {
     let loginPage: LoginPage
@@ -16,31 +16,31 @@ test.describe.serial("Dashboard Online Payment Test", () => {
         "Verify User can filter and show Summary Payment Data of Dashboard Online Payment",
         {tag: tag},
         async ({page}) => {
-            const dashboardPage = new DashboardPage(page)
-            await dashboardPage.validateSalesBySalesModeDataOnDashboardComposition()
+            const onlinePaymentPage = new OnlinePaymentPage(page)
+            await onlinePaymentPage.validateSummaryPaymentDataOnDashboardOnlinePayment()
         })
 
     test(
         "Verify User can filter and show Detail Payment Data of Dashboard Online Payment",
         {tag: tag},
         async ({page}) => {
-            const dashboardPage = new DashboardPage(page)
-            await dashboardPage.validateSalesBySalesModeDataOnDashboardComposition()
+            const onlinePaymentPage = new OnlinePaymentPage(page)
+            await onlinePaymentPage.validateDetailPaymentDataOnDashboardOnlinePayment()
         })
 
     test(
         "Verify User can filter and show Daily Payment Data of Dashboard Online Payment",
         {tag: tag},
         async ({page}) => {
-            const dashboardPage = new DashboardPage(page)
-            await dashboardPage.validateSalesBySalesModeDataOnDashboardComposition()
+            const onlinePaymentPage = new OnlinePaymentPage(page)
+            await onlinePaymentPage.validateDailyPaymentDataOnDashboardOnlinePayment()
         })
 
     test(
         "Verify User can filter, show, and download Data of Dashboard Online Payment",
         {tag: tag},
         async ({page}) => {
-            const dashboardPage = new DashboardPage(page)
-            await dashboardPage.validateSalesBySalesModeDataOnDashboardComposition()
+            const onlinePaymentPage = new OnlinePaymentPage(page)
+            await onlinePaymentPage.validateDownloadDataOnDashboardOnlinePayment()
         })
 })
