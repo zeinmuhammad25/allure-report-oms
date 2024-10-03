87f192988ce47f7979bb587b7c34fb94b45af941
alvin <alvin.tutupary@esb.co.id>
2024-10-03 11:52:26 +0700

    [MOD] implement branchList spec

--- tests/eso/branchListTC/searchBranch.spec.ts
+++ tests/eso/branchListTC/searchBranch.spec.ts
@@ -1,36 +1,47 @@
 import {test} from "@playwright/test";
 import LoginPage from "../../../src/modules/pld/login/login.page";
 import PaymentPage from "../../../src/modules/pld/report/payment/payment.page";
+import ProfilePage from "../../../src/modules/pld/profile/profile.page";
+import BranchListPage from "../../../src/modules/eso/pages/branchList/branchList.page";
 
 test.describe.serial("Branch List Test", () => {
+    let branchList: BranchListPage;
     const tag = '@smokeTest @eso @branchList @searchBranch '
+
+    test.beforeEach(async ({page}) => {
+        branchList = new BranchListPage(page);
+        await branchList.navigateHere();
+        await branchList.wait(300)
+    })
 
     test("Verify user can display the branch with the name Denny's Kasablanka",
         {tag: tag + '@positive'}, async ({page}) => {
-            // TODO :
-            //  Cari Branch yang tersedia memakai nama branch lengkap "Denny's Kasablanka" (User bisa melihat 1 branch yang tersedia)
+            branchList = new BranchListPage(page);
+            await branchList.searchBranch("Denny's Kasablanka")
         })
-    test("Verify user can display several branches with the name BSD",
+
+    test("Verify user can display several branches with the name Ma",
         {tag: tag + '@positive'}, async ({page}) => {
-            // TODO :
-            //  Cari Branch yang tersedia memakai sebagian nama branch "BSD" (User bisa melihat rekomendasi branch dengan nama BSD)
+            branchList = new BranchListPage(page);
+            await branchList.searchBranch("ma")
         })
 
     test("Verify user can display information that the branch is not available",
         {tag: tag + '@negative'}, async ({page}) => {
-            // TODO :
-            //  Cari Branch yang tidak tersedia dengan nama branch "XXX" (User bisa melihat informasi bahwa branch yang dicari tidak ada)
+            branchList = new BranchListPage(page);
+            await branchList.searchEmptyBranch("XXX");
         })
+
     test("Verify user can display that there are several reachable branches",
         {tag: tag + '@positive'}, async ({page}) => {
-            // TODO :
-            //  Lihat branch pada area yang terjangkau delivery
+            branchList = new BranchListPage(page);
+            await branchList.hasNearbyOutlet();
         })
 
     test("Verify user can display that there are several unreachable branches",
         {tag: tag + '@negative'}, async ({page}) => {
-            // TODO :
-            //  Lihat branch pada area yang tidak terjangkau delivery
+            branchList = new BranchListPage(page);
+            await branchList.hasOutReachOutlet();
         })
 })
 
