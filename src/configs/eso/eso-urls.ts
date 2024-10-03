1532d45c6df2c635ee8ca619c44a529c9c01b5f3
alvin <alvin.tutupary@esb.co.id>
2024-10-03 16:07:24 +0700

    [MOD] update Urls

--- src/configs/eso/eso-urls.ts
+++ src/configs/eso/eso-urls.ts
@@ -14,13 +14,15 @@ export default class EsoUrls extends BaseUrl {
     baseUrl = (): string => process.env.ESO_BASE_URL;
 
     public get = {
-        branchList: "/eso-qs/qa1",
-        loginWhatsapp: (branchCode: string) => `/eso-qs/login/whatsapp?company=QA1&branch=${branchCode}&mode=dinein&ref=%2FQA1%2FSFF10%2Forder%3Fmode%3Ddinein`,
-        history: "/eso-qs/history",
+        branchList: "/qa1",
+        searchAddress: "/search-address",
+        saveAddress: "/save-address",
         deliveryAddress: "/delivery-address",
 
-        modePage: (branchCode: string) => `/eso-qs/qa1/${branchCode}/mode`,
-        homePage: (branchCode: string, mode: string) => `/eso-qs/QA1/${branchCode}/home?mode=${mode}`,
-        orderPage: (branchCode: string, mode: string, categoryID: number) => `/eso-qs/QA1/${branchCode}/order?mode=${mode}&category=${categoryID}`
+        loginWhatsapp: (branchCode: string) => `/login/whatsapp?company=QA1&branch=${branchCode}&mode=dinein&ref=%2FQA1%2FSFF10%2Forder%3Fmode%3Ddinein`,
+        history: "/history",
+        modePage: (branchCode: string) => `/qa1/${branchCode}/mode`,
+        homePage: (branchCode: string, mode: string) => `/QA1/${branchCode}/home?mode=${mode}`,
+        orderPage: (branchCode: string, mode: string, categoryID: number) => `/QA1/${branchCode}/order?mode=${mode}&category=${categoryID}`
     };
 }
