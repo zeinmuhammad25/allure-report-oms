89b18280cb2158cd86d8c81164753ed43d304ef3
alvin <alvin.tutupary@esb.co.id>
2024-10-03 11:49:24 +0700

    [MOD] implement branchList scenario on branchList page

--- src/modules/eso/pages/branchList/branchList.page.ts
+++ src/modules/eso/pages/branchList/branchList.page.ts
@@ -3,6 +3,9 @@ import BranchListScenario from "./branchList.scenario";
 import BranchListLocator from "./branchList.locator";
 import Element from "../../../../base/objects/Element";
 import {Language} from "../../objects/language";
+import ModePage from "../mode/mode.page";
+import DeliveryAddressPage from "../location/deliveryAddress/deliveryAddress.page";
+import HistoryPage from "../history/history.page";
 
 export default class BranchListPage extends BaseEsoPage implements BranchListScenario {
 
@@ -18,21 +21,65 @@ export default class BranchListPage extends BaseEsoPage implements BranchListSce
         ]
     }
 
-
     async gotoLocationPage(): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.expectVisible(BranchListLocator.locationSection)
+        await this.clickAndExpectGotoPage(BranchListLocator.locationSection, DeliveryAddressPage)
     }
 
     async changeLanguage(language: Language): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.setLanguage(language)
+        await this.expectLanguage(language)
     }
 
     async searchBranch(branchName: string): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.expectVisible(BranchListLocator.inputField);
+        await this.fill(BranchListLocator.inputField, branchName);
+        await this.expectVisible(BranchListLocator.getBranchByName(branchName));
+    }
+
+    async searchEmptyBranch(branchName: string): Promise<void> {
+        await this.expectVisible(BranchListLocator.inputField);
+        await this.fill(BranchListLocator.inputField, branchName);
+        await this.expectInvisible(BranchListLocator.getBranchByName(branchName));
+    }
+
+    async hasNearbyOutlet(): Promise<void> {
+        await this.expectTextVisible('Find Outlets Around You');
+    }
+
+    async hasOutReachOutlet(): Promise<void> {
+        await this.setGeoLocation(-68.169749, 92.133269)
+        await this.expectTextVisible('Outlet is Out of Reach');
     }
 
     async selectBranch(branchName: string): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.expectVisible(BranchListLocator.getBranchByName(branchName));
+        await this.clickAndExpectGotoPage(BranchListLocator.getBranchByName(branchName), ModePage);
     }
 
+    async gotoHistoryPage(): Promise<void> {
+        await this.expectVisible(BranchListLocator.historyButton);
+        await this.clickAndExpectGotoPage(BranchListLocator.historyButton, HistoryPage);
+    }
+
+    private async setLanguage(language: Language) {
+        await this.expectVisible(BranchListLocator.languageButton);
+        await this.click(BranchListLocator.languageButton);
+
+        if (language === Language.Indonesia) {
+            await this.expectVisible(BranchListLocator.idLanguage);
+            await this.click(BranchListLocator.idLanguage);
+        } else {
+            await this.expectVisible(BranchListLocator.enLanguage);
+            await this.click(BranchListLocator.enLanguage);
+        }
+    }
+
+    private async expectLanguage(language: Language) {
+        if (language === Language.Indonesia) {
+            await this.expectVisible(BranchListLocator.languageIconButton('id'))
+        } else {
+            await this.expectVisible(BranchListLocator.languageIconButton('en'))
+        }
+    }
 }
--- src/modules/eso/pages/branchList/branchList.scenario.ts
+++ src/modules/eso/pages/branchList/branchList.scenario.ts
@@ -2,8 +2,13 @@ import BaseScenario from "../../../../base/base-scenario";
 import {Language} from "../../objects/language";
 
 export default interface BranchListScenario extends BaseScenario {
-    gotoLocationPage():Promise<void>
-    changeLanguage(language:Language):Promise<void>
-    searchBranch(branchName:string):Promise<void>
-    selectBranch(branchName:string):Promise<void>
+    gotoLocationPage(): Promise<void>
+
+    gotoHistoryPage(): Promise<void>
+
+    changeLanguage(language: Language): Promise<void>
+
+    searchBranch(branchName: string): Promise<void>
+
+    selectBranch(branchName: string): Promise<void>
 }
