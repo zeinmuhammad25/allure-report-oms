57bd1a9cbdb246c91ab28fd585821d4373101afc
alvin <alvin.tutupary@esb.co.id>
2024-10-07 05:08:15 +0700

    [MOD] implement reservation scenario on reservation page

--- src/modules/eso/pages/reservation/reservation.locator.ts
+++ src/modules/eso/pages/reservation/reservation.locator.ts
@@ -1,11 +1,47 @@
 import BaseLocator from "../../../../base/base-locator";
+import {Salutation} from "../../objects/salutation";
 
 
 export default class ReservationLocator extends BaseLocator {
-    static timeButton = (id: number) => `//button[@id='time-button-${id}']`;
+
+
+    static scheduleConfirmButton: string = `(//button//span[contains(text(),'Continue')])[1]`;
+    static fillDataConfirmButton: string = `(//button//span[contains(text(),'Continue')])[2]`;
+    static reservationConfirmButton: string = `//button[@id='confirm-reservation-button']`;
+    static cancelButton: string = `//button//span[text()='Cancel']`;
+    static cancelConfirmButton: string = `//button[@id='selection-botton-0']`;
+    static cancelAbortButton: string = `//button[@id='selection-botton-1']`;
+
+    // Pax
+    static paxDropdownButton: string = "//mat-icon[text()='people']";
+    static checkPax = (totalPax:number): string => `//button//div[contains(text(),'${totalPax} Pax')]`;
+    static bottomSheet: string = "//mat-bottom-sheet-container";
+    static paxPickerTop: string = "//div[@data-type='top']";
+    static paxPickerMiddle: string = "//div[@data-type='middle']";
+    static paxPickerBottom: string = "//div[@data-type='bottom']";
+    static paxPickerSaveButton: string = "//button[@id='save-button']";
+
+    // Date
+    static calenderButton: string = "//button[@id='calendar-button']";
     static dateButton = (id: number) => `//button[@id='button-date-${id}']`;
-    static continueButton = (id: number) => `//button//span[text()='Continue']`;
-    static cancelButton = (id: number) => `//button//span[text()='Cancel']`;
-    static cancelConfirmButton = (id: number) => `//button[@id='selection-botton-0']`;
-    static cancelAbortButton = (id: number) => `//button[@id='selection-botton-1']`;
+
+    // Time
+    static timeButton = (id: number) => `//button[@id='time-button-${id}']`;
+    static earlyTimeButton: string = `//button[@id='time-button-0']`;
+    static latestTimeButton: string = `//div[contains(@class,'btn-time-container')]/button[last()]`;
+    static confirmTimeButton: string = `//button[@id='submit-purpose-button']`;
+
+    static emailField: string = "//input[@id='email']";
+    static nameField: string = "//input[@id='customerName']";
+    static phoneField: string = "//input[@id='phoneNumber']";
+    static notesField: string = "//textarea[@id='notes']";
+    static salutationField: string = "//mat-select[@placeholder='Salutation']";
+    static salutation = (title: Salutation) => `//mat-option[@id='mat-option-${title}']`
+
+    // Purpose
+    static purposeDropdownButton: string = "//button[contains(@class,'btn-purpos')]";
+    static purposeOption = (purpose: string): string => `//mat-list-item/span[text()=' ${purpose} ']`;
+    static purposeConfirmButton: string = "//button[@id='submit-purpose-button']";
+
+
 }
--- src/modules/eso/pages/reservation/reservation.page.ts
+++ src/modules/eso/pages/reservation/reservation.page.ts
@@ -1,24 +1,129 @@
 import Element from "../../../../base/objects/Element";
 import BaseEsoPage from "../../base/base-eso-page";
 import ReservationScenario from "./reservation.scenario";
+import ReservationLocator from "./reservation.locator";
+import {Salutation} from "../../objects/salutation";
 
 export default class ReservationPage extends BaseEsoPage implements ReservationScenario {
 
-    pageUrl: () => string;
+    private apiReservation = '/qsv1/reservation/transaction';
+
+    pageUrl = (): string => this.urls.get.reservation;
 
     shouldHave(): Element[] {
-        throw new Error("Method not implemented.");
+        return [
+            Element.ofSelector(ReservationLocator.paxDropdownButton)
+        ]
     }
 
-    setDay(): Promise<void> {
-        throw new Error("Method not implemented.");
+    async setDay(daysFromNow: number = 0): Promise<void> {
+        if (daysFromNow < 0 && daysFromNow > 3) {
+            throw new Error("Method not implemented.");
+        } else {
+            await this.expectVisible(ReservationLocator.dateButton(daysFromNow));
+            await this.click(ReservationLocator.dateButton(daysFromNow));
+        }
     }
 
-    setTime(): Promise<void> {
-        throw new Error("Method not implemented.");
+    async setTime(minutesFromNow: number): Promise<void> {
+        if (minutesFromNow === -1) {
+            await this.expectVisible(ReservationLocator.latestTimeButton);
+            await this.click(ReservationLocator.latestTimeButton);
+        } else if (minutesFromNow > 0) {
+            const times = Math.floor(minutesFromNow / 30);
+            await this.expectVisible(ReservationLocator.timeButton(times));
+            await this.click(ReservationLocator.timeButton(times));
+        } else {
+            await this.expectVisible(ReservationLocator.earlyTimeButton);
+            await this.click(ReservationLocator.earlyTimeButton);
+        }
     }
 
-    goBack(): Promise<void> {
+
+    async setPax(total: number, expectEmpty:boolean = false): Promise<void> {
+        await this.click(ReservationLocator.paxDropdownButton);
+        await this.expectVisible(ReservationLocator.bottomSheet);
+        await this.selectPaxPicker(total);
+        await this.wait(300);
+        if(expectEmpty){
+            await this.expectInvisible(ReservationLocator.checkPax(total));
+        }else{
+            await this.expectVisible(ReservationLocator.checkPax(total));
+        }
+
+    }
+
+    private async selectPaxPicker(total: number) {
+        await this.expectVisible(ReservationLocator.paxPickerTop);
+        await this.expectVisible(ReservationLocator.paxPickerMiddle);
+        await this.expectVisible(ReservationLocator.bottomSheet);
+
+        if (total < 2) {
+            await this.click(ReservationLocator.paxPickerTop)
+        } else if (total > 2) {
+            const counter = total - 2;
+            for (let i = 0; i < counter; i++) {
+                await this.click(ReservationLocator.paxPickerBottom);
+            }
+        }
+        await this.wait(300);
+        await this.expectVisible(ReservationLocator.paxPickerSaveButton);
+        await this.click(ReservationLocator.paxPickerSaveButton);
+    }
+
+    async inputSalutation(title: Salutation): Promise<void> {
+        await this.expectVisible(ReservationLocator.salutationField);
+        await this.click(ReservationLocator.salutationField);
+        await this.expectVisible(ReservationLocator.salutation(title));
+        await this.click(ReservationLocator.salutation(title));
+    }
+
+    async inputEmail(email: string): Promise<void> {
+        await this.expectVisible(ReservationLocator.emailField);
+        await this.fill(ReservationLocator.emailField, email);
+    }
+
+    async inputName(name: string): Promise<void> {
+        await this.expectVisible(ReservationLocator.nameField);
+        await this.fill(ReservationLocator.nameField, name);
+    }
+
+    async inputPhoneNumber(phone: string): Promise<void> {
+        await this.expectVisible(ReservationLocator.phoneField);
+        await this.fillPhone(ReservationLocator.phoneField, phone, false);
+    }
+
+    async inputPurpose(purpose: string): Promise<void> {
+        await this.expectVisible(ReservationLocator.purposeDropdownButton);
+        await this.click(ReservationLocator.purposeDropdownButton);
+        await this.expectVisible(ReservationLocator.purposeOption(purpose));
+        await this.click(ReservationLocator.purposeOption(purpose));
+        await this.expectVisible(ReservationLocator.purposeConfirmButton);
+        await this.click(ReservationLocator.purposeConfirmButton);
+    }
+
+    async inputNotes(notes: string) {
+        await this.expectVisible(ReservationLocator.notesField);
+        await this.fill(ReservationLocator.notesField, notes);
+    }
+
+
+    async confirmSchedule(): Promise<void> {
+        await this.expectVisible(ReservationLocator.scheduleConfirmButton);
+        await this.click(ReservationLocator.scheduleConfirmButton);
+    }
+
+    async confirmFillData(): Promise<void> {
+        await this.expectVisible(ReservationLocator.fillDataConfirmButton);
+        await this.click(ReservationLocator.fillDataConfirmButton);
+    }
+
+    async confirmReservation(): Promise<void> {
+        await this.expectVisible(ReservationLocator.reservationConfirmButton);
+        await this.click(ReservationLocator.reservationConfirmButton);
+    }
+
+    async goBack(): Promise<void> {
         throw new Error("Method not implemented.");
     }
 }
