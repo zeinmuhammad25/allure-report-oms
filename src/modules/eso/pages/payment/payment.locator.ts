d5d404f95d05473a2fd8c06512e64ea40b803516
alvin <alvin.tutupary@esb.co.id>
2024-10-09 13:16:13 +0700

    [MOD] implement payment page

--- src/modules/eso/pages/payment/payment.locator.ts
+++ src/modules/eso/pages/payment/payment.locator.ts
@@ -1,7 +1,20 @@
 import BaseLocator from "../../../../base/base-locator";
+import {PaymentMethod} from "../../objects/paymentMethod";
 
 
 export default class PaymentLocator extends BaseLocator {
-    static backButton: string;
+    static backButton: string = "//button[contains(@class, 'back-button')]";
+    static payButton: string = "//button[@id='p-payment']";
+    static payConfirmButton: string = "//button[@id='selection-botton-1']";
+    static payCancelButton: string = "//button[@id='selection-botton-0']";
+    static fullNameField: string = "//input[@id='full-name-input']";
+    static phoneField: string = "//input[@id='phone-number-input']";
+    static emailField: string = "//input[@id='email-input']";
+    static tableNameField: string = "//input[@id='table-name-input']";
+    static onlinePaymentButton: string = "(//input[@id='table-name-input'])[1]";
+    static offlinePaymentButton: string = "(//input[@id='table-name-input'])[2]";
+    static paymentMethodRadioButton = (paymentMethod:PaymentMethod) : string => `//mat-radio-button[@id='payment-method-list-radio-${paymentMethod}']`;
+    static paymentTotalButton: string = "mat-icon";
+    static addPromotionButton: string = "//button[@id='p-promo-voucher']";
 
 }
