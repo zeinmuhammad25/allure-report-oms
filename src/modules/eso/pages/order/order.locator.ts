732db45e2e6b324667c73372ea60d8fad9061987
alvin <alvin.tutupary@esb.co.id>
2024-10-09 17:14:44 +0700

    [MOD] update order page

--- src/modules/eso/pages/order/order.locator.ts
+++ src/modules/eso/pages/order/order.locator.ts
@@ -9,7 +9,7 @@ export default class OrderLocator extends BaseLocator {
 
     private static getOrderButton =
         (menuID: number, buttonName: string): string =>
-            `//app-menu-list-item[@id='menu-${menuID}']//button[aria-label='${buttonName}-button']`;
+            `//app-menu-list-item[@id='menu-${menuID}']//button[@aria-label='${buttonName}-button']`;
     static addButton = (menuID: number): string => this.getOrderButton(menuID, 'add');
     static minusButton = (menuID: number): string => this.getOrderButton(menuID, 'plus');
     static plusButton = (menuID: number): string => this.getOrderButton(menuID, 'minus');
