c13cebd94c47c2fb635bc76b95d2e7910eeea093
alvin <alvin.tutupary@esb.co.id>
2024-10-10 15:10:52 +0700

    [MOD] reformat dineIn-membership spec

--- tests/eso/dineInTC/dineIn-applyMembership.spec.ts
+++ tests/eso/dineInTC/dineIn-applyMembership.spec.ts
@@ -5,33 +5,33 @@ import OrderPage from "../../../src/modules/eso/pages/order/order.page";
 import {EsoMode} from "../../../src/modules/eso/objects/esoMode";
 
 test.describe.serial("Dine In Test", () => {
-    const tag = '@smokeTest @eso @dineIn @applyMembership '
+    const tag = "@smokeTest @eso @dineIn @applyMembership ";
 
     let orderPage: OrderPage;
 
-    const brancName = "Denny's Kasablanka"
+    const branchName = "Denny's Kasablanka";
 
     test.beforeEach(async ({page}) => {
 
         let branchList = new BranchListPage(page);
         let modePage = new ModePage(page);
-        orderPage = new OrderPage(page)
+        orderPage = new OrderPage(page);
 
         await branchList.navigateHere();
         await branchList.wait(300);
-        await branchList.searchBranch(brancName);
-        await branchList.selectBranch(brancName);
+        await branchList.searchBranch(branchName);
+        await branchList.selectBranch(branchName);
         await modePage.performCheckInitialElements();
         await modePage.selectMode(EsoMode.DineIn);
         await orderPage.inputTable(1);
         await orderPage.openSideBar();
         await orderPage.openMembershipForm();
-    })
+    });
 
     test("Verify user successfully apply Membership Loop in dine-in mode  ",
-        {tag: tag + '@positive'}, async ({page}) => {
-            const phoneNumber = '083806992528'
-            const password = 'abcd123'
+        {tag: tag + "@positive"}, async ({page}) => {
+            const phoneNumber = "083806992528";
+            const password = "abcd123";
 
             await orderPage.inputPhoneNumberMembership(phoneNumber);
             await orderPage.inputPasswordMembership(password);
@@ -39,12 +39,12 @@ test.describe.serial("Dine In Test", () => {
         });
 
     test("Verify user fail to apply Membership Loop  ",
-        {tag: tag + '@negative'}, async ({page}) => {
-            const invalidPhoneNumber = '0838';
-            const password = 'abcd123';
+        {tag: tag + "@negative"}, async ({page}) => {
+            const invalidPhoneNumber = "0838";
+            const password = "abcd123";
 
             await orderPage.inputPhoneNumberMembership(invalidPhoneNumber);
             await orderPage.inputPasswordMembership(password);
-            await orderPage.expectFailedApplyMembership();
+            await orderPage.expectInvalidInputOnApplyMembership();
         });
-})
+});
