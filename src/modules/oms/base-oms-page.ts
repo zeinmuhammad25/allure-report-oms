5db8542930813dfbf7665a928e4b4ef77985f76b
safruddin.setia <safruddin.setia@esb.co.id>
2024-10-13 14:11:43 +0700

    [ADD] Files base-oms-page.ts

---
+++ src/modules/oms/base-oms-page.ts
@@ -0,0 +1,13 @@
+import BasePage from "../../base/base-page";
+import OmsUrls from "../../configs/oms/pos-oms-urls";
+import {Page} from "@playwright/test";
+import OmsConfigs from "../../configs/oms/pos-oms-configs";
+
+export default abstract class BaseOmsPage extends BasePage<OmsUrls, OmsConfigs> {
+
+    public constructor(page: Page) {
+        super(page, OmsUrls.Instance, OmsConfigs.Instance);
+    }
+
+    protected variableBuatSemuaPosLitePage: string = this.urls.baseUrl();
+}
