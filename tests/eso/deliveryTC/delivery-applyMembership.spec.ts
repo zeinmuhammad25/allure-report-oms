f7447a8e910d1f01cbf7963193f1de6aca7a89e9
alvin <alvin.tutupary@esb.co.id>
2024-10-15 10:37:39 +0700

    [MOD] implement Delivery spec

--- tests/eso/deliveryTC/delivery-applyMembership.spec.ts
+++ tests/eso/deliveryTC/delivery-applyMembership.spec.ts
@@ -1,32 +1,46 @@
 import {test} from "@playwright/test";
+import BranchListPage from "../../../src/modules/eso/pages/branchList/branchList.page";
+import ModePage from "../../../src/modules/eso/pages/mode/mode.page";
+import {EsoMode} from "../../../src/modules/eso/objects/esoMode";
+import WhatsappPage from "../../../src/modules/eso/pages/login/whatsapp/whatsapp.page";
+import OrderPage from "../../../src/modules/eso/pages/order/order.page";
 
 test.describe.serial("Delivery Test", () => {
-    const tag = '@smokeTest @eso @delivery @applyMembership '
+    const tag = "@smokeTest @eso @delivery @applyMembership ";
 
     test.beforeEach(async ({page}) => {
-        // 1. Buka eso delivery via url https://qa7.esb.co.id/eso-qs/qa1/SFF10
-        // 2. Pilih mode delivery
-        // 3. Klik simpan
-        // 4. Klik side burger menu
-        // 5. Klik icon mambership loop
-    })
+        let branchListPage = new BranchListPage(page);
+        let modePage = new ModePage(page);
+
+        await branchListPage.navigateHere();
+        await branchListPage.wait(300);
+        await branchListPage.searchBranch("Denny's Kasablanka");
+        await branchListPage.selectBranch("Denny's Kasablanka");
+        await modePage.selectMode(EsoMode.Delivery);
+        await modePage.performCheckInitialElements();
+    });
 
     test("Verify user can successfully apply membership loop in delivery mode",
-        {tag: tag + '@positive'}, async ({page}) => {
-            //TODO:
-            // Apply Membership
-            // 6. Masukkan nomor ponsel yang valid
-            // 7 Masukkan kata sandi yang valid
-            // 8. Klik masuk
-        })
+        {tag: tag + "@positive"}, async ({page}) => {
+            let orderPage = new OrderPage(page);
+
+            const phoneNumber = "083806992528";
+            const password = "abcd123";
+
+            await orderPage.performApplyMembershipSubs(phoneNumber, password);
+        });
 
     test("Verify user cannot apply membership loop",
-        {tag: tag + '@negative'}, async ({page}) => {
-            //TODO:
-            // Apply Membership
-            // 6. Masukkan nomor ponsel yang tidak valid
-            // 7. Masukkan kata sandi yang valid
-            // 8. Klik masuk
-        })
+        {tag: tag + "@negative"}, async ({page}) => {
+            let orderPage = new OrderPage(page);
+            const invalidPhoneNumber = "084806992528";
+            const password = "abcd123";
 
-})
+            await orderPage.openSideBar();
+            await orderPage.openMembershipForm();
+            await orderPage.inputPhoneNumberMembership(invalidPhoneNumber);
+            await orderPage.inputPasswordMembership(password);
+            await orderPage.submitWithExpectationFailedResult();
+        });
+
+});
--- tests/eso/deliveryTC/delivery-language.spec.ts
+++ tests/eso/deliveryTC/delivery-language.spec.ts
@@ -6,7 +6,7 @@ import {EsoMode} from "../../../src/modules/eso/objects/esoMode";
 import {Language} from "../../../src/modules/eso/objects/language";
 
 test.describe.serial("Delivery Test", () => {
-    const tag = '@smokeTest @eso @delivery @language '
+    const tag = "@smokeTest @eso @delivery @language ";
 
     let orderPage: OrderPage;
 
@@ -15,7 +15,7 @@ test.describe.serial("Delivery Test", () => {
         let modePage = new ModePage(page);
         orderPage = new OrderPage(page);
 
-        const branchName = "Denny's Kasablanka"
+        const branchName = "Denny's Kasablanka";
 
         await branchList.navigateHere();
         await branchList.wait(300);
@@ -23,19 +23,19 @@ test.describe.serial("Delivery Test", () => {
         await branchList.selectBranch(branchName);
         await modePage.performCheckInitialElements();
         await modePage.selectMode(EsoMode.Delivery);
-    })
+    });
 
     test("Verify user can successfully set the language to Indonesian",
-        {tag: tag + '@positive'}, async ({page}) => {
+        {tag: tag + "@positive"}, async ({page}) => {
             await orderPage.openSideBar();
             await orderPage.changeLanguage(Language.Indonesia);
-        })
+        });
 
     test("Verify user can successfully set the language to English",
-        {tag: tag + '@positive'}, async ({page}) => {
+        {tag: tag + "@positive"}, async ({page}) => {
             await orderPage.openSideBar();
             await orderPage.changeLanguage(Language.Indonesia);
             await orderPage.openSideBar();
             await orderPage.changeLanguage(Language.English);
-        })
-})
+        });
+});
