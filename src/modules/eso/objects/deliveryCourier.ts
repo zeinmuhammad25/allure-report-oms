40d83bba7fc283f644ad04c6dc5f037f9eb568b3
alvin <alvin.tutupary@esb.co.id>
2024-10-16 15:04:00 +0700

    [MOD] Update delivery page

---
+++ src/modules/eso/objects/deliveryCourier.ts
@@ -0,0 +1,9 @@
+export enum DeliveryCourier {
+    GrabExpress = "Grab Express",
+    GrabExpressSameDay = "Grab Express - Sameday",
+    PaxelSameDay = "Paxel - Sameday",
+    GoSend = "GoSend",
+    Lalamove = "Lalamove",
+    Borzo = "Borzo",
+    InternalCourier = "Internal Courier",
+}
