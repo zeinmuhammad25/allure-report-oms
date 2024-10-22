c4abbdb8c38e5152fca1f17dfa93c3d4cdcc300e
safruddin.setia <safruddin.setia@esb.co.id>
2024-10-22 16:37:46 +0700

    [MOD] terminalID.locator.ts

--- src/modules/oms/terminalID/terminalID.locator.ts
+++ src/modules/oms/terminalID/terminalID.locator.ts
@@ -2,11 +2,12 @@ import BaseLocator from "../../../base/base-locator";
 
 export default class TerminalIDLocator extends BaseLocator {
 
-    static getTerminalID = (terminalNumber: number): string => `//b[normalize-space()='Terminal ID 0${terminalNumber}']`;
     static btnSyncTerminalID: string = "//label[normalize-space()='Sync']";
     static btnApplyTerminalID: string = "//span[@class='mat-button-wrapper' and text()='Apply']";
     static btnCancelTerminalID: string = "//span[normalize-space()='Cancel']";
     static btnClaimTerminalID: string = "//span[normalize-space()='Claim']";
     static btnSaveTerminalID: string = "//span[normalize-space()='Save']";
     static btnInputNotesTerminalID: string = "//input[@placeholder='Input notes']";
+
+    static getTerminalID = (terminalNumber: number): string => `//b[normalize-space()='Terminal ID 0${terminalNumber}']`;
 }
