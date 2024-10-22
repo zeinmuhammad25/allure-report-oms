d193a6d2851b88ee1e2aeb08dc6daed698202651
reza fadhilah <reza.fadhilah@esb.co.id>
2024-10-22 21:16:01 +0700

    [ADD] new Page in signPin page + submitPinValidateStartDayYes + submitPinNoUnregisteredUser + submitPinValidateStartDayNo

--- src/modules/oms/signPin/signPin.page.ts
+++ src/modules/oms/signPin/signPin.page.ts
@@ -14,6 +14,9 @@ export default class SignPinPage extends BaseOmsPage implements signPinScenario 
             Element.ofSelector(SignPinLocator.buttonPin(2)),
             Element.ofSelector(SignPinLocator.buttonPin("CLR")),
             Element.ofSelector(SignPinLocator.buttonSignIn),
+            Element.ofSelector(SignPinLocator.validationSignInUserYes),
+            Element.ofSelector(SignPinLocator.validationSignInUserNo),
+            Element.ofSelector(SignPinLocator.userNotFoundPopup),
             Element.ofSelector(SignPinLocator.quickServiceListBtn),
             Element.ofSelector(SignPinLocator.tableListSingIn1),
             Element.ofSelector(SignPinLocator.tableListSingIn2),
@@ -44,11 +47,35 @@ export default class SignPinPage extends BaseOmsPage implements signPinScenario 
     async clearPin(): Promise<void> {
         await this.expectVisible(SignPinLocator.buttonPin("CLR"));
         await this.click(SignPinLocator.buttonPin("CLR"));
+        await this.wait(2000)
     }
 
-    async submitPin() {
+    async submitPinValidateStartDayYes(): Promise<void> {
         await this.expectVisible(SignPinLocator.buttonSignIn);
         await this.click(SignPinLocator.buttonSignIn);
+        await this.wait(1000);
+        await this.click(SignPinLocator.validationSignInUserYes);
+        await this.expectVisible(SignPinLocator.pageStartShift);
+        await this.wait(1000);
     }
+
+    async submitPinValidateStartDayNo(): Promise<void> {
+        await this.expectVisible(SignPinLocator.buttonSignIn);
+        await this.click(SignPinLocator.buttonSignIn);
+        await this.wait(1000);
+        await this.click(SignPinLocator.validationSignInUserNo);
+        await this.expectVisible(SignPinLocator.buttonSignIn);
+        await this.wait(1000);
+    }
+
+    async submitPinNoUnregisteredUser(): Promise<void> {
+        await this.expectVisible(SignPinLocator.buttonSignIn);
+        await this.click(SignPinLocator.buttonSignIn);
+        await this.wait(1000);
+        await this.expectVisible(SignPinLocator.userNotFoundPopup);
+        await this.click(SignPinLocator.userNotFoundPopup);
+        await this.wait(1000);
+    }
+
 }
 
