85fba98a8acafd9a02299b3c6e7aafc071138e21
safruddin.setia <safruddin.setia@esb.co.id>
2024-10-22 16:41:42 +0700

    [MOD] beforeEach in signPinTC.spec.ts

--- tests/oms/signPinTC/signPinTC.spec.ts
+++ tests/oms/signPinTC/signPinTC.spec.ts
@@ -1,11 +1,14 @@
 import {test} from "@playwright/test";
 import SignPinPage from "../../../src/modules/oms/signPin/signPin.page";
+import TerminalIDPage from "../../../src/modules/oms/terminalID/terminalID.page";
+
 
 test.describe.serial("Landing Page", () => {
 
     test.beforeEach(async ({page}) => {
-        let signPinPage = new SignPinPage(page);
-        await signPinPage.navigateHere()
+        let termianlIdPage = new TerminalIDPage(page);
+        await termianlIdPage.navigateHere()
+        await termianlIdPage.performTerminalID()
     })
 
     test("Validate Logic when User input valid/registered pin login",
