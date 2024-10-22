e4bc73ead6e2d4d281e68fbc9667ee38c04e66e3
safruddin.setia <safruddin.setia@esb.co.id>
2024-10-22 16:35:43 +0700

    [MOD] terminalID.page.ts

--- src/modules/oms/terminalID/terminalID.page.ts
+++ src/modules/oms/terminalID/terminalID.page.ts
@@ -2,30 +2,66 @@ import TerminalIDScenario from "./terminalID.scenario";
 import BaseOmsPage from "../base-oms-page";
 import Element from "../../../base/objects/Element";
 import TerminalIDLocator from "./terminalID.locator";
+import SignPinLocator from "../signPin/signPin.locator";
+import PromotionScenario from "../../pld/promotion/promotion.scenario";
 
 
 export default class TerminalIDPage extends BaseOmsPage implements TerminalIDScenario {
 
 
-    pageUrl = (): string => this.urls.get.terminalId.terminalList;
+    pageUrl = (): string => this.urls.get.generalPos.loginPage;
 
     // Real URL =
     shouldHave(): Element[] {
         return [
-            Element.ofSelector(TerminalIDLocator.terminalID01),
-            Element.ofSelector(TerminalIDLocator.terminalID02),
-            Element.ofSelector(TerminalIDLocator.terminalID03),
-            Element.ofSelector(TerminalIDLocator.terminalID04),
+            Element.ofSelector(TerminalIDLocator.getTerminalID(1)),
+            Element.ofSelector(TerminalIDLocator.getTerminalID(2)),
+            Element.ofSelector(TerminalIDLocator.getTerminalID(3)),
+            Element.ofSelector(TerminalIDLocator.getTerminalID(4)),
             Element.ofSelector(TerminalIDLocator.btnSyncTerminalID),
-            Element.ofSelector(TerminalIDLocator.btnApplyTerminalID),
-            Element.ofSelector(TerminalIDLocator.btnCancelTerminalID),
-            Element.ofSelector(TerminalIDLocator.btnClaimTerminalID),
-            Element.ofSelector(TerminalIDLocator.btnSaveTerminalID),
-            Element.ofSelector(TerminalIDLocator.btnInputNotesTerminalID),
-
 
         ];
     }
 
+    async selectTerminalID(): Promise<void> {
+        await this.expectVisible(TerminalIDLocator.getTerminalID(1));
+        await this.click(TerminalIDLocator.getTerminalID(1));
 
+    }
+
+    async applyTerminalID(): Promise<void>{
+        await this.expectVisible(TerminalIDLocator.btnApplyTerminalID);
+        await this.click(TerminalIDLocator.btnApplyTerminalID);
+    }
+
+    async claimTerminalID(): Promise<void>{
+        await this.expectVisible(TerminalIDLocator.btnClaimTerminalID);
+        await this.click(TerminalIDLocator.btnClaimTerminalID);
+
+    }
+    async inputNotesTerminalID(): Promise<void>{
+        await this.expectVisible(TerminalIDLocator.btnInputNotesTerminalID);
+        await this.click(TerminalIDLocator.btnInputNotesTerminalID);
+        await this.fill(TerminalIDLocator.btnInputNotesTerminalID, "Oms1");
+
+    }
+
+    async saveTerminalID(): Promise<void>{
+        await this.expectVisible(TerminalIDLocator.btnSaveTerminalID);
+        await this.click(TerminalIDLocator.btnSaveTerminalID);
+    }
+
+    async performTerminalID(): Promise<void>{
+        await this.expectVisible(TerminalIDLocator.getTerminalID(1));
+        await this.click(TerminalIDLocator.getTerminalID(1));
+        await this.expectVisible(TerminalIDLocator.btnApplyTerminalID);
+        await this.click(TerminalIDLocator.btnApplyTerminalID);
+        await this.expectVisible(TerminalIDLocator.btnClaimTerminalID);
+        await this.click(TerminalIDLocator.btnClaimTerminalID);
+        await this.expectVisible(TerminalIDLocator.btnInputNotesTerminalID);
+        await this.click(TerminalIDLocator.btnInputNotesTerminalID);
+        await this.fill(TerminalIDLocator.btnInputNotesTerminalID, "Oms1");
+        await this.expectVisible(TerminalIDLocator.btnSaveTerminalID);
+        await this.click(TerminalIDLocator.btnSaveTerminalID);
+    }
 }
