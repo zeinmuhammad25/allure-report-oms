fd9ca4840d7bad8b28e3ec2256bf6a4f4f3816a9
safruddin.setia <safruddin.setia@esb.co.id>
2024-10-24 13:07:26 +0700

    [ADD] startDayTC.spec.ts

---
+++ tests/oms/startDayTC/startDayTC.spec.ts
@@ -0,0 +1,33 @@
+import {test} from "@playwright/test";
+import SignPinPage from "../../../src/modules/oms/signPin/signPin.page";
+import TerminalIDPage from "../../../src/modules/oms/terminalID/terminalID.page";
+import StartDayPage from "../../../src/modules/oms/startDay/startDay.page";
+
+
+test.describe.serial("Start Day Test", () => {
+
+    test.beforeEach(async ({page}) => {
+        let terminalIdPage = new TerminalIDPage(page);
+        let signPinPage = new SignPinPage(page);
+
+        await terminalIdPage.navigateHere();
+        await terminalIdPage.performTerminalID();
+        await signPinPage.wait(2000)
+        await signPinPage.inputPinByTouch("22");
+        await signPinPage.submitPinValidateStartDayYes();
+    })
+
+    test("Validate Logic when User input number in Starting Cash",
+        {tag: "@smokeTest @oms @StartDay @positive"}, async ({page}) => {
+            let startDay = new StartDayPage(page);
+
+            await startDay.inputStartingCash();
+            await startDay.confirmStartingCash();
+            await startDay.notificationSuccessStartDay();
+
+
+        }
+    )
+
+
+})
