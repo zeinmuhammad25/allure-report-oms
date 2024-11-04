456a8ddebbff2e46c5e8bac9b14d59ba8382c971
safruddin.setia <safruddin.setia@esb.co.id>
2024-11-04 10:43:14 +0700

    [MOD] Specific parameter name

--- src/modules/oms/tableList/quickServiceList/quickServiceList.locator.ts
+++ src/modules/oms/tableList/quickServiceList/quickServiceList.locator.ts
@@ -4,6 +4,6 @@ export default class QuickServiceListLocator extends BaseLocator {
 
     static sectionQuickService: string = "//span[contains(text(),'QUICK SERVICE')]";
     static buttonAddQuickService: string = "//span[normalize-space()='Add Quick Service']";
-    static getLocatorPagination = (key: string): string => `//button[@aria-label='${key}']`;
+    static getLocatorPagination = (paginationControlKey: string): string => `//button[@aria-label='${paginationControlKey}']`;
 
 }
