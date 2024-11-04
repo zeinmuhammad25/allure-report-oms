a234c139545262cd1495f24d2600c36eda440333
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-04 11:43:42 +0700

    [MOD] REVISION locator adjust  element locator in linkTable component

--- src/modules/oms/components/linkTable/linkTable.component.ts
+++ src/modules/oms/components/linkTable/linkTable.component.ts
@@ -11,16 +11,17 @@ export default class LinkTableComponent extends BaseOmsPage implements LinkTable
             Element.ofSelector(LinkTableLocator.buttonLinkTable),
             Element.ofSelector(LinkTableLocator.buttonNextPage),
             Element.ofSelector(LinkTableLocator.buttonBackPage),
-            Element.ofSelector(LinkTableLocator.applyOrCancelButton("Apply")),
-            Element.ofSelector(LinkTableLocator.applyOrCancelButton("Cancel")),
-            Element.ofSelector(LinkTableLocator.tableAc1),
-            Element.ofSelector(LinkTableLocator.tableAc2),
-            Element.ofSelector(LinkTableLocator.tableAc3),
-            Element.ofSelector(LinkTableLocator.tableAc4),
-            Element.ofSelector(LinkTableLocator.tableSr1),
-            Element.ofSelector(LinkTableLocator.tableSr2),
-            Element.ofSelector(LinkTableLocator.tableSr3),
-            Element.ofSelector(LinkTableLocator.tableSr4)
+            Element.ofSelector(LinkTableLocator.applyButton),
+            Element.ofSelector(LinkTableLocator.CancelButton),
+            Element.ofSelector(LinkTableLocator.selectTableList('AC 1')),
+            Element.ofSelector(LinkTableLocator.selectTableList('AC 2')),
+            Element.ofSelector(LinkTableLocator.selectTableList('AC 3')),
+            Element.ofSelector(LinkTableLocator.selectTableList('AC 4')),
+            Element.ofSelector(LinkTableLocator.selectTableList('SR 1')),
+            Element.ofSelector(LinkTableLocator.selectTableList('SR 2')),
+            Element.ofSelector(LinkTableLocator.selectTableList('SR 3')),
+            Element.ofSelector(LinkTableLocator.selectTableList('SR 4')),
+
         ];
 
     }
@@ -28,32 +29,32 @@ export default class LinkTableComponent extends BaseOmsPage implements LinkTable
     async userSingleLinkTable(): Promise<void> {
         await this.click(LinkTableLocator.buttonLinkTable);
         await this.expectVisible(LinkTableLocator.popupPage);
-        if (await this.isInvisible(LinkTableLocator.tableAc1)) {
-            await this.click(LinkTableLocator.tableAc1);
-            await this.click(LinkTableLocator.applyOrCancelButton("Apply"));
-        } else if (await this.isInvisible(LinkTableLocator.tableAc2)) {
-            await this.click(LinkTableLocator.tableAc2);
-            await this.click(LinkTableLocator.applyOrCancelButton("Apply"));
-        } else if (await this.isInvisible(LinkTableLocator.tableAc3)) {
-            await this.click(LinkTableLocator.tableAc3);
-            await this.click(LinkTableLocator.applyOrCancelButton("Apply"));
-        } else if (await this.isInvisible(LinkTableLocator.tableAc4)) {
-            await this.click(LinkTableLocator.tableAc4);
-            await this.click(LinkTableLocator.applyOrCancelButton("Apply"));
-        } else if (await this.isInvisible(LinkTableLocator.tableSr1)) {
-            await this.click(LinkTableLocator.tableSr1);
-            await this.click(LinkTableLocator.applyOrCancelButton("Apply"));
-        } else if (await this.isInvisible(LinkTableLocator.tableSr2)) {
-            await this.click(LinkTableLocator.tableSr2);
-            await this.click(LinkTableLocator.applyOrCancelButton("Apply"));
-        } else if (await this.isInvisible(LinkTableLocator.tableSr3)) {
-            await this.click(LinkTableLocator.tableSr3);
-            await this.click(LinkTableLocator.applyOrCancelButton("Apply"));
-        } else if (await this.isInvisible(LinkTableLocator.tableSr4)) {
-            await this.click(LinkTableLocator.tableSr4);
-            await this.click(LinkTableLocator.applyOrCancelButton("Apply"));
+        if (await this.isInvisible(LinkTableLocator.selectTableList('AC 1'))) {
+            await this.click(LinkTableLocator.selectTableList('AC 1'));
+            await this.click(LinkTableLocator.applyButton);
+        } else if (await this.isInvisible(LinkTableLocator.selectTableList('AC 2'))) {
+            await this.click(LinkTableLocator.selectTableList('AC 2'));
+            await this.click(LinkTableLocator.CancelButton);
+        } else if (await this.isInvisible(LinkTableLocator.selectTableList('AC 3'))) {
+            await this.click(LinkTableLocator.selectTableList('AC 3'));
+            await this.click(LinkTableLocator.applyButton);
+        } else if (await this.isInvisible(LinkTableLocator.selectTableList('AC 4'))) {
+            await this.click(LinkTableLocator.selectTableList('AC 4'));
+            await this.click(LinkTableLocator.applyButton);
+        } else if (await this.isInvisible(LinkTableLocator.selectTableList('SR 1'))) {
+            await this.click(LinkTableLocator.selectTableList('SR 1'));
+            await this.click(LinkTableLocator.applyButton);
+        } else if (await this.isInvisible(LinkTableLocator.selectTableList('SR 2'))) {
+            await this.click(LinkTableLocator.selectTableList('SR 2'));
+            await this.click(LinkTableLocator.applyButton);
+        } else if (await this.isInvisible(LinkTableLocator.selectTableList('SR 3'))) {
+            await this.click(LinkTableLocator.selectTableList('SR 3'));
+            await this.click(LinkTableLocator.applyButton);
+        } else if (await this.isInvisible(LinkTableLocator.selectTableList('SR 4'))) {
+            await this.click(LinkTableLocator.selectTableList('SR 4'));
+            await this.click(LinkTableLocator.applyButton);
         } else {
-            await this.click(LinkTableLocator.applyOrCancelButton("Cancel"));
+            await this.click(LinkTableLocator.CancelButton);
         }
 
     }
