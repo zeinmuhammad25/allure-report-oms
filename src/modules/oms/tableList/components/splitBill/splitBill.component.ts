80abc8ba9f2b6851aa76b085a22407acdafebd0a
alvin <alvin.tutupary@esb.co.id>
2024-11-04 10:17:25 +0700

    [ADD] Split Bill Component

--- src/modules/oms/tableList/components/splitBill/splitBill.component.ts
+++ src/modules/oms/tableList/components/splitBill/splitBill.component.ts
@@ -7,6 +7,28 @@ export default class SplitBillComponent extends BaseOmsPage implements SplitBill
     pageUrl: () => string;
 
     shouldHave(): Element[] {
+        return [];
+    }
+
+    async setMainBillName(billName: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    async addBill(billName: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    async deleteBill(billName: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    async selectBill(billName: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    async moveMenu(toBillName: string, menuName: string, menuQty: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    async returnMenu(billName: string, menuName: string): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    async closeSplitBill(): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
