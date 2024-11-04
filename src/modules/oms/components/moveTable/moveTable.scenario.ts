a81eee53de1a79d4d6536407ee2b4d8255ac8e80
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-04 21:35:55 +0700

    [ADD]scenario in file moveTable scenario

---
+++ src/modules/oms/components/moveTable/moveTable.scenario.ts
@@ -0,0 +1,7 @@
+import BaseScenario from "../../../../base/base-scenario";
+
+export default interface MoveTableScenario extends BaseScenario {
+    userSelectMoveTableToTable(): Promise<void>;
+
+    userSelectMoveQuickServiceToTable(): Promise<void>;
+}
