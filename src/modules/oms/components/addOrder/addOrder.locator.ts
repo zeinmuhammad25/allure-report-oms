b7e8d8eae2fa9fcbe99aeef3336944c0cb6c228a
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-05 18:17:12 +0700

    [MOD]locator name in file Add locator

--- src/modules/oms/components/addOrder/addOrder.locator.ts
+++ src/modules/oms/components/addOrder/addOrder.locator.ts
@@ -9,8 +9,8 @@ export default class AddOrderLocator extends BaseLocator {
     static backPageItemList: string = "//div[@class='d-flex']//i[@class='glyphicon glyphicon-arrow-left']";
     private static buttonModifier = (menuName: string, icon: string): string =>
         `//div[span[normalize-space() = '${menuName}']]/following-sibling::div//i[@class='glyphicon glyphicon-${icon}']`;
-    static addOrderLocatorButtonNext: string = `//span[normalize-space()='Next']`;
-    static addOrderLocatorButtonBack: string = `//span[normalize-space()='Back']`;
+    static buttonNext: string = `//span[normalize-space()='Next']`;
+    static buttonBack: string = `//span[normalize-space()='Back']`;
     static applyButton: string = "//span[normalize-space()='Apply']";
     static cancelButton: string = "//div[@class='modal-footer ng-star-inserted']//button[@type='button']//span[1]";
 
