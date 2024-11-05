b26a596a84e9ccf043f5a0803461511146c24aef
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-05 17:52:12 +0700

    [MOD]locator in file Add Order locator

--- src/modules/oms/components/addOrder/addOrder.locator.ts
+++ src/modules/oms/components/addOrder/addOrder.locator.ts
@@ -7,14 +7,11 @@ export default class AddOrderLocator extends BaseLocator {
     static refreshButton: string = "//i[@class='glyphicon glyphicon-repeat']";
     static nextPageItemList: string = "//div[@class='d-flex']//i[@class='glyphicon glyphicon-arrow-right']";
     static backPageItemList: string = "//div[@class='d-flex']//i[@class='glyphicon glyphicon-arrow-left']";
-    static buttonQtyPlusByMenu = (menuName: string): string =>
-        `//div[span[normalize-space() = '${menuName}']]/following-sibling::div//i[@class='glyphicon glyphicon-plus']`;
-    static buttonQtyMinusByMenu = (menuName: string): string =>
-        `//div[span[normalize-space() = '${menuName}']]/following-sibling::div//i[@class='glyphicon glyphicon-minus']`;
-    static buttonAddNotesByMenu = (menuName: string): string =>
-        `//div[span[normalize-space() = '${menuName}']]/following-sibling::div//i[@class='glyphicon glyphicon-pencil']`;
-    static buttonNextOrBackInPopupAddOrder = (Label: "Next" | "Back"): string => `//span[normalize-space()='${Label}']`;
+    private static buttonModifier = (menuName: string, icon: string): string =>
+        `//div[span[normalize-space() = '${menuName}']]/following-sibling::div//i[@class='glyphicon glyphicon-${icon}']`;
+    static addOrderLocatorButtonNext: string = `//span[normalize-space()='Next']`;
+    static addOrderLocatorButtonBack: string = `//span[normalize-space()='Back']`;
     static applyButton: string = "//span[normalize-space()='Apply']";
-    static CancelButton: string = "//div[@class='modal-footer ng-star-inserted']//button[@type='button']//span[1]";
+    static cancelButton: string = "//div[@class='modal-footer ng-star-inserted']//button[@type='button']//span[1]";
 
 }
