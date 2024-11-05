4b983afaf35d0576c1270b953e609ea402384e42
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-05 17:51:57 +0700

    [MOD]component in file Add Order component

--- src/modules/oms/components/addOrder/addOrder.component.ts
+++ src/modules/oms/components/addOrder/addOrder.component.ts
@@ -1,67 +1,45 @@
 import Element from "../../../../base/objects/Element";
 import BaseOmsPage from "../../base-oms-page";
 import AddOrderLocator from "./addOrder.locator";
-import addOrderScenario from "./addOrder.scenario";
+import AddOrderScenario from "./addOrder.scenario";
 
-export default class AddOrderComponent extends BaseOmsPage implements addOrderScenario {
+export default class AddOrderComponent extends BaseOmsPage implements AddOrderScenario {
     pageUrl: () => string;
 
     shouldHave(): Element[] {
         return [
-            Element.ofSelector(AddOrderLocator.buttonPackage("ISIAN PAKET MAHAL")),
             Element.ofSelector(AddOrderLocator.filedSearch),
             Element.ofSelector(AddOrderLocator.refreshButton),
             Element.ofSelector(AddOrderLocator.nextPageItemList),
             Element.ofSelector(AddOrderLocator.backPageItemList),
-            Element.ofSelector(AddOrderLocator.buttonQtyPlusByMenu("[21+] Bombay Sapphire Dry Gin")),
-            Element.ofSelector(AddOrderLocator.buttonQtyPlusByMenu("[21+] Gilbeys Whisky 350ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyPlusByMenu("[21+] Sababay White Velvet 750")),
-            Element.ofSelector(AddOrderLocator.buttonQtyPlusByMenu("[21+] Batavia Blended 700ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyPlusByMenu("[21+] Baileys Original 700ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyPlusByMenu("[21+] Captain Morgan 200ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyPlusByMenu("[21+] Iceland Vodka 250ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyMinusByMenu("[21+] Bombay Sapphire Dry Gin")),
-            Element.ofSelector(AddOrderLocator.buttonQtyMinusByMenu("[21+] Gilbeys Whisky 350ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyMinusByMenu("[21+] Sababay White Velvet 750")),
-            Element.ofSelector(AddOrderLocator.buttonQtyMinusByMenu("[21+] Batavia Blended 700ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyMinusByMenu("[21+] Baileys Original 700ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyMinusByMenu("[21+] Captain Morgan 200ml")),
-            Element.ofSelector(AddOrderLocator.buttonQtyMinusByMenu("[21+] Iceland Vodka 250ml")),
-            Element.ofSelector(AddOrderLocator.buttonAddNotesByMenu("[21+] Bombay Sapphire Dry Gin")),
-            Element.ofSelector(AddOrderLocator.buttonAddNotesByMenu("[21+] Gilbeys Whisky 350ml")),
-            Element.ofSelector(AddOrderLocator.buttonAddNotesByMenu("[21+] Sababay White Velvet 750")),
-            Element.ofSelector(AddOrderLocator.buttonAddNotesByMenu("[21+] Batavia Blended 700ml")),
-            Element.ofSelector(AddOrderLocator.buttonAddNotesByMenu("[21+] Baileys Original 700ml")),
-            Element.ofSelector(AddOrderLocator.buttonAddNotesByMenu("[21+] Captain Morgan 200ml")),
-            Element.ofSelector(AddOrderLocator.buttonAddNotesByMenu("[21+] Iceland Vodka 250ml")),
-            Element.ofSelector(AddOrderLocator.buttonNextOrBackInPopupAddOrder("Next")),
-            Element.ofSelector(AddOrderLocator.buttonNextOrBackInPopupAddOrder("Back")),
+            Element.ofSelector(AddOrderLocator.addOrderLocatorButtonNext),
+            Element.ofSelector(AddOrderLocator.addOrderLocatorButtonBack),
             Element.ofSelector(AddOrderLocator.applyButton),
-            Element.ofSelector(AddOrderLocator.CancelButton)
+            Element.ofSelector(AddOrderLocator.cancelButton)
         ];
     }
 
-    async userAddOneMenuDetailPackage(): Promise<void> {
+    async addOneMenuDetailPackage(): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
-    async userAddMultiMenuDetailPackage(): Promise<void> {
+    async addMultiMenuDetailPackage(): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
-    async userAddNotesMenuDetailPackage(): Promise<void> {
+    async addNotesMenuDetailPackage(): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
-    async userMinusQtyInDetailMenuPackage(): Promise<void> {
+    async minusQtyInDetailMenuPackage(): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
-    async userAddMakQtyInOneMenuDetailPackage(): Promise<void> {
+    async addMaxQtyInOneMenuDetailPackage(): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
-    async useraddMenuDetailPackageViaSearch(): Promise<void> {
+    async addMenuDetailPackageViaSearch(): Promise<void> {
         throw new Error("Method not implemented.");
     }
 
