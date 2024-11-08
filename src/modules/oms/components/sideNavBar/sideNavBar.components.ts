1df3c16524276950e5fb2a1d1426bfe37dba5c5e
Safruddin Setia Budi <safruddin.budi@esb.co.id>
2024-11-08 09:20:40 +0000

    ADD Function implementation sideNavBar

--- src/modules/oms/components/sideNavBar/sideNavBar.components.ts
+++ src/modules/oms/components/sideNavBar/sideNavBar.components.ts
@@ -8,68 +8,101 @@ export default class SideNavBarComponents extends BaseOmsPage implements SideNav
 
     shouldHave(): Element[] {
         return [
-            Element.ofSelector(SideNavBarLocator.GetLocatorHideNav("/table")),
-            Element.ofSelector(SideNavBarLocator.GetLocatorHideNav("/ezo-delivery")),
-            Element.ofSelector(SideNavBarLocator.GetLocatorHideNav("/ezo-delivery-booking"))
+            Element.ofSelector(SideNavBarLocator.getLocatorHideNavBar("/table")),
+            Element.ofSelector(SideNavBarLocator.getLocatorHideNavBar("/ezo-delivery")),
+            Element.ofSelector(SideNavBarLocator.getLocatorHideNavBar("/ezo-delivery-booking"))
         ];
     }
 
     async gotoPageTableList(): Promise<void> {
-        throw new Error("Method not implemented");
-
+        await this.expectVisible(SideNavBarLocator.getLocatorHideNavBar("/table"));
+        await this.click(SideNavBarLocator.getLocatorHideNavBar("/table"));
+        await this.waitForResponse("/table");
     }
 
     async gotoPageEsbOrderDashboard(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.getLocatorHideNavBar("/ezo-delivery"));
+        await this.click(SideNavBarLocator.getLocatorHideNavBar("/ezo-delivery"));
+        await this.waitForResponse("/ezo-delivery-pos");
 
     }
 
     async gotoPageEsbOrderDeliveryHistory(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.getLocatorHideNavBar("/ezo-delivery-booking"));
+        await this.click(SideNavBarLocator.getLocatorHideNavBar("/ezo-delivery-booking"));
+        await this.waitForResponse("/ezo-delivery-pos");
     }
 
     async gotoPagePickupDeliveryDashboard(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.getLocatorHideNavBar("/pickup-delivery-dashboard"));
+        await this.click(SideNavBarLocator.getLocatorHideNavBar("/pickup-delivery-dashboard"));
+        await this.waitForResponse("/ezo-delivery-pos");
     }
 
     async gotoPageSalesRecapitulation(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.getLocatorHideNavBar("/sales"));
+        await this.click(SideNavBarLocator.getLocatorHideNavBar("/sales"));
+        await this.waitForResponse("/sales");
     }
 
     async gotoPageRegularMember(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.getLocatorHideNavBar("/member"));
+        await this.click(SideNavBarLocator.getLocatorHideNavBar("/member"));
+        await this.waitForResponse("/member");
     }
 
     async gotoPageRegularMemberDeposit(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.getLocatorHideNavBar("/member-deposit"));
+        await this.click(SideNavBarLocator.getLocatorHideNavBar("/member-deposit"));
+        await this.waitForResponse("/deposit");
     }
 
     async gotoPageTools(): Promise<void> {
-        throw new Error("Method not implemented");
-    }
-
-    async gotoPageOthers(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.getLocatorHideNavBar("/tools"));
+        await this.click(SideNavBarLocator.getLocatorHideNavBar("/tools"));
+        await this.expectTextVisible("Troubleshoot", true);
+        await this.expectTextVisible("Application Setting", true);
+        await this.expectTextVisible("Synchronize Data", true);
     }
 
     async gotoPageRegularMemberWithdrawal(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.hideNavBarOthers);
+        await this.click(SideNavBarLocator.hideNavBarOthers);
+        await this.expectVisible(SideNavBarLocator.menuSectionOther("Regular Member Withdrawal"));
+        await this.click(SideNavBarLocator.menuSectionOther("Regular Member Withdrawal"));
+        await this.waitForResponse("/withdrawal");
     }
 
     async gotoPageDayStartEnd(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.hideNavBarOthers);
+        await this.click(SideNavBarLocator.hideNavBarOthers);
+        await this.expectVisible(SideNavBarLocator.menuSectionOther("Day Start / End"));
+        await this.click(SideNavBarLocator.menuSectionOther("Day Start / End"));
+        await this.waitForResponse("/current");
     }
 
     async gotoPageDayShiftLog(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.hideNavBarOthers);
+        await this.click(SideNavBarLocator.hideNavBarOthers);
+        await this.expectVisible(SideNavBarLocator.menuSectionOther("Shift Log"));
+        await this.click(SideNavBarLocator.menuSectionOther("Shift Log"));
+        await this.waitForResponse("/shift");
     }
 
     async gotoPageBranchMenu(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.hideNavBarOthers);
+        await this.click(SideNavBarLocator.hideNavBarOthers);
+        await this.expectVisible(SideNavBarLocator.menuSectionOther("Branch Menu"));
+        await this.click(SideNavBarLocator.menuSectionOther("Branch Menu"));
+        await this.waitForResponse("branch-menu");
     }
 
     async gotoPageStation(): Promise<void> {
-        throw new Error("Method not implemented");
+        await this.expectVisible(SideNavBarLocator.hideNavBarOthers);
+        await this.click(SideNavBarLocator.hideNavBarOthers);
+        await this.expectVisible(SideNavBarLocator.menuSectionOther("Station"));
+        await this.click(SideNavBarLocator.menuSectionOther("Station"));
+        await this.expectTextVisible("Station Management", true);
     }
 
 }
