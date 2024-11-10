40fcb9d0061b15bbe878e024f79e4bf4d378fbfb
safruddin.setia <safruddin.setia@esb.co.id>
2024-11-10 11:36:31 +0700

    [MOD] locator paymentPOS.locator -locator action payment to function getLocatorButtonAction -locator action arrow to function getLocatorButtonArrow

--- src/modules/oms/tableList/payment/paymentPOS.locator.ts
+++ src/modules/oms/tableList/payment/paymentPOS.locator.ts
@@ -1,18 +1,18 @@
 import BaseLocator from "../../../../base/base-locator";
-import {PaymentList} from "./PaymentList";
+import {PaymentObject} from "./PaymentObject";
+
 
 export default class PaymentPOSLocator extends BaseLocator {
     static buttonApplyMember = "//mat-icon[normalize-space()='account_box']";
     static buttonCheckOnlinePayment = "//mat-icon[normalize-space()='money']";
     static buttonEmployeeMode = "//i[@class='glyphicon glyphicon-credit-card']";
     static buttonAddPromo = "//span[contains(text(),'ADD PROMO')]";
-    static buttonPurchaseVoucher = "//span[normalize-space()='Purchase Voucher']";
-    static buttonSavePayment = "//span[normalize-space()='Save Payment']";
-    static buttonCancelPayment = "//span[normalize-space()='Cancel']";
-    static buttonApplyPayment = "//span[normalize-space()='Apply']";
-    static getLocatorButtonArrow = (label: string): string => `//i[@class='glyphicon glyphicon-arrow-${label}']`;
-    static getLocatorPaymentType = (paymentType: PaymentList): string => `//span[contains(text(),'${paymentType}')]`;
-    static getLocatorPaymentMethod = (paymentMethod: PaymentList): string => `//span[contains(text(),'${paymentMethod}')]`;
+    static escapeKeyboard = "//div[@class='modal-header bg-primary']";
+
+    static getLocatorButtonAction = (actionPayment: PaymentObject): string => `//span[normalize-space()='${actionPayment}']`;
+    static getLocatorButtonArrow = (actionArrow: PaymentObject): string => `//i[@class='glyphicon glyphicon-arrow-${actionArrow}']`;
+    static getLocatorPaymentType = (paymentType: PaymentObject): string => `//span[contains(text(),'${paymentType}')]`;
+    static getLocatorPaymentMethod = (paymentMethod: PaymentObject): string => `//span[contains(text(),'${paymentMethod}')]`;
 
     //sectionPaymentCash
     static buttonPayCashFullAmount = "//div[@class='d-flex ng-star-inserted']//span[1]//button[1]";
