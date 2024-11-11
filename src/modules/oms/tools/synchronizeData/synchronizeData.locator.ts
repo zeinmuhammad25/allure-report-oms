cc9e807b11fa2b94743bf2d72c392b3db4b5ecce
safruddin.setia <safruddin.setia@esb.co.id>
2024-11-11 11:29:45 +0700

    [MOD] variable name

--- src/modules/oms/tools/synchronizeData/synchronizeData.locator.ts
+++ src/modules/oms/tools/synchronizeData/synchronizeData.locator.ts
@@ -1,11 +1,11 @@
 import BaseLocator from "../../../../base/base-locator";
+import {SynchronizeDataObject} from "./SynchronizeDataObject";
 
 export default class SynchronizeDataLocator extends BaseLocator {
 
-    static getLocatorSynchronize = (key: string): string => `//div[normalize-space()='${key}`;
+    static getLocatorSynchronize = (actionSync: SynchronizeDataObject): string => ` //div[normalize-space()='${actionSync}']`;
     static buttonUpdateSetting: string = "//span[normalize-space()='Update Setting']";
     static buttonSynchronize: string = "//span[normalize-space()='Synchronize']";
     static buttonSelectAll: string = "//label[@for='mat-checkbox-6-input']//span[@class='mat-checkbox-label']";
-
 
 }
