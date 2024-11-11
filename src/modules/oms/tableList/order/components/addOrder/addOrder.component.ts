61d0cecd14ff4e856ddc085501c078134ad2fa2b
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-11 22:07:39 +0700

    [mod] replace previous logic and ad new logic in addOrder component

--- src/modules/oms/tableList/order/components/addOrder/addOrder.component.ts
+++ src/modules/oms/tableList/order/components/addOrder/addOrder.component.ts
@@ -2,6 +2,7 @@ import Element from "../../../../../../base/objects/Element";
 import BaseOmsPage from "../../../../base-oms-page";
 import AddOrderLocator from "./addOrder.locator";
 import AddOrderScenario from "./addOrder.scenario";
+import AddMenuModel from "../../addMenu.model";
 
 
 export default class AddOrderComponent extends BaseOmsPage implements AddOrderScenario {
@@ -20,101 +21,46 @@ export default class AddOrderComponent extends BaseOmsPage implements AddOrderSc
         ];
     }
 
-    async addOneMenuDetailPackage(menuName:string,qty:number): Promise<void> {
-        await this.waitForResponse("/get-menu-package");
+    private async menuModifier(menuOrder: AddMenuModel): Promise<void> {
 
-        for (let i = 0; i < qty; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu(menuName));
+        let qty: number = menuOrder.qty == "max" ? 10 : menuOrder.qty;
+        if (qty < 0) {
+            qty = qty * -1;
+
+            for (let i = 0; i < qty; i++) {
+                await this.click(AddOrderLocator.buttonQtyMinusByMenu(menuOrder.menuName));
+            }
+        } else {
+            for (let i = 0; i < qty; i++) {
+                await this.click(AddOrderLocator.buttonQtyPlusByMenu(menuOrder.menuName));
+            }
         }
-        await this.click(AddOrderLocator.applyButton);
 
+        if (menuOrder.notes != null && menuOrder.notes != "") {
+            await this.click(AddOrderLocator.buttonAddNotesByMenu(menuOrder.menuName));
+            await this.click(AddOrderLocator.fieldNotes);
+            await this.fill(AddOrderLocator.fieldNotes, menuOrder.notes);
+            await this.expectVisible(AddOrderLocator.popUpNotes);
+            await this.click(AddOrderLocator.popUpNotes);
+            await this.expectVisible(AddOrderLocator.buttonApplyNotes);
+            await this.click(AddOrderLocator.buttonApplyNotes);
+        }
     }
 
-    async addMultiMenuDetailPackage(): Promise<void> {
+    async modifyMenuDetailPackage(menuOrder: AddMenuModel[]): Promise<void> {
         await this.waitForResponse("/get-menu-package");
-        const clickCountPlus = 4;
-        for (let i = 0; i < clickCountPlus; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu("[21+] Captain Morgan 200ml"));
+        for (let i = 0; i < menuOrder.length; i++) {
+            await this.menuModifier(menuOrder[i]);
         }
-        const clickCountPlus1 = 2;
-        for (let i = 0; i < clickCountPlus1; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu("[21+] Batavia Blended 700ml"));
-        }
-        const clickCountPlus2 = 2;
-        for (let i = 0; i < clickCountPlus2; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu("[21+] Baileys Original 700ml"));
-        }
-        const clickCountPlus3 = 2;
-        for (let i = 0; i < clickCountPlus3; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu("[21+] Iceland Vodka 250ml"));
-        }
-        await this.click(AddOrderLocator.applyButton);
     }
 
-    // async inputNotes(notes: string): Promise<void> {
-    //     await this.isVisible(AddOrderLocator.filedNotes);
-    //     await this.click(AddOrderLocator.filedNotes);
-    //     await this.fill(AddOrderLocator.filedNotes, notes);
-    //     await this.click(AddOrderLocator.buttonApplyNotes)
-    //
-    // }
-
-    async addNotesMenuDetailPackage(menuName:string,qty:number): Promise<void> {
-        await this.waitForResponse("/get-menu-package");
-        for (let i = 0; i < qty; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu(menuName));
-        }
-        await this.expectVisible(AddOrderLocator.buttonAddNotesByMenu(menuName));
-        await this.click(AddOrderLocator.buttonAddNotesByMenu(menuName));
-        await this.expectVisible(AddOrderLocator.fieldNotes);
-        await this.click(AddOrderLocator.fieldNotes);
-        await this.fill(AddOrderLocator.fieldNotes,"test")
-        await this.expectVisible(AddOrderLocator.popUpNotes)
-        await this.click(AddOrderLocator.popUpNotes)
-        await this.expectVisible(AddOrderLocator.buttonApplyNotes);
-        await this.click(AddOrderLocator.buttonApplyNotes);
-
+    async applyMenuDetailPackage(): Promise<void> {
+        await this.expectVisible(AddOrderLocator.applyButton);
+        await this.click(AddOrderLocator.cancelButton);
     }
 
-    async minusQtyInDetailMenuPackage(menuName:string,qty:number): Promise<void> {
-        await this.waitForResponse("/get-menu-package");
-        for (let i = 0; i < qty; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu(menuName));
-        }
-        await this.wait(2000);
-        for (let i = 0; i < qty; i++) {
-            await this.click(AddOrderLocator.buttonQtyMinusByMenu(menuName));
-        }
-
-        await this.click(AddOrderLocator.applyButton);
-    }
-
-    async addMaxQtyInOneMenuDetailPackage(menuName:string,qty:number): Promise<void> {
-        await this.waitForResponse("/get-menu-package");
-        for (let i = 0; i < qty; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu(menuName));
-        }
-        await this.click(AddOrderLocator.applyButton);
-    }
-
-    async cancelAddMenuDetailPackage(menuName:string,qty:number): Promise<void> {
-        await this.waitForResponse("/get-menu-package");
-        const clickCountPlus = 4;
-        for (let i = 0; i < clickCountPlus; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu("[21+] Captain Morgan 200ml"));
-        }
-        const clickCountPlus1 = 2;
-        for (let i = 0; i < clickCountPlus1; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu("[21+] Batavia Blended 700ml"));
-        }
-        const clickCountPlus2 = 2;
-        for (let i = 0; i < clickCountPlus2; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu("[21+] Baileys Original 700ml"));
-        }
-        const clickCountPlus3 = 2;
-        for (let i = 0; i < clickCountPlus3; i++) {
-            await this.click(AddOrderLocator.buttonQtyPlusByMenu("[21+] Iceland Vodka 250ml"));
-        }
+    async cancelMenuDetailPackage(): Promise<void> {
+        await this.expectVisible(AddOrderLocator.cancelButton);
         await this.click(AddOrderLocator.cancelButton);
     }
 
