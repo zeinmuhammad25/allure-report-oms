8fca4099042d62bc448b7942a6d3890198dae92a
alvin <alvin.tutupary@esb.co.id>
2024-11-12 15:15:46 +0700

    [MOD] update book order

--- src/modules/oms/tableList/components/bookOrder/bookOrder.locator.ts
+++ src/modules/oms/tableList/components/bookOrder/bookOrder.locator.ts
@@ -1,13 +1,15 @@
 import BaseLocator from "../../../../../base/base-locator";
 
 export default class BookOrderLocator extends BaseLocator {
-    static buttonPax = (numberOfPax: number): string => `//span[@class='mat-button-wrapper'][text()='${numberOfPax}']`;
-    static buttonSalesMode = (salesModeName: string): string => `//button[contains(@class,'btn-vp')]//span[contains(text(),'${salesModeName}')]`;
-    static fieldNumberOfPax = "(//app-number-input//input)[1]";
-    static fieldOrderTimeOut = "(//app-number-input//input)[2]";
-    static buttonBookTable = "//div[@class='ng-star-inserted']//button/span[contains(text(),'Book Table')]";
-    static buttonBookOrder = "//div[@class='ng-star-inserted']//button/span[contains(text(),'Book & Order')]";
-    static phoneFieldCustomerData = "//input[@placeholder='8xx xxx xxx']";
-    static buttonLaterCustomerData = "//button[normalize-space()='Later']";
-    static buttonApplyCustomerData = "//button[normalize-space()='Apply']";
+    static paxButton = (numberOfPax: number): string => `//span[@class='mat-button-wrapper'][text()='${numberOfPax}']`;
+    static salesModeButton = (salesModeName: string): string => `//button[contains(@class,'btn-vp')]//span[contains(text(),'${salesModeName}')]`;
+    static numberOfPaxField = "(//app-number-input//input)[1]";
+    static orderTimeOutField = "(//app-number-input//input)[2]";
+    static bookTableButton = "//div[@class='ng-star-inserted']//button/span[contains(text(),'Book Table')]";
+    static bookOrderButton = "//div[@class='ng-star-inserted']//button/span[contains(text(),'Book & Order')]";
+    static bookTableTab = "//app-table-open//button[normalize-space()='Book Table']";
+    static bookingListTab = "//app-table-open//button[normalize-space()='Booking List']";
+    static customerDataPhoneField = "//app-sales-contact-info//input[@placeholder='8xx xxx xxx']";
+    static laterCustomerDataButton = "//app-sales-contact-info//button[normalize-space()='Later']";
+    static CustomerDataApplyButton = "//app-sales-contact-info//button[normalize-space()='Apply']";
 }
