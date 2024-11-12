99093ed96643e5d629a2c1fd11442cd9213523cf
alvin <alvin.tutupary@esb.co.id>
2024-11-12 15:17:03 +0700

    [MOD] update promotion list

--- src/modules/oms/tableList/components/promotionList/promotionList.component.ts
+++ src/modules/oms/tableList/components/promotionList/promotionList.component.ts
@@ -1,30 +1,39 @@
 import PromotionListScenario from "./promotionList.scenario";
 import BaseOmsPage from "../../../base-oms-page";
 import Element from "../../../../../base/objects/Element";
+import PromotionListLocator from "./promotionList.locator";
 
 
 export default class PromotionListComponent extends BaseOmsPage implements PromotionListScenario {
     pageUrl: () => string;
 
     shouldHave(): Element[] {
-        return [];
+        return [
+            Element.ofSelector(PromotionListLocator.paginationButton("first")),
+            Element.ofSelector(PromotionListLocator.paginationButton("last")),
+            Element.ofSelector(PromotionListLocator.paginationButton("next")),
+            Element.ofSelector(PromotionListLocator.paginationButton("previous"))
+        ];
     }
 
     async searchPromotion(keyword: string): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.expectVisible(PromotionListLocator.searchPromoField);
+        await this.fill(PromotionListLocator.searchPromoField, keyword);
     }
 
     async selectPromotion(promotionID: string): Promise<void> {
-        throw new Error("Method not implemented.");
+        // No Data need more data to simulate element
     }
 
     async selectPromotionType(promotionType: string): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.expectVisible(PromotionListLocator.promotionTypeDropdown);
+        await this.click(PromotionListLocator.promotionTypeDropdown);
+        await this.expectVisible(PromotionListLocator.promotionTypeOption(promotionType));
+        await this.click(PromotionListLocator.promotionTypeOption(promotionType));
+        await this.click(PromotionListLocator.applyButton);
     }
 
     async gotoPromotionPage(type: "first" | "previous" | "next" | "last"): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.click(PromotionListLocator.paginationButton(type));
     }
-
-
 }
