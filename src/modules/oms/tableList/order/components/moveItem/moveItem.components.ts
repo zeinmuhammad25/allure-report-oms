897318bd88ef22f2d2502247d04d398602ceb38f
safruddin.setia <safruddin.setia@esb.co.id>
2024-11-12 11:24:58 +0700

    [ADD] Function implement related-moveItem: moveSelectAllItemMenu, moveAllMenu, actionApplyMoveItem, actionVerifyMenu, movePartialItemMenu

--- src/modules/oms/tableList/order/components/moveItem/moveItem.components.ts
+++ src/modules/oms/tableList/order/components/moveItem/moveItem.components.ts
@@ -9,12 +9,45 @@ export default class MoveItemComponents extends BaseOmsPage implements MoveItemS
         return [];
     }
 
-    async moveAllMenu(): Promise<void> {
-        throw new Error("Method not implemented.");
+    async moveSelectAllItemMenu(): Promise<void> {
+        await this.expectVisible(MoveItemLocator.buttonSelectAll);
+        await this.click(MoveItemLocator.buttonSelectAll);
     }
 
-    async movePartialItemMenu(): Promise<void> {
-        throw new Error("Method not implemented.");
+    async moveAllMenu(menuName: string): Promise<void> {
+        await this.expectVisible(MoveItemLocator.buttonMoveAll(menuName));
+        await this.click(MoveItemLocator.buttonMoveAll(menuName));
+    }
+
+    async actionApplyMoveItem(): Promise<void> {
+        await this.expectVisible(MoveItemLocator.getLocatorButtonActionFooter("Apply"));
+        await this.click(MoveItemLocator.getLocatorButtonActionFooter("Apply"));
+        await this.click(MoveItemLocator.escapeKeyboard);
+        await this.click(MoveItemLocator.buttonApplyBookTable);
+    }
+
+    async actionVerifyMenu(menuName: string): Promise<void> {
+        await this.wait(5000);
+        const buttonYes = await this.isVisible(MoveItemLocator.buttonYes);
+        if (buttonYes) {
+            console.log("Yes button is visible, click Yes button");
+            await this.expectVisible(MoveItemLocator.buttonYes);
+            await this.click(MoveItemLocator.buttonYes);
+            await this.expectTextVisible("QUICK SERVICE", true);
+        } else {
+            const isMenuNameVisible = await this.isVisible(MoveItemLocator.verifyMenu(menuName));
+            if (isMenuNameVisible) {
+                throw new Error(`Menu ${menuName} is still visible on the screen after the operation.`);
+            }
+            await this.expectVisible(MoveItemLocator.buttonSaveOrder);
+            await this.click(MoveItemLocator.buttonSaveOrder);
+            await this.expectTextVisible("Select Payment Method", true);
+        }
+    }
+
+    async movePartialItemMenu(menuName: string): Promise<void> {
+        await this.expectVisible(MoveItemLocator.buttonPlusMenu(menuName));
+        await this.click(MoveItemLocator.buttonPlusMenu(menuName));
     }
 
     async moveItemToSectionDineIn(): Promise<void> {
