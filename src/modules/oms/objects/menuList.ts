25aa46e72ab3b6fc08905574e5947332ef5b18c0
alvin <alvin.tutupary@esb.co.id>
2024-11-13 12:05:28 +0700

    [ADD] add menu list

---
+++ src/modules/oms/objects/menuList.ts
@@ -0,0 +1,133 @@
+export default class MenuList {
+    public static anggur = {
+        name: "Anggur",
+        minumanAnggur: {
+            name: "Minuman Anggur",
+            anggurHijauKawaKawa600ml: {
+                name: "[21+] Anggur Hijau Kawa Kawa 600ml",
+                shortName: "[21+] Anggur Hijau Kawa Kawa"
+            },
+            anggurMerahKawaKawa600ml: {
+                name: "[21+] Anggur Merah Kawa Kawa 600ml",
+                shortName: "[21+] Anggur Merah Kawa Kawa"
+            },
+            anggurMerahOT620ml: {
+                name: "[21+] Anggur Merah OT 620ml",
+                shortName: "[21+] Anggur Merah OT 620ml"
+            },
+            anggurMerahOTGold620ml: {
+                name: "[21+] Anggur Merah OT Gold 620ml",
+                shortName: "[21+] Anggur Merah OT Gold 620"
+            },
+            anggurPutihOT620ml: {
+                name: "[21+] Anggur Putih OT 620ml",
+                shortName: "[21+] Anggur Putih OT 620ml"
+            },
+            sababayBlackVelvet750ml: {
+                name: "[21+] Sababay Black Velvet 750ml",
+                shortName: "[21+] Sababay Black Velvet 750"
+            },
+            sababayLudisia750ml: {
+                name: "[21+] Sababay Ludisia 750ml",
+                shortName:
+                    "[21+] Sababay Ludisia 750ml"
+            },
+            sababayMistelle750ml: {
+                name: "[21+] Sababay Mistelle 750ml",
+                shortName: "[21+] Sababay Mistelle 750ml"
+            },
+            sababayWhiteVelvet750ml: {
+                name: "[21+] Sababay White Velvet 750ml",
+                shortName: "[21+] Sababay White Velvet 750"
+            },
+            ch21620ml: {
+                name: "【21+】黒ぶどう＆もち米OT 620ml",
+                shortName: "【21+】黒ぶどう＆もち米OT 620ml"
+            }
+        }
+    };
+
+    public static whisky = {
+        name: "Whisky",
+        minumanWhisky: {
+            name: "Minuman Whisky",
+            bataviaBlended700ml: {
+                name: "[21+] Batavia Blended 700ml",
+                shortName: "[21+] Batavia Blended 700ml"
+            },
+            gilbeysWhisky350ml: {
+                name: "[21+] Gilbeys Whisky 350ml",
+                shortName: "[21+] Gilbeys Whisky 350ml"
+            },
+            gilbeysWhisky700ml: {
+                name: "[21+] Gilbeys Whisky 700ml",
+                shortName: "[21+] Gilbeys Whisky 700ml"
+            },
+            pennyPacker700ml: {
+                name: "[21+] Penny Packer 700ml",
+                shortName: "[21+] Penny Packer 700ml"
+            },
+            teacherWhisky700ml: {
+                name: "[21+] Teacher Whisky 700ml",
+                shortName: "[21+] Teacher Whisky 700ml"
+            },
+            whiskyDrum250ml: {
+                name: "[21+] Whisky Drum 250ml",
+                shortName: "[21+] Whisky Drum 250ml"
+            },
+            whiskyDrum700mlL: {
+                name: "[21+] Whisky Drum 700mlL",
+                shortName: "[21+] Whisky Drum 700mlL"
+            },
+            mrDowellsWhisky750ml: {
+                name: "Mr Dowells Whisky 750ml",
+                shortName: "Mr Dowells Whisky 750ml"
+            },
+            royalBrewHouseRedRoyal350ml: {
+                name: "Royal Brew House Red Royal 350ml",
+                shortName: "Royal Brew House Red Royal 350"
+            }
+        }
+    };
+
+    public static atCategory = {
+        name: "AT MENU CATEGORY",
+        atMenuBiasa: {
+            name: "AT MENU BIASA",
+            atMenuBiasaGoreng: {
+                name: "AT MENU BIASA GORENG",
+                shortName: "AT MENU BIASA GORENG"
+            },
+            atMenuBiasaRebus: {
+                name: "AT MENU BIASA REBUS",
+                shortName: "AT MENU BIASA REBUS"
+            },
+            atMenuBiasaBakar: {
+                name: "AT MENU BIASA BAKAR",
+                shortName: "AT MENU BIASA BAKAR"
+            }
+        },
+        atMenuExtra: {
+            name: "AT MENU EXTRA",
+            atMenuExtraAlpha: {
+                name: "AT MENU EXTRA ALPHA",
+                shortName: "AT MENU EXTRA ALPHA"
+            },
+            atMenuExtraBeta: {
+                name: "AT MENU EXTRA BETA",
+                shortName: "AT MENU EXTRA BETA"
+            }
+        },
+        atMenuPaket: {
+            name: "AT MENU PAKET",
+            atMenuPaketMurah: {
+                name: "AT MENU PAKET MURAH",
+                shortName: "AT MENU PAKET MURAH"
+            },
+            atMenuPaketMahal: {
+                name: "AT MENU PAKET MAHAL",
+                shortName: "AT MENU PAKET MAHAL"
+            }
+        }
+    };
+}
