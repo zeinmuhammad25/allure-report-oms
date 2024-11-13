e93abc025b88a192788eba00b5e3430b6f419234
alvin <alvin.tutupary@esb.co.id>
2024-11-13 15:35:07 +0700

    [ADD] order page

---
+++ src/modules/oms/tableList/order/order.scenario.ts
@@ -0,0 +1,31 @@
+import BaseScenario from "../../../../base/base-scenario";
+
+export default interface OrderScenario extends BaseScenario {
+    // Top Actions
+    inputPhoneNumber(phoneNumber: string): Promise<void>;
+
+    addAdditionalInfo(additionalInfo: string): Promise<void>;
+
+    editTable(qty: number, timeOut: number): Promise<void>;
+
+    // Menu Action
+    selectCategoryMenu(categoryName: string): Promise<void>;
+
+    selectCategoryDetailMenu(categoryDetailName: string): Promise<void>;
+
+    selectMenu(menuName: string): Promise<void>;
+
+    deleteMenu(menuName: string): Promise<void>;
+
+    // Bottom Action
+    saveOrder(): Promise<void>;
+
+    gotoPayment(): Promise<void>;
+
+    printBill(): Promise<void>;
+
+    printChecker(): Promise<void>;
+
+    cancelTable(notes: string): Promise<void>;
+
+}
