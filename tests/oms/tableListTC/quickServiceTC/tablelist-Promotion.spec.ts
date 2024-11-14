0d4bfebfb42f066747be60f066d44cf027a66b9c
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-15 00:17:10 +0700

    [ADD] SCENARIO [TC_0204053]

---
+++ tests/oms/tableListTC/quickServiceTC/tablelist-Promotion.spec.ts
@@ -0,0 +1,60 @@
+import {test} from "@playwright/test";
+import TerminalIDPage from "../../../../src/modules/oms/terminalID/terminalID.page";
+import SignPinPage from "../../../../src/modules/oms/signPin/signPin.page";
+import TableListPage from "../../../../src/modules/oms/tableList/tableList.page";
+import QuickServiceListPage from "../../../../src/modules/oms/tableList/quickServiceList/quickServiceList.page";
+import BookOrderComponent from "../../../../src/modules/oms/tableList/components/bookOrder/bookOrder.component";
+import OrderPage from "../../../../src/modules/oms/tableList/order/order.page";
+import MenuList from "../../../../src/modules/oms/objects/menuList";
+import EditOrderComponents from "../../../../src/modules/oms/tableList/order/components/editOrder/editOrder.components";
+import AddOrderComponent from "../../../../src/modules/oms/tableList/order/components/addOrder/addOrder.component";
+import PromotionListComponent
+    from "../../../../src/modules/oms/tableList/components/promotionList/promotionList.component";
+
+
+test.describe.serial("Quick Service Promotion", () => {
+
+    const tags = "@smokeTest @oms @apply_promotion ";
+
+    test.beforeEach(async ({page}) => {
+        let terminalIdPage = new TerminalIDPage(page);
+        let signPinPage = new SignPinPage(page);
+        let quickServicePage = new QuickServiceListPage(page);
+        await terminalIdPage.navigateHere();
+        await terminalIdPage.performTerminalID();
+        await signPinPage.inputPinByTouch("22");
+        await signPinPage.submitPin();
+        await quickServicePage.addOrderQuickService();
+
+    });
+
+    test("[TC_0204053] Validate Logic When User Apply Promotion Head - Order Pages- Discount Bill Rp",
+        {tag: tags + "@positive"}, async ({page}) => {
+            let bookOrder = new BookOrderComponent(page);
+            let orderPage = new OrderPage(page);
+            let addOrderComponent = new AddOrderComponent(page);
+            let promotionListComponent = new PromotionListComponent(page);
+            await bookOrder.setPax(2);
+            await bookOrder.selectSalesMode("AT EXCLUSIVE");
+            await bookOrder.applyQuickService();
+            await bookOrder.skipCustomerPhoneNumber();
+            await orderPage.selectCategoryMenu(MenuList.atCategory.name);
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuBiasa.name);
+            await orderPage.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaBakar.name);
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuBiasa.name);
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+            await orderPage.selectMenu(MenuList.atCategory.atMenuPaket.atMenuPaketMahal.name);
+            await addOrderComponent.modifyMenuDetailPackage([
+                {menuName: MenuList.menuPackages.bombaySapphireDryGin750ml.shortName, qty: "max", notes: "test"}
+            ]);
+            await addOrderComponent.wait(2000);
+            await addOrderComponent.applyMenuDetailPackage();
+            await orderPage.wait(2000);
+            await orderPage.addPromotion();
+            await promotionListComponent.selectPromotion("BILL DISCOUNT RP");
+            await orderPage.saveOrder();
+
+        }
+    );
+
+});
