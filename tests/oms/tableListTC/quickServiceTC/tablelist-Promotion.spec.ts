9eaf16b5755a54516828feb4fbda6664a344e93a
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-16 20:31:52 +0700

    [MOD] add Quick Service - Promotion [TC_0204076] - Validate Logic When User Apply Promotion Head - Payment Pages - Menu Discount Rp Menu

--- tests/oms/tableListTC/quickServiceTC/tablelist-Promotion.spec.ts
+++ tests/oms/tableListTC/quickServiceTC/tablelist-Promotion.spec.ts
@@ -851,6 +851,41 @@ test.describe.serial("Quick Service Promotion", () => {
 
         }
     );
+    test("[TC_0204076] Validate Logic When User Apply Promotion Head - Payment Pages -  Menu Discount Rp Menu",
+        {tag: tags + "@positive"}, async ({page}) => {
+            let bookOrder = new BookOrderComponent(page);
+            let orderPage = new OrderPage(page);
+            let addOrderComponent = new AddOrderComponent(page);
+            let promotionListComponent = new PromotionListComponent(page);
+            let paymentPOSPage = new PaymentPOSPage(page);
+            await bookOrder.setPax(2);
+            await bookOrder.selectSalesMode("AT EXCLUSIVE");
+            await bookOrder.applyQuickService();
+            await bookOrder.skipCustomerPhoneNumber();
+            await orderPage.selectCategoryMenu(MenuList.atCategory.name);
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuBiasa.name);
+            await orderPage.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaRebus.name, 5);
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuBiasa.name);
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+            await orderPage.selectMenu(MenuList.atCategory.atMenuPaket.atMenuPaketMurah.name);
+            await addOrderComponent.modifyMenuDetailPackage([
+                {menuName: MenuList.menuPackages.bataviaBlended700ml.shortName, qty: 4, notes: null},
+                {menuName: MenuList.menuPackages.baileysOriginal700ml.shortName, qty: 3, notes: null},
+                {menuName: MenuList.menuPackages.captainMorgan200ml.shortName, qty: 1, notes: null},
+                {menuName: MenuList.menuPackages.icelandVodka250ml.shortName, qty: 2, notes: null},
+            ]);
+            await addOrderComponent.wait(2000);
+            await addOrderComponent.applyMenuDetailPackage();
+            await orderPage.wait(2000);
+            await orderPage.saveOrder();
+            await paymentPOSPage.wait(1000);
+            await paymentPOSPage.paymentType(PaymentObject.AddPromo);
+            await promotionListComponent.searchPromotion("MENU DISC RP MENU");
+            await promotionListComponent.selectPromotion("MENU DISC RP MENU");
+            await paymentPOSPage.wait(1000);
+
+        }
+    );
 
 
 });
