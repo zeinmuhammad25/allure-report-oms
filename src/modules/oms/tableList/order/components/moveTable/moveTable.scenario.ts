69eefca77e4307860f40cda1ba481321a1d7749a
alvin <alvin.tutupary@esb.co.id>
2024-11-18 03:42:02 +0700

    [MOD] add function on moveTable.component.ts - selectRoom() - selectTable() - disableButtonByLabel()

--- src/modules/oms/tableList/order/components/moveTable/moveTable.scenario.ts
+++ src/modules/oms/tableList/order/components/moveTable/moveTable.scenario.ts
@@ -3,11 +3,16 @@ import BaseScenario from "../../../../../../base/base-scenario";
 export default interface MoveTableScenario extends BaseScenario {
     autoMoveTable(): Promise<void>;
 
+    selectRoom(roomName: string): Promise<void>;
+
+    selectTable(tableName: string): Promise<void>;
+
+    disableButtonByLabel(label: string): Promise<void>;
+
     cancelMoveTableBackTableList(): Promise<void>;
 
     selectTableAndCancelInAcRoom(): Promise<void>;
 
     selectTableAndCancelInSmokingRoom(): Promise<void>;
-
 
 }
