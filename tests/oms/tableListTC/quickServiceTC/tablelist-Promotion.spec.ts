3babb8b2d8703461b28a023eb06145c0814e7b54
reza fadhilah <reza.fadhilah@esb.co.id>
2024-11-20 19:26:45 +0700

    [MOD] add Quick Service - Promotion [TC_0204071] Validate Logic When User Apply Promotion Head - Payment Pages -  Discount % Menu Category Detail - adding menu extra in tc

--- tests/oms/tableListTC/quickServiceTC/tablelist-Promotion.spec.ts
+++ tests/oms/tableListTC/quickServiceTC/tablelist-Promotion.spec.ts
@@ -930,6 +930,7 @@ test.describe.serial("Quick Service Promotion", () => {
             let bookOrder = new BookOrderComponent(page);
             let orderPage = new OrderPage(page);
             let addOrderComponent = new AddOrderComponent(page);
+            let editOrderComponents = new EditOrderComponents(page);
             let promotionListComponent = new PromotionListComponent(page);
             let paymentPOSPage = new PaymentPOSPage(page);
             await bookOrder.setPax(2);
@@ -940,8 +941,20 @@ test.describe.serial("Quick Service Promotion", () => {
             await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuBiasa.name);
             await orderPage.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaRebus.name, 2);
             await orderPage.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaBakar.name, 5);
-            await orderPage.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaGoreng.name, 5);
             await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuBiasa.name);
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuExtra.name);
+            await orderPage.selectMenu(MenuList.atCategory.atMenuExtra.atMenuExtraAlpha.name, 3);
+            await orderPage.clickMenuDetail(MenuList.atCategory.atMenuExtra.atMenuExtraAlpha.name);
+            await editOrderComponents.escapeKeyboard();
+            await editOrderComponents.actionButtonFooter("Next");
+            await editOrderComponents.actionButtonFooter("Next");
+            await editOrderComponents.selectMenuExtraCategory(MenuList.anggur.name);
+            await editOrderComponents.selectMenuExtra(MenuList.anggur.minumanAnggur.anggurMerahOT620ml.shortName);
+            await editOrderComponents.selectMenuExtra(MenuList.anggur.minumanAnggur.anggurMerahOT620ml.shortName);
+            await editOrderComponents.selectMenuExtra(MenuList.anggur.minumanAnggur.anggurMerahOT620ml.shortName);
+            await editOrderComponents.selectMenuExtra(MenuList.anggur.minumanAnggur.anggurMerahOT620ml.shortName);
+            await editOrderComponents.actionButtonFooter("Apply");
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuExtra.name);
             await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
             await orderPage.selectMenu(MenuList.atCategory.atMenuPaket.atMenuPaketMurah.name);
             await addOrderComponent.modifyMenuDetailPackage([
