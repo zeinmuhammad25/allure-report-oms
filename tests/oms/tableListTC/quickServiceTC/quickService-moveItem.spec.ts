9ad34943b2929d384ceadea0802c8ee177596309
alvin <alvin.tutupary@esb.co.id>
2024-11-22 16:34:46 +0700

    [ADD] add Quick Service - Move Item [TC_0204145] - Validate Logic when User can Move Item from Quick Service to Dine-In Parent Merge Table

--- tests/oms/tableListTC/quickServiceTC/quickService-moveItem.spec.ts
+++ tests/oms/tableListTC/quickServiceTC/quickService-moveItem.spec.ts
@@ -1113,24 +1113,40 @@ test.describe.serial("Quick Service Move Item", () => {
 
     test("[TC_0204145] Validate Logic when User can Move Item from Quick Service to Dine-In Parent Merge Table",
         {tag: tags + "@positive"}, async ({page}) => {
-            //TODO :
-            // Precondition:
-            //  POS
-            //  1. Open POS
-            //  2. Open other transaction Dine In Merge Table
-            //  3. Order menu
-            // Steps:
-            //  1. Create transaction Quick Service
-            //  2. Choose Sales Mode Not Inclusive
-            //  3. Order menu
-            //  4. Click Save Order
-            //  5. Click transaction Quick Service again
-            //  6. Click button Move Item
-            //  7. Click section Dine In
-            //  8. Select other transaction Dine In Parent Merge Table
-            //  9. Click button Next
-            //  10. Select menu
-            //  11. Click button Apply
+            ////
+            await tableListPage.selectRoom(Table.acRoom.name);
+            await tableListPage.selectTable(Table.acRoom.ac3.name);
+            await bookOrderComponent.selectSalesMode("AT EXCLUSIVE");
+            await bookOrderComponent.bookAndOrder();
+            await bookOrderComponent.skipCustomerPhoneNumber();
+            await orderPage.saveOrder();
+
+            await tableListPage.selectRoom(Table.acRoom.name);
+            await tableListPage.selectTable(Table.acRoom.ac3.name);
+            await orderPage.mergeTable();
+            await mergeTableComponent.selectRoom(Table.acRoom.name);
+            await mergeTableComponent.selectTable(Table.acRoom.ac1.name);
+            await mergeTableComponent.applyMergeTable();
+            await orderPage.saveOrder();
+
+            await quickServiceListPage.addOrderQuickService();
+            await bookOrderComponent.setPax(2);
+            await bookOrderComponent.selectSalesMode("AT EXCLUSIVE");
+            await bookOrderComponent.applyQuickService();
+            await bookOrderComponent.skipCustomerPhoneNumber();
+            await orderPage.selectCategoryMenu(MenuList.atCategory.name);
+            await orderPage.selectCategoryDetailMenu(MenuList.atCategory.atMenuBiasa.name);
+            await orderPage.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaBakar.name, 5);
+            await orderPage.saveOrder();
+
+            await sideNavBarComponents.gotoPageTableList();
+            await tableListPage.gotoQuickService();
+            await quickServiceListPage.selectSalesNum("last");
+            await orderPage.moveItem();
+            await moveItemComponents.moveItemToSectionDineIn(Table.acRoom.name, Table.acRoom.ac3.name);
+            await moveItemComponents.movePartialItemMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaBakar.name);
+            await moveItemComponents.movePartialItemMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaBakar.name);
+            await moveItemComponents.actionApplyMoveItem();
         }
     );
 
