966230925d8fcea69b08d65e51823733a9b05731
safruddin.setia <safruddin.setia@esb.co.id>
2024-11-28 22:58:08 +0700

    [MOD] Locator pax

--- src/modules/oms/tableList/components/bookOrder/bookOrder.locator.ts
+++ src/modules/oms/tableList/components/bookOrder/bookOrder.locator.ts
@@ -1,12 +1,13 @@
 import BaseLocator from "../../../../../base/base-locator";
 
 export default class BookOrderLocator extends BaseLocator {
-    static paxButton = (numberOfPax: number): string => `//span[@class='mat-button-wrapper'][text()='${numberOfPax}']`;
+    static paxButton = (numberOfPax: number): string => `//span[@class='mat-button-wrapper'][normalize-space()='${numberOfPax}']`;
     static salesModeButton = (salesModeName: string): string => `//button[contains(@class,'btn-vp')]//span[contains(text(),'${salesModeName}')]`;
     static numberOfPaxField = "(//app-number-input//input)[1]";
     static orderTimeOutField = "(//app-number-input//input)[2]";
     static bookTableButton = "//div[@class='ng-star-inserted']//button/span[contains(text(),'Book Table')]";
     static bookOrderButton = "//div[@class='ng-star-inserted']//button/span[contains(text(),'Book & Order')]";
+    static bookScanAndApplyButton = (action: "Apply" | "Scan / Input"): string => `//span[normalize-space()='${action}']`;
     static bookTableTab = "//app-table-open//button[normalize-space()='Book Table']";
     static bookingListTab = "//app-table-open//button[normalize-space()='Booking List']";
     static customerDataPhoneField = "//app-sales-contact-info//input[@placeholder='8xx xxx xxx']";
