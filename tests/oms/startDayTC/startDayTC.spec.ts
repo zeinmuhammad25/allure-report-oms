a85cd851b5cdcb74bc6cf04af017705e124457ff
Safruddin Setia Budi <safruddin.budi@esb.co.id>
2024-12-21 12:12:56 +0000

    Clean Up - StartDay

--- tests/oms/startDayTC/startDayTC.spec.ts
+++ tests/oms/startDayTC/startDayTC.spec.ts
@@ -1,55 +1,40 @@
-import {test} from "@playwright/test";
-import SignPinPage from "../../../src/modules/oms/signPin/signPin.page";
-import TerminalIDPage from "../../../src/modules/oms/terminalID/terminalID.page";
-import StartDayPage from "../../../src/modules/oms/startDay/startDay.page";
+import {test} from "../injection";
 
 test.setTimeout(100000);
 test.describe.serial("Start Day Test", () => {
 
-    test.beforeEach(async ({page}) => {
-        let terminalIdPage = new TerminalIDPage(page);
-        let signPinPage = new SignPinPage(page);
-
-        await terminalIdPage.navigateHere();
-        await terminalIdPage.performTerminalID();
-        await signPinPage.wait(2000)
-        await signPinPage.inputPinByTouch("22");
-        await signPinPage.submitPinValidateStartDayYes();
-    })
+    test.beforeEach(async ({terminalID, signPin}) => {
+        await terminalID.goHere();
+        await terminalID.performTerminalID();
+        await signPin.inputPinByTouch("22");
+        await signPin.submitPinValidateStartDayYes();
+    });
 
     test("Validate Logic when User input number in Starting Cash",
-        {tag: "@smokeTest @oms @StartDay @positive"}, async ({page}) => {
-            let startDay = new StartDayPage(page);
-
+        {tag: "@smokeTest @oms @StartDay @positive"}, async ({startDay}) => {
             await startDay.inputStartingCash("20.000");
             await startDay.confirmStartingCash();
             await startDay.notificationSuccessStartDay();
-
         }
-    )
+    );
 
     test("Validate Logic when User Not input Starting Cash",
-        {tag: "@smokeTest @oms @StartDay @negative"}, async ({page}) => {
-            let startDay = new StartDayPage(page);
-
+        {tag: "@smokeTest @oms @StartDay @negative"}, async ({startDay}) => {
             await startDay.inputStartingCash("");
             await startDay.popUpShiftInZero();
-
         }
-    )
+    );
 
     test("Validate Logic when User input Value 0 in Starting Cash",
-        {tag: "@smokeTest @oms @StartDay @negative"}, async ({page}) => {
-            let startDay = new StartDayPage(page);
-
+        {tag: "@smokeTest @oms @StartDay @negative"}, async ({startDay}) => {
             await startDay.inputStartingCash("0");
             await startDay.popUpShiftInZero();
 
         }
-    )
+    );
 
 
-})
+});
 
 
 
