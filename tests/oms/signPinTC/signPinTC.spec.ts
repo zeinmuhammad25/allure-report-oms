c9dbea29e7cf220fe1fc9861b848ff476cee7208
alvin <alvin.tutupary@esb.co.id>
2024-12-30 16:06:16 +0700

    [MOD] clean up signPinTC.spec.ts

--- tests/oms/signPinTC/signPinTC.spec.ts
+++ tests/oms/signPinTC/signPinTC.spec.ts
@@ -1,96 +1,64 @@
-import {test} from "@playwright/test";
-import SignPinPage from "../../../src/modules/oms/signPin/signPin.page";
-import TerminalIDPage from "../../../src/modules/oms/terminalID/terminalID.page";
+import {test} from "../injection";
 
-test.describe.serial("Landing Page", () => {
+test.describe.serial("Sign PIN", () => {
+    const tags = "@smokeTest @oms @signPin ";
 
-    test.beforeEach(async ({page}) => {
-        let terminalIdPage = new TerminalIDPage(page);
-        await terminalIdPage.navigateHere();
-        await terminalIdPage.performTerminalID();
-    })
+    test.beforeEach(async ({terminalID}) => {
+        await terminalID.goHere();
+        await terminalID.performTerminalID();
+    });
 
-    test("Validate Logic when User input valid/registered pin login validate start yes",
-        {tag: "@smokeTest @oms @signPin @positive"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
+    test("[TC_0202001] Validate Logic when User input valid/registered pin login validate start yes",
+        {tag: tags + "@positive"}, async ({signPin}) => {
+            await signPin.inputPinByTouch("22");
+            await signPin.submitPinValidateStartDayYes();
+        });
 
-            await signPinPage.inputPinByTouch("22");
-            await signPinPage.submitPinValidateStartDayYes();
-        }
-    )
+    test("[TC_020200?] Validate Logic when User input valid/registered pin login validate start no",
+        {tag: tags + "@negative"}, async ({signPin}) => {
+            await signPin.inputPinByTouch("22");
+            await signPin.submitPinValidateStartDayNo();
+        });
 
-    test("Validate Logic when User input valid/registered pin login validate start no",
-        {tag: "@smokeTest @oms @signPin @negative"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
+    test("[TC_0202002] Validate Logic when User input invalid/unregistered pin login",
+        {tag: tags + "@negative"}, async ({signPin}) => {
+            await signPin.inputPinByTouch("99999");
+            await signPin.submitPinNoUnregisteredUser();
+        });
 
-            await signPinPage.inputPinByTouch("22");
-            await signPinPage.submitPinValidateStartDayNo();
-        }
-    )
+    test("[TC_0202003] Validate Logic when User input Alphabet pin login",
+        {tag: tags + "@negative"}, async ({signPin}) => {
+            await signPin.inputPinByKeyboard("TEST");
+            await signPin.submitPinNoUnregisteredUser();
+        });
 
-    test("Validate Logic when User input invalid/unregistered pin login",
-        {tag: "@smokeTest @oms @signPin @negative"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
+    test("[TC_0202004] Validate Logic when User input alphanumeric pin login",
+        {tag: tags + "@negative"}, async ({signPin}) => {
+            await signPin.inputPinByKeyboard("$^%*)");
+            await signPin.submitPinNoUnregisteredUser();
+        });
 
-            await signPinPage.inputPinByTouch("99999");
-            await signPinPage.submitPinNoUnregisteredUser();
-        }
-    )
+    test("[TC_0202005] Validate Logic when User input 1 pin login",
+        {tag: tags + "@positive"}, async ({signPin}) => {
+            await signPin.inputPinByTouch("1");
+            await signPin.submitPinValidateStartDayYes();
+        });
 
-    test("Validate Logic when User input Alphabet pin login",
-        {tag: "@smokeTest @oms @signPin @negative"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
+    test("[TC_0202006] Validate Logic when User input 50 pin login",
+        {tag: tags + "@positive"}, async ({signPin}) => {
+            await signPin.inputPinByTouch("12345678901234567890123456789012345678901234567890");
+            await signPin.submitPinValidateStartDayYes();
+        });
 
-            await signPinPage.inputPinByKeyboard("TEST");
-            await signPinPage.submitPinNoUnregisteredUser();
-        }
-    )
+    test("[TC_0202007] Validate Logic when User input > 50 pin login",
+        {tag: tags + "@negative"}, async ({signPin}) => {
+            await signPin.inputPinByTouch("123456789012345678901234567890123456789012345678901232123123");
+            await signPin.submitPinNoUnregisteredUser();
+        });
 
-    test("Validate Logic when User input alphanumeric pin login",
-        {tag: "@smokeTest @oms @signPin @negative"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
-
-            await signPinPage.inputPinByKeyboard("$^%*)");
-            await signPinPage.submitPinNoUnregisteredUser();
-        }
-    )
-
-    test("Validate Logic when User input 1 pin login",
-        {tag: "@smokeTest @oms @signPin @positive"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
-
-            await signPinPage.inputPinByTouch("1");
-            await signPinPage.submitPinValidateStartDayYes();
-
-        }
-    )
-
-    test("Validate Logic when User input 50 pin login",
-        {tag: "@smokeTest @oms @signPin @positive"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
-
-            await signPinPage.inputPinByTouch("12345678901234567890123456789012345678901234567890")
-            await signPinPage.submitPinValidateStartDayYes();
-        }
-    )
-
-    test("Validate Logic when User input > 50 pin login",
-        {tag: "@smokeTest @oms @signPin @negative"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
-
-            await signPinPage.inputPinByTouch("123456789012345678901234567890123456789012345678901232123123")
-            await signPinPage.submitPinNoUnregisteredUser();
-        }
-    )
-
-    test("Validate Logic when User clear pin field",
-        {tag: "@smokeTest @oms @signPin @positive"}, async ({page}) => {
-            let signPinPage = new SignPinPage(page);
-
-            await signPinPage.inputPinByTouch("1234")
-            await signPinPage.clearPin();
-        }
-    )
-
-
-})
+    test("[TC_0202008] Validate Logic when User clear pin field",
+        {tag: tags + "@positive"}, async ({signPin}) => {
+            await signPin.inputPinByTouch("1234");
+            await signPin.clearPin();
+        });
+});
