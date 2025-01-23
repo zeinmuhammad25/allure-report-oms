f626c8b7f958b7311ebb961139ecf1c5effc870d
alvin <alvin.tutupary@esb.co.id>
2025-01-23 16:50:38 +0700

    chore : reformat

--- tests/pld/dashboardTC/dashboard-rawMaterialAndSalesStatistic.spec.ts
+++ tests/pld/dashboardTC/dashboard-rawMaterialAndSalesStatistic.spec.ts
@@ -3,28 +3,28 @@ import LoginPage from "../../../src/modules/pld/login/login.page";
 import {test} from "@playwright/test";
 
 test.describe.serial("Dashboard Composition Test", () => {
-    let loginPage: LoginPage
-    const tag = '@smokeTest @dashboard @rawMaterialAndSalesStatistic'
+    let loginPage: LoginPage;
+    const tag = "@smokeTest @dashboard @rawMaterialAndSalesStatistic";
 
     test.beforeEach(async ({page}) => {
-        loginPage = new LoginPage(page)
-        await loginPage.navigateHere()
-        await loginPage.performLoginSubs()
-    })
+        loginPage = new LoginPage(page);
+        await loginPage.navigateHere();
+        await loginPage.performLoginSubs();
+    });
 
     test(
         "Verify User can filter and show Sales Statistic Data of on Dashboard Page",
         {tag: tag},
         async ({page}) => {
-            const dashboardPage = new DashboardPage(page)
-            await dashboardPage.validateSalesStatisticOnDashboard()
-        })
+            const dashboardPage = new DashboardPage(page);
+            await dashboardPage.validateSalesStatisticOnDashboard();
+        });
 
     test(
         "Verify User can filter and show Raw Material Stock Data of on Dashboard Page",
         {tag: tag},
         async ({page}) => {
-            const dashboardPage = new DashboardPage(page)
-            await dashboardPage.validateRawMaterialStockOnDashboard()
-        })
-})
+            const dashboardPage = new DashboardPage(page);
+            await dashboardPage.validateRawMaterialStockOnDashboard();
+        });
+});
