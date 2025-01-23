068c25edab86ae17ff11af5d181a9c9e6e2417ca
alvin <alvin.tutupary@esb.co.id>
2025-01-23 16:49:08 +0700

    chore : reformat

--- tests/pld/reportTC/salesSummaryReport.spec.ts
+++ tests/pld/reportTC/salesSummaryReport.spec.ts
@@ -3,42 +3,42 @@ import SalesSummaryPage from "../../../src/modules/pld/report/salesSummary/sales
 import LoginPage from "../../../src/modules/pld/login/login.page";
 
 test.describe.serial("Sales Summary Report Test", () => {
-    let loginPage: LoginPage
-    const tag = '@smokeTest @report @salesSummaryReport'
+    let loginPage: LoginPage;
+    const tag = "@smokeTest @report @salesSummaryReport";
 
     test.beforeEach(async ({page}) => {
-        loginPage = new LoginPage(page)
-        await loginPage.navigateHere()
-        await loginPage.performLoginSubs()
-    })
+        loginPage = new LoginPage(page);
+        await loginPage.navigateHere();
+        await loginPage.performLoginSubs();
+    });
 
     test("Verify Summary Report has Sales Statistic", {tag: tag}, async ({page}) => {
-        const salesSummaryPage = new SalesSummaryPage(page)
-        await salesSummaryPage.validateSalesStatistic()
-    })
+        const salesSummaryPage = new SalesSummaryPage(page);
+        await salesSummaryPage.validateSalesStatistic();
+    });
 
     test("Verify Summary Report has Data Sales Per Day", {tag: tag}, async ({page}) => {
-        const salesSummaryPage = new SalesSummaryPage(page)
-        await salesSummaryPage.validateDataSalesPerDay()
-    })
+        const salesSummaryPage = new SalesSummaryPage(page);
+        await salesSummaryPage.validateDataSalesPerDay();
+    });
 
     test("Verify Summary Report has Data Sales Per Time", {tag: tag}, async ({page}) => {
-        const salesSummaryPage = new SalesSummaryPage(page)
-        await salesSummaryPage.validateDataSalesPerTime()
-    })
+        const salesSummaryPage = new SalesSummaryPage(page);
+        await salesSummaryPage.validateDataSalesPerTime();
+    });
 
     test("Verify Summary Report has Data Sales", {tag: tag}, async ({page}) => {
-        const salesSummaryPage = new SalesSummaryPage(page)
-        await salesSummaryPage.validateDataSales()
-    })
+        const salesSummaryPage = new SalesSummaryPage(page);
+        await salesSummaryPage.validateDataSales();
+    });
 
     test("Verify Summary Report has Data Fraud Controll", {tag: tag}, async ({page}) => {
-        const salesSummaryPage = new SalesSummaryPage(page)
-        await salesSummaryPage.validateDataSalesFraudControl()
-    })
+        const salesSummaryPage = new SalesSummaryPage(page);
+        await salesSummaryPage.validateDataSalesFraudControl();
+    });
 
     test("Verify Summary Report can be download", {tag: tag}, async ({page}) => {
-        const salesSummaryPage = new SalesSummaryPage(page)
-        await salesSummaryPage.downloadSalesSummary()
-    })
-})
+        const salesSummaryPage = new SalesSummaryPage(page);
+        await salesSummaryPage.downloadSalesSummary();
+    });
+});
