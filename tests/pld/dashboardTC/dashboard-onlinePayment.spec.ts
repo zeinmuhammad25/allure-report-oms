770dafc0dafa1393765f2e08930da78ce6950b72
alvin <alvin.tutupary@esb.co.id>
2025-01-24 14:31:44 +0700

    chore: add testcase_id

--- tests/pld/dashboardTC/dashboard-onlinePayment.spec.ts
+++ tests/pld/dashboardTC/dashboard-onlinePayment.spec.ts
@@ -12,25 +12,25 @@ test.describe.serial("Dashboard Online Payment Test", () => {
         await loginPage.performLoginSubs();
     });
 
-    test("Verify User can filter and show Summary Payment Data of Dashboard Online Payment",
+    test("[TC_0401026] Verify User can filter and show Summary Payment Data of Dashboard Online Payment",
         {tag: tag}, async ({page}) => {
             const onlinePaymentPage = new OnlinePaymentPage(page);
             await onlinePaymentPage.validateSummaryPaymentDataOnDashboardOnlinePayment();
         });
 
-    test("Verify User can filter and show Detail Payment Data of Dashboard Online Payment",
+    test("[TC_0401027] Verify User can filter and show Detail Payment Data of Dashboard Online Payment",
         {tag: tag}, async ({page}) => {
             const onlinePaymentPage = new OnlinePaymentPage(page);
             await onlinePaymentPage.validateDetailPaymentDataOnDashboardOnlinePayment();
         });
 
-    test("Verify User can filter and show Daily Payment Data of Dashboard Online Payment",
+    test("[TC_0401028] Verify User can filter and show Daily Payment Data of Dashboard Online Payment",
         {tag: tag}, async ({page}) => {
             const onlinePaymentPage = new OnlinePaymentPage(page);
             await onlinePaymentPage.validateDailyPaymentDataOnDashboardOnlinePayment();
         });
 
-    test("Verify User can filter, show, and download Data of Dashboard Online Payment",
+    test("[TC_0401029] Verify User can filter, show, and download Data of Dashboard Online Payment",
         {tag: tag}, async ({page}) => {
             const onlinePaymentPage = new OnlinePaymentPage(page);
             await onlinePaymentPage.validateDownloadDataOnDashboardOnlinePayment();
