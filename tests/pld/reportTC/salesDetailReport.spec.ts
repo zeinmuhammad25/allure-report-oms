5ee269df0de3cd11b4a3a9eee0d1748060f60d37
alvin <alvin.tutupary@esb.co.id>
2025-01-24 15:01:38 +0700

    chore: reformat and add testcase_id

--- tests/pld/reportTC/salesDetailReport.spec.ts
+++ tests/pld/reportTC/salesDetailReport.spec.ts
@@ -3,27 +3,30 @@ import LoginPage from "../../../src/modules/pld/login/login.page";
 import SalesDetailPage from "../../../src/modules/pld/report/salesDetail/salesDetail.page";
 
 test.describe.serial("Sales Detail Report Test", () => {
-    let loginPage: LoginPage
-    const tag = '@smokeTest @report @salesDetail'
+    let loginPage: LoginPage;
+    const tag = "@smokeTest @report @salesDetail";
 
     test.beforeEach(async ({page}) => {
-        loginPage = new LoginPage(page)
-        await loginPage.navigateHere()
-        await loginPage.performLoginSubs()
-    })
+        loginPage = new LoginPage(page);
+        await loginPage.navigateHere();
+        await loginPage.performLoginSubs();
+    });
 
-    test("Verify User can filter and view data Sales Detail", {tag: tag}, async ({page}) => {
-        const salesDetailPage = new SalesDetailPage(page)
-        await salesDetailPage.validateFilterAndShowData()
-    })
+    test("[TC_0402007] Verify User can filter and view data Sales Detail",
+        {tag: tag}, async ({page}) => {
+            const salesDetailPage = new SalesDetailPage(page);
+            await salesDetailPage.validateFilterAndShowData();
+        });
 
-    test("Verify User can filter and view data Sales Detail with Transaction Number", {tag: tag}, async ({page}) => {
-        const salesDetailPage = new SalesDetailPage(page)
-        await salesDetailPage.validateShowDataWithTransactionNumber()
-    })
+    test("[TC_0402007] Verify User can filter and view data Sales Detail with Transaction Number",
+        {tag: tag}, async ({page}) => {
+            const salesDetailPage = new SalesDetailPage(page);
+            await salesDetailPage.validateShowDataWithTransactionNumber();
+        });
 
-    test("Verify User can filter, view, and download data", {tag: tag}, async ({page}) => {
-        const salesDetailPage = new SalesDetailPage(page)
-        await salesDetailPage.validateShowDataAndDownload()
-    })
-})
+    test("[TC_0402007] Verify User can filter, view, and download data",
+        {tag: tag}, async ({page}) => {
+            const salesDetailPage = new SalesDetailPage(page);
+            await salesDetailPage.validateShowDataAndDownload();
+        });
+});
