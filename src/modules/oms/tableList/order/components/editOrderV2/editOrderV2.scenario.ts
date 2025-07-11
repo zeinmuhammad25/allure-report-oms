c52bbf77caf07ac02708c45efa07a53538bad988
reza fadhilah <reza.fadhilah@esb.co.id>
2025-07-11 18:13:25 +0700

    Add new scenario in editOrderV2.scenario.ts

--- src/modules/oms/tableList/order/components/editOrderV2/editOrderV2.scenario.ts
+++ src/modules/oms/tableList/order/components/editOrderV2/editOrderV2.scenario.ts
@@ -2,4 +2,28 @@ import BaseScenario from "../../../../../../base/base-scenario";
 
 export default interface EditOrderV2Scenario extends BaseScenario {
 
+    editQtyInput(qtyMenu: string): Promise<void>;
+
+    modifyEditHeadPackage(qty: number[]): Promise<void>;
+
+    inputMenuNotesSingelMenu(notesSingelMenu: string): Promise<void>;
+
+    inputMenuNotesPackageHead(notesHeadPackage: string): Promise<void>;
+
+    editSelectPackageGroup(groupName: string): Promise<void>;
+
+    addPromotionMenu(): Promise<void>;
+
+    applyViaSearchPromotionMenu(promotionName: string): Promise<void>;
+
+    inputPriceMenuOpenPrice(price: string): Promise<void>;
+
+    inputCustomNameMenuOpenPrice(nameMenu: string): Promise<void>;
+
+    inputNoteMenuOpenPrice(valueNotes: string): Promise<void>;
+
+    applyOpenPrice(): Promise<void>;
+
+    cancelOpenPrice(): Promise<void>;
+
 }
