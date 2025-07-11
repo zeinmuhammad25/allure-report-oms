7951fff47e2f869c78ae3714517642365780acad
reza fadhilah <reza.fadhilah@esb.co.id>
2025-07-11 11:39:14 +0700

    Add new Locator in editOrderV2.locator.ts

---
+++ src/modules/oms/tableList/order/components/editOrderV2/editOrderV2.locator.ts
@@ -0,0 +1,50 @@
+import BaseLocator from "../../../../../../base/base-locator";
+
+export default class EditOrderV2Locator extends BaseLocator {
+
+    static editInputQty: string = "//input[@class='qty-input ng-valid ng-star-inserted ng-dirty ng-touched']";
+    static editPlusQtyPackageHead: string = "//button[@class='qty-button qty-button-active mat-stroked-button ng-star-inserted']";
+    static editMinusQtyPackageHead: string = "//button[@class='qty-button mat-stroked-button ng-star-inserted']";
+    static buttonEditSelectPackage = (menuName: string): string =>
+        `//div[contains(@class, 'menu-package-group-active')]//span[starts-with(normalize-space(), '${menuName}')]`;
+    static filedEditSearchPackageV2: string = "//input[@placeholder='Search menu']";
+    static nextPageEditPackageList: string = "//img[@src='assets/images/icon-chevron-single-right.png']";
+    static backPageEditPackageList: string = "//div[@class='cdk-overlay-container']//div[@class='d-flex flex-column']//button[1]";
+    static upPageEditPackageItemList: string = "//span//img[@class='prev-paginator-img']";
+    static downPageEditPackageItemList: string = "//span//img[@class='next-paginator-img']";
+
+    private static qtyEditButtonDetailMenu = (menuName: string, symbol: string): string =>
+        `//div[span[normalize-space()='${menuName}']]/following-sibling::*//button[contains(@class, 'qty-btn')]//span[normalize-space()='${symbol}']`;
+    static plusQtyPackageDetail = (menuName: string): string => this.qtyEditButtonDetailMenu(menuName, "+");
+    static minusQtyPackageDetail = (menuName: string): string => this.qtyEditButtonDetailMenu(menuName, "-");
+
+    private static buttonEditNotesFunction = (menuName: string, label: string): string =>
+        `//div[span[normalize-space()='${menuName}']]/following-sibling::*//button[.//span[normalize-space()='${label}']]`;
+    static notesDetailMenu = (menuName: string): string => this.buttonEditNotesFunction(menuName, "Notes");
+    static fieldNotesDetailMenu: string = "//app-menu-notes-v2//textarea";
+    static popUpNotesDetailMenu: string = "//app-menu-notes-v2";
+
+    private static editButtonFunction = (label: string): string =>
+        `//button[.//span[normalize-space()='${label}']]`;
+    private static editButtonFunctionDisabled = (label: string): string =>
+        `//button[@disabled and .//span[normalize-space()='${label}']]`;
+    static buttonNext: string = this.editButtonFunction("Next");
+    static disableButtonNext: string = this.editButtonFunctionDisabled("Next");
+    static buttonUpdate: string = this.editButtonFunction("Update");
+    static buttonCancel: string = this.editButtonFunction("Cancel");
+    static addNotesMenu: string = this.editButtonFunction("Add Menu Notes");
+    static fieldNotesMenu: string = "//app-menu-edit-v2//textarea";
+    static popUpNotesMenu: string = "//app-menu-edit-v2";
+    static buttonApplyNotes: string = "//span[@class='mat-button-wrapper'][normalize-space()='Confirm']";
+    static buttonBackNotes: string = "//span[@class='mat-button-wrapper'][normalize-space()='Back']";
+    static buttonPromotion: string = this.editButtonFunction("Apply Promotion");
+    static editOrderSearchPromotion: string = "//input[@placeholder='Search Promo']";
+    static editOrderPromotionType: string = "//div[@class='mat-select-arrow-wrapper']";
+    static editOrderPromotionTypeList = (typeName: string) => `//span[@class='mat-option-text'][normalize-space()='${typeName}']`;
+    static editOrderPromotionName = (promotionName: string) => `//td[normalize-space()='${promotionName}']`;
+    static buttonNextPromotionList = "//mat-icon[normalize-space()='chevron_right']";
+    static buttonBackPromotionList = "//mat-icon[normalize-space()='chevron_left']";
+    static buttonApplyPromotion: string = this.editButtonFunction("Apply");
+    static buttonBackPromotion: string = this.editButtonFunction("Back");
+
+}
