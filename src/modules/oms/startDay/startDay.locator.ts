f9f1c50013307934b047a0fa432c65001caf31f9
reza fadhilah <reza.fadhilah@esb.co.id>
2025-07-15 14:07:11 +0700

    Adjustment Locator scapeKeyboard

--- src/modules/oms/startDay/startDay.locator.ts
+++ src/modules/oms/startDay/startDay.locator.ts
@@ -4,8 +4,7 @@ export default class StartDayLocator extends BaseLocator {
 
     static startingCash: string = "//input[@placeholder='e.g. 200.000']";
     static getLocatorStartDay = (key: string): string => `//span[normalize-space()='${key}']`;
-    static escapeKeyboard = "//h5[normalize-space()='Start Day']";
-    static notificationSuccess = "//h4[normalize-space()='Synchronization process has completed']";
-
+    static escapeKeyboard = "//mat-icon[normalize-space()='desktop_windows']";
+    static notificationSuccess = "//h4[normalize-space()='Data sync finished']";
 
 }
