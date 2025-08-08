c334323cf515ee0da113f5bf38427e4e92b8e4e2
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-08 14:51:40 +0700

    [MOD] [TC_0205265] Validate Logic When User Able To Add Menu Paket

--- tests/oms/tableListTC/quickServiceTC/quickService-addOrder.spec.ts
+++ tests/oms/tableListTC/quickServiceTC/quickService-addOrder.spec.ts
@@ -186,12 +186,15 @@ test.describe.serial("Quick Service Add Order", () => {
             }, {quickServiceList, bookOrder, order, paymentV2}, testInfo);
         });
 
-    test("[TC_0204002] Validate Logic When User Able To Add Menu Paket",
-        {tag: tag + "@positive"}, async ({quickServiceList, bookOrder, order, editOrder, addOrder}) => {
-            await addNewQuickService(quickServiceList, bookOrder);
-            await selectMenuPaket(order, addOrder);
-            await editOrder.actionButtonFooter("Apply");
-            await order.saveOrder();
+    test("[TC_0205265] Validate Logic When User Able To Add Menu Paket",
+        {tag: tag + "@positive"}, async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2}) => {
+                await makeOrder("AT EXCLUSIVE", bookOrder, quickServiceList);
+                await selectMenuPaket(order, addOrderV2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.saveOrder();
+                await paymentQrESB(paymentV2);
+            }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2}, testInfo);
         });
 
     test("[TC_0204003] Validate Logic When User Able To Add Menu Extra",
