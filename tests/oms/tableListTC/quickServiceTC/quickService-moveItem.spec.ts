3ac7e85ace57f5b21dd12e30e9f5938e5feb200a
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-14 15:33:05 +0700

    [MOD] [TC_0205343] Validate Logic when User can Select All Move Item to the other order from Quick Service to Quick Service

--- tests/oms/tableListTC/quickServiceTC/quickService-moveItem.spec.ts
+++ tests/oms/tableListTC/quickServiceTC/quickService-moveItem.spec.ts
@@ -421,19 +421,28 @@ test.describe.serial("Quick Service Move Item", () => {
             }, {quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}, testInfo);
         });
 
-    test("[TC_0204117] Validate Logic when User can Select All Move Item to the other order from Quick Service to Quick Service",
-        {tag: tags + "@positive"}, async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}) => {
-            await quickServiceList.addOrderQuickService();
-            await bookOrder.setPax(2);
-            await makeOrder(bookOrder);
-            await orderMenuBiasa(order, 5);
-            await sideNavBar.gotoPageTableList();
-            await tableList.gotoQuickService();
-            await quickServiceList.selectSalesNum("last");
-            await order.moveItem();
-            await moveItem.moveItemToSectionQuickService();
-            await moveItem.moveSelectAllItemMenu();
-            await moveItem.actionApplyMoveItem();
+    test("[TC_0205343] Validate Logic when User can Select All Move Item to the other order from Quick Service to Quick Service",
+        {tag: tags + "@positive"}, async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}) => {
+                await makeOrder("AT INCLUSIVE", bookOrder, quickServiceList);
+                await order.saveOrder();
+                await sideNavBar.gotoPageTableList();
+                await tableList.gotoQuickService();
+                await makeOrder("AT INCLUSIVE", bookOrder, quickServiceList);
+                await selectMenuBiasa(order,10);
+                await order.saveOrder();
+                await sideNavBar.gotoPageTableList();
+                await tableList.gotoQuickService();
+                await quickServiceList.clickLastSalesNum();
+                await order.moveItem();
+                await moveItem.moveItemToOtherQuickServiceTransaction();
+                await moveItem.moveSelectAllItemMenu();
+                await moveItem.actionApplyMoveItem();
+                await order.confirmationCloseTable("Yes");
+                await sideNavBar.gotoPageTableList();
+                await tableList.gotoQuickService();
+                await quickServiceList.clickLastSalesNum();
+            }, {quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}, testInfo);
         });
 
     test("[TC_0204118] Validate Logic when User can Deselect All Move Item to the other order from Quick Service to Quick Service",
