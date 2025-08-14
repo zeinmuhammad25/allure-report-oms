fe4612ffba253115c5693db501a89b5f31a7d310
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-14 17:22:24 +0700

    [MOD] [TC_0205348] Validate Logic when User cannot Move Item to the other filled order with different Sales Mode from Quick Service to Quick Service

--- tests/oms/tableListTC/quickServiceTC/quickService-moveItem.spec.ts
+++ tests/oms/tableListTC/quickServiceTC/quickService-moveItem.spec.ts
@@ -506,17 +506,24 @@ test.describe.serial("Quick Service Move Item", () => {
             }, {quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}, testInfo);
         });
 
-    test("[TC_0204122] Validate Logic when User cannot Move Item to the other filled order with different Sales Mode from Quick Service to Quick Service",
-        {tag: tags + "@negative"}, async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}) => {
-            await quickServiceList.addOrderQuickService();
-            await bookOrder.setPax(2);
-            await makeOrder(bookOrder);
-            await orderMenuBiasa(order, 5);
-            await sideNavBar.gotoPageTableList();
-            await tableList.gotoQuickService();
-            await quickServiceList.selectSalesNum("last");
-            await order.moveItem();
-            await moveItem.selectQuickService();
+    test("[TC_0205348] Validate Logic when User cannot Move Item to the other filled order with different Sales Mode from Quick Service to Quick Service",
+        {tag: tags + "@negative"}, async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}) => {
+                await makeOrder("AT EXCLUSIVE", bookOrder, quickServiceList);
+                await selectMenuBiasa(order, 10);
+                await order.saveOrder();
+                await sideNavBar.gotoPageTableList();
+                await tableList.gotoQuickService();
+                await makeOrder("AT INCLUSIVE", bookOrder, quickServiceList);
+                await selectMenuBiasa(order, 10);
+                await order.saveOrder();
+                await sideNavBar.gotoPageTableList();
+                await tableList.gotoQuickService();
+                await quickServiceList.selectSalesNum("last");
+                await order.moveItem();
+                await moveItem.selectQuickService();
+                await moveItem.disableSalesNum();
+            }, {quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}, testInfo);
         });
 
     test("[TC_0204123] Validate Logic when User cannot Move Item to the other empty order with different Sales Mode from Quick Service to Quick Service",
