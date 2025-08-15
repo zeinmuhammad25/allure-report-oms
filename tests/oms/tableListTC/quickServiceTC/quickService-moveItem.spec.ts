ffc29e8216bc449fa7d86a3f13bd4c95afc27669
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-15 09:49:34 +0700

    [MOD] [TC_0205352] Validate Logic when User cannot Move Item without ordered item to the other order from Quick Service to Quick Service

--- tests/oms/tableListTC/quickServiceTC/quickService-moveItem.spec.ts
+++ tests/oms/tableListTC/quickServiceTC/quickService-moveItem.spec.ts
@@ -582,17 +582,17 @@ test.describe.serial("Quick Service Move Item", () => {
             }, {quickServiceList, bookOrder, sideNavBar, tableList, order, topNavBar, signPin}, testInfo);
         });
 
-    test("[TC_0204126] Validate Logic when User cannot Move Item without ordered item to the other order from Quick Service to Quick Service",
-        {tag: tags + "@negative"}, async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}) => {
-            await quickServiceList.addOrderQuickService();
-            await bookOrder.setPax(2);
-            await makeOrder(bookOrder);
-            await order.saveOrder();
-            await sideNavBar.gotoPageTableList();
-            await tableList.gotoQuickService();
-            await quickServiceList.selectSalesNum("last");
-            await order.moveItem();
-            await moveItem.moveItemToSectionQuickService();
+    test("[TC_0205352] Validate Logic when User cannot Move Item without ordered item to the other order from Quick Service to Quick Service",
+        {tag: tags + "@negative"}, async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem},testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}) => {
+                await makeOrder("AT INCLUSIVE", bookOrder, quickServiceList);
+                await order.saveOrder();
+                await sideNavBar.gotoPageTableList();
+                await tableList.gotoQuickService();
+                await quickServiceList.selectSalesNum("last");
+                await order.moveItem();
+                await moveItem.moveItemToSectionQuickService();
+            }, {quickServiceList, bookOrder, sideNavBar, tableList, order, moveItem}, testInfo);
         });
 
     test("[TC_0204127] Validate Logic when User cannot Move Item from Quick Service to Quick Service while having no ordered items and not saving order first",
