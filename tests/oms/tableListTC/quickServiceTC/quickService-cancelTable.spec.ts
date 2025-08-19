54899abdae926b53bef7cc521315271728fb01b7
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-19 16:36:04 +0700

    [ADD] [TC_0205376] Validate Logic when User can't Cancel Order before saving order AND ORDERING CONFIG

---
+++ tests/oms/tableListTC/quickServiceTC/quickService-cancelTable.spec.ts
@@ -0,0 +1,82 @@
+import {test} from "../../injection";
+import MenuList from "../../../../src/modules/oms/objects/menuList";
+import OrderScenario from "../../../../src/modules/oms/tableList/order/order.scenario";
+import QuickServiceListScenario from "../../../../src/modules/oms/tableList/quickServiceList/quickServiceList.scenario";
+import BookOrderScenario from "../../../../src/modules/oms/tableList/components/bookOrder/bookOrder.scenario";
+import AddOrderV2Scenario from "../../../../src/modules/oms/tableList/order/components/addOrderV2/addOrderV2.scenario";
+import PaymentV2Scenario from "../../../../src/modules/oms/tableList/paymentV2/paymentV2.scenario";
+import PaymentList from "../../../../src/modules/oms/objects/paymentList";
+import {safeTest} from "../../../../src/base/utils/safeTest";
+
+test.setTimeout(600000);
+test.describe.serial("Quick Service Add Order", () => {
+    const tag = "@smokeTest @oms @quickService @addOrder ";
+
+    const makeOrder = async (
+        salesMode: "AT EXCLUSIVE" | "AT INCLUSIVE", bookOrder: BookOrderScenario, quickServiceList: QuickServiceListScenario
+    ) => {
+        await quickServiceList.addOrderQuickService();
+        await bookOrder.setPax(2);
+        await bookOrder.selectSalesMode(salesMode);
+        await bookOrder.applyQuickService();
+        await bookOrder.skipCustomerPhoneNumber();
+    };
+
+    const selectMultipleMenuBiasa = async (order: OrderScenario, qty1: number, qty2: number, qty3: number) => {
+        await order.selectCategoryMenu(MenuList.atCategory.name);
+        await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuBiasa.name);
+        await order.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaBakar.name, qty1);
+        await order.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaGoreng.name, qty2);
+        await order.selectMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaRebus.name, qty3);
+    };
+
+
+    const cancelTableSelectNotes = async (order: OrderScenario, reason: "Cancel" | "Tidak Jadi" | "Testing A" | "Testing B") => {
+        await order.cancelTableSelectNotes(reason);
+        await order.confirmationCloseTable("Yes");
+    };
+
+    const cancelTable = async (order: OrderScenario) => {
+        await order.cancelTable("Cancel");
+        await order.confirmationCloseTable("Yes");
+    };
+
+    const UndoCancelTable = async (order: OrderScenario, reason: "Cancel" | "Tidak Jadi" | "Testing A" | "Testing B") => {
+        await order.UndoCancelTable(reason);
+    };
+
+    test.beforeEach(async ({terminalID, signPin, tableList}) => {
+        const testWithAuthentication = [
+            "[TC_0205376] Validate Logic when User can't Cancel Order before saving order"
+        ];
+        if (testWithAuthentication.includes(test.info().title)) {
+            await terminalID.goHere();
+            await terminalID.performTerminalID();
+            await signPin.inputPinByTouch("22");
+            await signPin.validateShowStarCash("20.000");
+            await signPin.storeAuthState();
+        }
+        await tableList.goHere();
+    });
+
+    test.afterEach(async ({tableList}) => {
+        await Promise.all([
+            tableList.cancelAllQuickServices(),
+            tableList.cancelAllTables()
+        ]);
+    });
+    test("[TC_0205376] Validate Logic when User can't Cancel Order before saving order",
+        {tag: tag + "@Negative"}, async ({quickServiceList, bookOrder, order}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, order}) => {
+                await makeOrder("AT EXCLUSIVE", bookOrder, quickServiceList);
+                await selectMultipleMenuBiasa(order,1,1,1);
+                await order.disabledCancelTable();
+            }, {quickServiceList, bookOrder, order}, testInfo);
+        });
+
+
+
+
+
+
+});
