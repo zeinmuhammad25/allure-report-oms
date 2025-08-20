0a2798841f45330bd9c4d30816bdb30407f0a677
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-20 11:41:43 +0700

    [ADD] buttonCloseSyncPopup locator

--- src/modules/oms/tools/synchronizeData/synchronizeData.locator.ts
+++ src/modules/oms/tools/synchronizeData/synchronizeData.locator.ts
@@ -7,5 +7,6 @@ export default class SynchronizeDataLocator extends BaseLocator {
     static buttonUpdateSetting: string = "//span[normalize-space()='Update Setting']";
     static buttonSynchronize: string = "//span[normalize-space()='Synchronize']";
     static buttonSelectAll: string = "//label[@for='mat-checkbox-6-input']//span[@class='mat-checkbox-label']";
+    static buttonCloseSyncPopup: string = "//span[normalize-space()='Close']";
 
 }
