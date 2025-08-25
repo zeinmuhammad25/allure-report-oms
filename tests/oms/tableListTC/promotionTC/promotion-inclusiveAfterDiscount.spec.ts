dd1dbce04f7504169e0ed6eedb9ab14eae12db40
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-25 12:05:22 +0700

    [ADD] [TC_0205452] Validate Logic When User Apply Promotion Head - Order Pages - Menu Discount Rp Menu Category

--- tests/oms/tableListTC/promotionTC/promotion-inclusiveAfterDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-inclusiveAfterDiscount.spec.ts
@@ -318,4 +318,25 @@ test.describe.serial("Promotion Inclusive After Discount", () => {
             }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
+    test("[TC_0205452] Validate Logic When User Apply Promotion Head - Order Pages - Menu Discount Rp Menu Category",
+        {tag: tags + "@positive"}, async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
+                await makeOrder("AT INCLUSIVE", bookOrder, quickServiceList);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,3,2,1)
+                await order.addPromotion();
+                await promotionList.searchPromotion("MENU DISC RP MENU CATEGORY");
+                await promotionList.selectPromotion("MENU DISC RP MENU CATEGORY");
+                await order.saveOrder();
+                await paymentCashFull(paymentV2);
+            }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
+        });
+
 });
