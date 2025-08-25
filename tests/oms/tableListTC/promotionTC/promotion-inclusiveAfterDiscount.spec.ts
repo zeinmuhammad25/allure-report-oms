15b3a97b3e4d73403304f41ea8a415b603ac6a3b
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-25 16:47:04 +0700

    [ADD] [TC_0205470] Validate Logic When User Apply Promotion - Apply All Qty - FREE ITEM ALL CATEGORY

--- tests/oms/tableListTC/promotionTC/promotion-inclusiveAfterDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-inclusiveAfterDiscount.spec.ts
@@ -697,4 +697,25 @@ test.describe.serial("Promotion Inclusive After Discount", () => {
             }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
+    test("[TC_0205470] Validate Logic When User Apply Promotion - Apply All Qty - FREE ITEM ALL CATEGORY",
+        {tag: tags + "@positive"}, async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2, editOrderV2}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2, editOrderV2}) => {
+                await makeOrder("AT INCLUSIVE", bookOrder, quickServiceList);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,8,8,8)
+                await order.clickMenuDetail(MenuList.atCategory.atMenuBiasa.atMenuBiasaBakar.name);
+                await editOrderV2.addPromotionMenu();
+                await editOrderV2.applyViaSearchPromotionMenu("FREE ITEM ALL CATEGORY");
+                await order.saveOrder();
+                await paymentCashFull(paymentV2);
+            }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2, editOrderV2}, testInfo);
+        });
+
 });
