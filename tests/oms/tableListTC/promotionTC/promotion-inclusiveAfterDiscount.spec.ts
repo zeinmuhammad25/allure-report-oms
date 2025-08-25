0275cf5ad42e715eb64f80607b8c0668eea2d1ab
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-25 17:48:17 +0700

    [ADD] [TC_0205476] Validate Logic When User Apply Promotion Head Then Cancel 1 Menu - Order Pages - Discount % Menu

--- tests/oms/tableListTC/promotionTC/promotion-inclusiveAfterDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-inclusiveAfterDiscount.spec.ts
@@ -822,4 +822,26 @@ test.describe.serial("Promotion Inclusive After Discount", () => {
             }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
+    test("[TC_0205476] Validate Logic When User Apply Promotion Head Then Cancel 1 Menu - Order Pages - Discount % Menu",
+        {tag: tags + "@negative"}, async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
+                await makeOrder("AT INCLUSIVE", bookOrder, quickServiceList);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,8,8,8)
+                await order.deleteMenu(MenuList.atCategory.atMenuPaket.atMenuPaketMahal.name);
+                await order.addPromotion();
+                await promotionList.searchPromotion("DISCOUNT % MENU");
+                await promotionList.selectPromotion("DISCOUNT % MENU");
+                await order.saveOrder();
+                await paymentQrESB(paymentV2);
+            }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
+        });
+
 });
