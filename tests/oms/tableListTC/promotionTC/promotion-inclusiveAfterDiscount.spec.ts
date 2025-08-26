15beb0289260234881f1cecd97045d983f21bf9d
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-26 09:36:38 +0700

    [ADD] [TC_0205485] Validate Logic When User Apply Promotion Head Then Cancel 1 Menu - Order Pages - Menu Discount Rp Menu Category Detail

--- tests/oms/tableListTC/promotionTC/promotion-inclusiveAfterDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-inclusiveAfterDiscount.spec.ts
@@ -1019,4 +1019,26 @@ test.describe.serial("Promotion Inclusive After Discount", () => {
                 await paymentQrESB(paymentV2);
             }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
+
+    test("[TC_0205485] Validate Logic When User Apply Promotion Head Then Cancel 1 Menu - Order Pages - Menu Discount Rp Menu Category Detail",
+        {tag: tags + "@negative"}, async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
+                await makeOrder("AT INCLUSIVE", bookOrder, quickServiceList);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,5,6,7)
+                await order.deleteMenu(MenuList.atCategory.atMenuPaket.atMenuPaketMahal.name);
+                await order.addPromotion();
+                await promotionList.searchPromotion("MENU DISC RP MENU CATEGORY DETAIL");
+                await promotionList.selectPromotion("MENU DISC RP MENU CATEGORY DETAIL");
+                await order.saveOrder();
+                await paymentQrESB(paymentV2);
+            }, {quickServiceList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
+        });
 });
