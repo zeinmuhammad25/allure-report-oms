2c46bfc2f5311b63b5597f2618de900626a889d6
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-26 16:33:12 +0700

    [ADD] [TC_0205510] Validate Logic When User Apply Promotion Head - Payment Pages - Discount % Menu

--- tests/oms/tableListTC/promotionTC/promotion-exclusiveBeforeDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-exclusiveBeforeDiscount.spec.ts
@@ -552,6 +552,31 @@ test.describe.serial("Promotion Exclusive Before Discount", () => {
             }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
-
+    test("[TC_0205510] Validate Logic When User Apply Promotion Head - Payment Pages - Discount % Menu",
+        {tag: tags + "@positive"}, async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
+            await safeTest(async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
+                await tableList.selectRoom(Table.acRoom.name);
+                await tableList.selectTable(Table.acRoom.ac1.name);
+                await makeOrder("AT EXCLUSIVE", bookOrder);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,4,2,3)
+                await order.saveOrder();
+                await tableList.selectRoom(Table.acRoom.name);
+                await tableList.selectTable(Table.acRoom.ac1.name);
+                await order.printNowPrintingSetting();
+                await order.gotoPayment();
+                await paymentV2.paymentType(PaymentObject.AddPromo);
+                await promotionList.searchPromotion("DISCOUNT % MENU");
+                await promotionList.selectPromotion("DISCOUNT % MENU");
+                await paymentCashFull(paymentV2);
+            }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
+        });
 
 });
