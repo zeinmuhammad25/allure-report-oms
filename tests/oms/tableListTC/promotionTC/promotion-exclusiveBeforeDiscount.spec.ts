7986e8cb1f9cc9721ea17d9f3d5800fdfe0c25d7
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-26 14:10:49 +0700

    [ADD] [TC_0205497] Validate Logic When User Apply Promotion Head - Order Pages - Type: Discount % Menu Category

--- tests/oms/tableListTC/promotionTC/promotion-exclusiveBeforeDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-exclusiveBeforeDiscount.spec.ts
@@ -202,4 +202,31 @@ test.describe.serial("Promotion Exclusive Before Discount", () => {
             }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
+    test("[TC_0205497] Validate Logic When User Apply Promotion Head - Order Pages - Type: Discount % Menu Category",
+        {tag: tags + "@positive"}, async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
+            await safeTest(async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
+                await tableList.selectRoom(Table.acRoom.name);
+                await tableList.selectTable(Table.acRoom.ac4.name);
+                await makeOrder("AT EXCLUSIVE", bookOrder);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,5,4,3)
+                await order.addPromotion();
+                await promotionList.searchPromotion("DISCOUNT % MENU CATEGORY");
+                await promotionList.selectPromotion("DISCOUNT % MENU CATEGORY");
+                await order.saveOrder();
+                await tableList.selectRoom(Table.acRoom.name);
+                await tableList.selectTable(Table.acRoom.ac4.name);
+                await order.printNowPrintingSetting();
+                await order.gotoPayment();
+                await paymentQrESB(paymentV2);
+            }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
+        });
+
 });
