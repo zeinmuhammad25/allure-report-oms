7a4552dd09ed6e03592731e2ed00b5c60971b56e
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-27 14:47:00 +0700

    [ADD][TC_0205540] Validate Logic When User Apply Promotion Head Then Cancel 1 Menu - Order Pages - FREE ITEM MENU CATEGORY

--- tests/oms/tableListTC/promotionTC/promotion-exclusiveBeforeDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-exclusiveBeforeDiscount.spec.ts
@@ -1395,13 +1395,37 @@ test.describe.serial("Promotion Exclusive Before Discount", () => {
                 await order.clickMenuDetail(MenuList.atCategory.atMenuBiasa.atMenuBiasaBakar.name);
                 await editOrderV2.addPromotionMenu();
                 await editOrderV2.applyViaSearchPromotionMenu("FREE ITEM ALL CATEGORY");
-                await order.clickMenuDetail(MenuList.atCategory.atMenuBiasa.atMenuBiasaGoreng.name);
-                await editOrderV2.addPromotionMenu();
-                await editOrderV2.applyViaSearchPromotionMenu("FREE ITEM ALL CATEGORY");
-                await order.deleteMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaGoreng.name);
                 await order.saveOrder();
                 await tableList.selectRoom(Table.smokingRoom.name);
                 await tableList.selectTable(Table.smokingRoom.sr3.name);
+                await order.printNowPrintingSetting();
+                await order.gotoPayment();
+                await paymentCashFull(paymentV2);
+            }, {tableList, bookOrder, order, addOrderV2, paymentV2, editOrderV2}, testInfo);
+        });
+
+    test("[TC_0205540] Validate Logic When User Apply Promotion Head Then Cancel 1 Menu - Order Pages - FREE ITEM MENU CATEGORY",
+        {tag: tags + "@negative"}, async ({tableList, bookOrder, order, addOrderV2, paymentV2, editOrderV2}, testInfo) => {
+            await safeTest(async ({tableList, bookOrder, order, addOrderV2, paymentV2, editOrderV2}) => {
+                await tableList.selectRoom(Table.smokingRoom.name);
+                await tableList.selectTable(Table.smokingRoom.sr4.name);
+                await makeOrder("AT EXCLUSIVE", bookOrder);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,10,10,10)
+                await order.clickMenuDetail(MenuList.atCategory.atMenuBiasa.atMenuBiasaGoreng.name);
+                await editOrderV2.addPromotionMenu();
+                await editOrderV2.applyViaSearchPromotionMenu("FREE ITEM MENU CATEGORY");
+                await order.deleteMenu(MenuList.atCategory.atMenuBiasa.atMenuBiasaGoreng.name);
+                await order.saveOrder();
+                await tableList.selectRoom(Table.smokingRoom.name);
+                await tableList.selectTable(Table.smokingRoom.sr4.name);
                 await order.printNowPrintingSetting();
                 await order.gotoPayment();
                 await paymentCashFull(paymentV2);
