06baa1193f47648d8f31709de85fb7092232a900
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-28 11:16:44 +0700

    [ADD] [TC_0205559] Validate Logic When User Apply Promotion Head - Order Pages - Open Bill Dicount %

--- tests/oms/tableListTC/promotionTC/promotion-exclusiveAfterDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-exclusiveAfterDiscount.spec.ts
@@ -489,4 +489,31 @@ test.describe.serial("Promotion Exclusive After Discount", () => {
             }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
+    test("[TC_0205559] Validate Logic When User Apply Promotion Head - Order Pages - Open Bill Dicount %",
+        {tag: tags + "@positive"}, async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
+            await safeTest(async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
+                await tableList.selectRoom(Table.smokingRoom.name);
+                await tableList.selectTable(Table.smokingRoom.sr2.name);
+                await makeOrder("AT EXCLUSIVE", bookOrder);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,10,10,10)
+                await order.addPromotion();
+                await promotionList.searchPromotion("OPEN BILL DISCOUNT %");
+                await promotionList.selectPromotion("OPEN BILL DISCOUNT %", 50);
+                await order.saveOrder();
+                await tableList.selectRoom(Table.smokingRoom.name);
+                await tableList.selectTable(Table.smokingRoom.sr2.name);
+                await order.printNowPrintingSetting();
+                await order.gotoPayment();
+                await paymentQrESB(paymentV2);
+            }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
+        });
+
 });
