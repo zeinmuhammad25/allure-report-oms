28211331978cd919e0c087c291c281d17fab72ec
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-28 10:52:28 +0700

    [ADD] [TC_0205550] Validate Logic When User Apply Promotion Head - Order Pages - Type: Discount % Menu Category Detail

--- tests/oms/tableListTC/promotionTC/promotion-exclusiveAfterDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-exclusiveAfterDiscount.spec.ts
@@ -219,7 +219,6 @@ test.describe.serial("Promotion Exclusive After Discount", () => {
             }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
-
     test("[TC_0205549] Validate Logic When User Apply Promotion Head - Order Pages - Type: Discount % Menu Category",
         {tag: tags + "@positive"}, async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
             await safeTest(async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
@@ -244,6 +243,33 @@ test.describe.serial("Promotion Exclusive After Discount", () => {
                 await order.printNowPrintingSetting();
                 await order.gotoPayment();
                 await paymentQrESB(paymentV2);
+            }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
+        });
+
+    test("[TC_0205550] Validate Logic When User Apply Promotion Head - Order Pages - Type: Discount % Menu Category Detail",
+        {tag: tags + "@positive"}, async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
+            await safeTest(async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
+                await tableList.selectRoom(Table.smokingRoom.name);
+                await tableList.selectTable(Table.smokingRoom.sr1.name);
+                await makeOrder("AT EXCLUSIVE", bookOrder);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,6,7,8)
+                await order.addPromotion();
+                await promotionList.searchPromotion("DISCOUNT % MENU CATEGORY DETAIL");
+                await promotionList.selectPromotion("DISCOUNT % MENU CATEGORY DETAIL");
+                await order.saveOrder();
+                await tableList.selectRoom(Table.smokingRoom.name);
+                await tableList.selectTable(Table.smokingRoom.sr1.name);
+                await order.printNowPrintingSetting();
+                await order.gotoPayment();
+                await paymentCashFull(paymentV2);
             }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
