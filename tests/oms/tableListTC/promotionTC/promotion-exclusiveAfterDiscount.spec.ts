aaf5ec79b109921359f6170911f44913c3401416
reza fadhilah <reza.fadhilah@esb.co.id>
2025-08-28 16:19:32 +0700

    [ADD][TC_0205589] Validate Logic When User Apply Promotion Head Then Cancel Order - Order Pages - Open Bill Dicount %

--- tests/oms/tableListTC/promotionTC/promotion-exclusiveAfterDiscount.spec.ts
+++ tests/oms/tableListTC/promotionTC/promotion-exclusiveAfterDiscount.spec.ts
@@ -1339,5 +1339,33 @@ test.describe.serial("Promotion Exclusive After Discount", () => {
             }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
         });
 
+    test("[TC_0205589] Validate Logic When User Apply Promotion Head Then Cancel Order - Order Pages - Open Bill Dicount %",
+        {tag: tags + "@negative"}, async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo) => {
+            await safeTest(async ({tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}) => {
+                await tableList.selectRoom(Table.smokingRoom.name);
+                await tableList.selectTable(Table.smokingRoom.sr2.name);
+                await makeOrder("AT EXCLUSIVE", bookOrder);
+                await order.selectCategoryMenu(MenuList.atCategory.name);
+                await orderMenuPaketMahal(order, addOrderV2,2);
+                await selectMenuExtra(addOrderV2, 5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderMenuPaketMurah(order, addOrderV2,2);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await order.selectCategoryDetailMenu(MenuList.atCategory.atMenuPaket.name);
+                await orderSingleMenu(order,7,8,8)
+                await order.deleteMenu(MenuList.atCategory.atMenuPaket.atMenuPaketMurah.name);
+                await order.addPromotion();
+                await promotionList.searchPromotion("OPEN BILL DISCOUNT %");
+                await promotionList.selectPromotion("OPEN BILL DISCOUNT %", 70);
+                await order.saveOrder();
+                await tableList.selectRoom(Table.smokingRoom.name);
+                await tableList.selectTable(Table.smokingRoom.sr2.name);
+                await order.printNowPrintingSetting();
+                await order.gotoPayment();
+                await paymentQrESB(paymentV2);
+            }, {tableList, bookOrder, order, addOrderV2, paymentV2, promotionList}, testInfo);
+        });
+
 
 });
