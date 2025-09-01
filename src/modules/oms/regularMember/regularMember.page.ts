6cecbfefd74f75f72deeaf29bac5a85cd2b9d67c
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-01 14:50:59 +0700

    [ADD] REGULAR MEMBER PAGE

---
+++ src/modules/oms/regularMember/regularMember.page.ts
@@ -0,0 +1,16 @@
+import BaseOmsPage from "../base-oms-page";
+import RegularMemberScenario from "./regularMember.scenario";
+import Element from "../../../base/objects/Element";
+import RegularMemberLocator from "./regularMember.locator";
+
+export default class RegularMemberPage extends BaseOmsPage implements RegularMemberScenario {
+    pageUrl: () => string;
+
+    shouldHave(): Element[] {
+        return [];
+    }
+
+    async createdRegularMember(): Promise<void> {
+
+    }
+}
