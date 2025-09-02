ad6da03e7498495cfe1f04a81e649914364d273b
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-02 14:57:17 +0700

    [ADD] [TC_0205594] Validate Edit from Detail member on Regular Member List

---
+++ tests/oms/internalMember/regularMember.spec.ts
@@ -0,0 +1,47 @@
+import {test} from "../injection";
+import {safeTest} from "../../../src/base/utils/safeTest";
+
+test.setTimeout(100000);
+test.describe.serial("Internal Member", () => {
+    const tags = "@smokeTest @oms @Internal Member";
+
+    test.beforeEach(async ({terminalID, signPin, sideNavBar}) => {
+        const testWithAuthentication = [
+            "[TC_0205594] Validate Edit from Detail member on Regular Member List"
+        ];
+
+        if (testWithAuthentication.includes(test.info().title)) {
+            await terminalID.goHere();
+            await terminalID.performTerminalID();
+            await signPin.inputPinByTouch("22");
+            await signPin.validateShowStarCash("20.000");
+            await signPin.storeAuthState();
+        }
+        await sideNavBar.gotoPageRegularMember();
+    });
+
+    test.afterEach(async ({}) => {
+
+    });
+
+    test("[TC_0205594] Validate Edit from Detail member on Regular Member List",
+        {tag: tags + "@positive"}, async ({regularMember}, testInfo) => {
+            await safeTest(async ({}) => {
+                await regularMember.searchRegularMember("MRM0001");
+                await regularMember.selectAndEditMember("MRM0001");
+                await regularMember.inputFormMemberName({}, "TEST MEMBER AT 1");
+                await regularMember.selectFormGander("Male");
+                await regularMember.clickButtonDate();
+                await regularMember.selectMonthAndYear("Choose month and year");
+                await regularMember.datePickerYear("2025");
+                await regularMember.datePickerMonth("JAN");
+                await regularMember.datePickerDate("1");
+                await regularMember.inputFormPhone({}, "12345678964");
+                await regularMember.inputFormEmail({}, "test@esb.co.id");
+                await regularMember.inputFormAddress({}, "TEST ESB AT MEMBER");
+                await regularMember.updateRegularMember();
+            }, {regularMember}, testInfo);
+        });
+
+
+});
