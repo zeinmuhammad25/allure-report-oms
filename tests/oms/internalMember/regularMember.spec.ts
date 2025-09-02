9d7a63600fd3d69ed875dd33c5d389a05eabbd7c
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-02 15:21:25 +0700

    [ADD] [TC_0205595] Validate Cancel Edit from Detail member on Regular Member List

--- tests/oms/internalMember/regularMember.spec.ts
+++ tests/oms/internalMember/regularMember.spec.ts
@@ -5,7 +5,7 @@ test.setTimeout(100000);
 test.describe.serial("Internal Member", () => {
     const tags = "@smokeTest @oms @Internal Member";
 
-    test.beforeEach(async ({terminalID, signPin, sideNavBar}) => {
+    test.beforeEach(async ({terminalID, signPin, sideNavBar, tableList}) => {
         const testWithAuthentication = [
             "[TC_0205594] Validate Edit from Detail member on Regular Member List"
         ];
@@ -17,6 +17,7 @@ test.describe.serial("Internal Member", () => {
             await signPin.validateShowStarCash("20.000");
             await signPin.storeAuthState();
         }
+        await tableList.goHere();
         await sideNavBar.gotoPageRegularMember();
     });
 
@@ -40,6 +41,25 @@ test.describe.serial("Internal Member", () => {
                 await regularMember.inputFormEmail({}, "test@esb.co.id");
                 await regularMember.inputFormAddress({}, "TEST ESB AT MEMBER");
                 await regularMember.updateRegularMember();
+            }, {regularMember}, testInfo);
+        });
+
+    test("[TC_0205595] Validate Cancel Edit from Detail member on Regular Member List",
+        {tag: tags + "@positive"}, async ({regularMember}, testInfo) => {
+            await safeTest(async ({}) => {
+                await regularMember.searchRegularMember("DPRYH00000014");
+                await regularMember.selectAndEditMember("DPRYH00000014");
+                await regularMember.inputFormMemberName({}, "TEST MEMBER AT 1");
+                await regularMember.selectFormGander("Male");
+                await regularMember.clickButtonDate();
+                await regularMember.selectMonthAndYear("Choose month and year");
+                await regularMember.datePickerYear("2025");
+                await regularMember.datePickerMonth("JAN");
+                await regularMember.datePickerDate("1");
+                await regularMember.inputFormPhone({}, "12345678964");
+                await regularMember.inputFormEmail({}, "test@esb.co.id");
+                await regularMember.inputFormAddress({}, "TEST ESB AT MEMBER");
+                await regularMember.cancelCreateAndUpdateMember();
             }, {regularMember}, testInfo);
         });
 
