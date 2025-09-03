9a5cb2794b315f493278d268ac3efd66bb5c98aa
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-03 17:44:04 +0700

    [ADD] createdMemberDeposit logic

--- src/modules/oms/regularMemberDeposit/regularMemberDeposit.page.ts
+++ src/modules/oms/regularMemberDeposit/regularMemberDeposit.page.ts
@@ -1,11 +1,17 @@
 import BaseOmsPage from "../base-oms-page";
 import RegularMemberDepositScenario from "./regularMemberDeposit.scenario";
 import Element from "../../../base/objects/Element";
+import RegularMemberDepositLocator from "./regularMemberDeposit.locator";
 
-export default class RegularMemberDepositPage extends BaseOmsPage implements RegularMemberDepositScenario{
+export default class RegularMemberDepositPage extends BaseOmsPage implements RegularMemberDepositScenario {
     pageUrl: () => string;
 
     shouldHave(): Element[] {
         return [];
+    }
+
+    async createdMemberDeposit(): Promise<void> {
+        await this.expectVisible(RegularMemberDepositLocator.btnAddDepositMember);
+        await this.click(RegularMemberDepositLocator.btnAddDepositMember);
     }
 }
