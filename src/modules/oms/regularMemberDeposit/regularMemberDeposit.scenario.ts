bf28f558984f1a8c745335196ecf54a44df94f5a
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-03 17:44:17 +0700

    [ADD] createdMemberDeposit scenario

--- src/modules/oms/regularMemberDeposit/regularMemberDeposit.scenario.ts
+++ src/modules/oms/regularMemberDeposit/regularMemberDeposit.scenario.ts
@@ -2,5 +2,5 @@ import BaseScenario from "../../../base/base-scenario";
 
 
 export default interface RegularMemberDepositScenario extends BaseScenario {
-
+    createdMemberDeposit(): Promise<void>;
 }
