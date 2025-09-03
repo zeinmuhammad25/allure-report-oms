a22f7b5d68d65f8e686a62290fd939e79951114f
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-03 17:47:21 +0700

    [ADD] searchMemberDeposit logic

--- src/modules/oms/regularMemberDeposit/regularMemberDeposit.page.ts
+++ src/modules/oms/regularMemberDeposit/regularMemberDeposit.page.ts
@@ -2,6 +2,7 @@ import BaseOmsPage from "../base-oms-page";
 import RegularMemberDepositScenario from "./regularMemberDeposit.scenario";
 import Element from "../../../base/objects/Element";
 import RegularMemberDepositLocator from "./regularMemberDeposit.locator";
+import RegularMemberLocator from "../regularMember/regularMember.locator";
 
 export default class RegularMemberDepositPage extends BaseOmsPage implements RegularMemberDepositScenario {
     pageUrl: () => string;
@@ -14,4 +15,13 @@ export default class RegularMemberDepositPage extends BaseOmsPage implements Reg
         await this.expectVisible(RegularMemberDepositLocator.btnAddDepositMember);
         await this.click(RegularMemberDepositLocator.btnAddDepositMember);
     }
+
+    async searchMemberDeposit(valueMember: string): Promise<void> {
+        await this.expectVisible(RegularMemberDepositLocator.filedSearchDeposit);
+        await this.click(RegularMemberDepositLocator.filedSearchDeposit);
+        await this.fill(RegularMemberDepositLocator.filedSearchDeposit, valueMember);
+        await this.expectVisible(RegularMemberDepositLocator.escapeKeyboardDepositMember);
+        await this.click(RegularMemberDepositLocator.escapeKeyboardDepositMember);
+    }
+
 }
