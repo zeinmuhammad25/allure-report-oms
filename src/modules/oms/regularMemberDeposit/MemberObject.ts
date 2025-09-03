72b40840ca061d59f0b1fffa06afdec0fd9baef6
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-03 16:41:41 +0700

    [ADD] MEMBER OBJECT FOR PAYMENT AND MEMBER BOARD

---
+++ src/modules/oms/regularMemberDeposit/MemberObject.ts
@@ -0,0 +1,24 @@
+/**
+ * Enum to define payment types, payment methods,
+ * button directions, and payment actions used in the application.
+ */
+export enum MemberObject {
+    //category
+    CashCatMember = "CASH",
+    CardCatMember = "CARD",
+
+    // Payment Method
+    CashPaymentMember = "CASH PAYMENT",
+    CashMember = "CASH",
+    DebitBcaMember = "BCA Debit",
+
+    // Member Board
+    GridMemberBoard100000 = "100.000",
+    GridMemberBoard50000 = "50.000",
+    GridMemberBoard10000 = "10.000",
+    GridMemberBoard500 = "500",
+    GridMemberBoard100 = "100",
+    GridMemberBoard10 = "10",
+    GridMemberBoard1 = "1",
+    GridMemberBoard0 = "0"
+}
