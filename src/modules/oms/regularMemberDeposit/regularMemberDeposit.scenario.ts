dd4dd1af910a4869424f3ec58c300af0531ffa90
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-03 17:47:32 +0700

    [ADD] searchMemberDeposit scenario

--- src/modules/oms/regularMemberDeposit/regularMemberDeposit.scenario.ts
+++ src/modules/oms/regularMemberDeposit/regularMemberDeposit.scenario.ts
@@ -3,4 +3,6 @@ import BaseScenario from "../../../base/base-scenario";
 
 export default interface RegularMemberDepositScenario extends BaseScenario {
     createdMemberDeposit(): Promise<void>;
+
+    searchMemberDeposit(valueMember: string): Promise<void>;
 }
