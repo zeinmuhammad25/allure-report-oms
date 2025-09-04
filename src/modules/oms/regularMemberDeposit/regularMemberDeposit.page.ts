2f7a6032b0926b0e13d698137750461242ccd813
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-04 14:51:04 +0700

    [MOD] searchMemberList

--- src/modules/oms/regularMemberDeposit/regularMemberDeposit.page.ts
+++ src/modules/oms/regularMemberDeposit/regularMemberDeposit.page.ts
@@ -109,6 +109,7 @@ export default class RegularMemberDepositPage extends BaseOmsPage implements Reg
     async searchMemberList(): Promise<void> {
         await this.expectVisible(RegularMemberDepositLocator.fieldSearchMember);
         await this.click(RegularMemberDepositLocator.fieldSearchMember);
+        await this.click(RegularMemberDepositLocator.escapeKeyboardMemberList);
     }
 
 
