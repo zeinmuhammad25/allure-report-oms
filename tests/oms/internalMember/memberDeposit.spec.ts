5a04c379ec71aa19d0ef5f20020c2d65daeb777b
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-09 10:48:22 +0700

    [ADD] [TC_0205610] Validate Mandatory field when Add Deposit for member on Regular Member Deposit

--- tests/oms/internalMember/memberDeposit.spec.ts
+++ tests/oms/internalMember/memberDeposit.spec.ts
@@ -52,4 +52,18 @@ test.describe.serial("Internal Member", () => {
             }, {regularMemberDeposit}, testInfo);
         });
 
+    test("[TC_0205610] Validate Mandatory field when Add Deposit for member on Regular Member Deposit",
+        {tag: tags + "@positive"}, async ({regularMemberDeposit}, testInfo) => {
+            await safeTest(async ({}) => {
+                await regularMemberDeposit.createdMemberDeposit();
+                await regularMemberDeposit.applyRegularMemberNameList();
+                await regularMemberDeposit.searchMemberList("REZA_CUSTOMER");
+                await regularMemberDeposit.selectRegularMemberNameList("REZA_CUSTOMER");
+                await regularMemberDeposit.paymentMemberCategoryType(MemberObject.CashCatMember);
+                await regularMemberDeposit.paymentMethodMember(MemberObject.CashPaymentMember);
+                await regularMemberDeposit.inputTotalDeposit("100.000");
+                await regularMemberDeposit.clearTotalDeposit();
+            }, {regularMemberDeposit}, testInfo);
+        });
+
 });
