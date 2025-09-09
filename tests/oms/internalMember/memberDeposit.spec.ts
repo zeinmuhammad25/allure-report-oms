bf6a4c54fe867095fe55b2042f677f6a65454196
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-09 09:41:51 +0700

    [ADD] [TC_0205608] Add Deposit for member on Regular Member Deposit

--- tests/oms/internalMember/memberDeposit.spec.ts
+++ tests/oms/internalMember/memberDeposit.spec.ts
@@ -1,11 +1,15 @@
 import {test} from "../injection";
+import {safeTest} from "../../../src/base/utils/safeTest";
+import {MemberObject} from "../../../src/modules/oms/regularMemberDeposit/MemberObject";
 
 test.setTimeout(100000);
 test.describe.serial("Internal Member", () => {
     const tags = "@smokeTest @oms @MemberDeposit";
 
     test.beforeEach(async ({terminalID, signPin, sideNavBar, tableList}) => {
-        const testWithAuthentication = [];
+        const testWithAuthentication = [
+            "[TC_0205608] Add Deposit for member on Regular Member Deposit"
+        ];
 
         if (testWithAuthentication.includes(test.info().title)) {
             await terminalID.goHere();
@@ -13,15 +17,29 @@ test.describe.serial("Internal Member", () => {
             await signPin.inputPinByTouch("22");
             await signPin.validateShowStarCash("20.000");
             await signPin.storeAuthState();
+            await tableList.goHere();
         }
-        await tableList.goHere();
         await signPin.closePopUpAlert();
-        await sideNavBar.gotoPageRegularMember();
+        await sideNavBar.gotoPageRegularMemberDeposit();
     });
 
     test.afterEach(async ({}) => {
 
     });
 
+    test("[TC_0205608] Add Deposit for member on Regular Member Deposit",
+        {tag: tags + "@positive"}, async ({regularMemberDeposit}, testInfo) => {
+            await safeTest(async ({}) => {
+                await regularMemberDeposit.createdMemberDeposit();
+                await regularMemberDeposit.applyRegularMemberNameList();
+                await regularMemberDeposit.searchMemberList("REZA_CUSTOMER");
+                await regularMemberDeposit.selectRegularMemberNameList("REZA_CUSTOMER");
+                await regularMemberDeposit.paymentMemberCategoryType(MemberObject.CashCatMember);
+                await regularMemberDeposit.paymentMethodMember(MemberObject.CashPaymentMember);
+                await regularMemberDeposit.inputTotalDeposit("100.000");
+                await regularMemberDeposit.inputAdditionalInformation("TOPUP100K");
+                await regularMemberDeposit.saveDeposit();
+            }, {regularMemberDeposit}, testInfo);
+        });
 
 });
