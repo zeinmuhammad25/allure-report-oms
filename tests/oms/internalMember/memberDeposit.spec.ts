24d84961eab5da8e9bce7bdbe27b6497c15b9f48
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-09 10:55:22 +0700

    [ADD] [TC_0205611] Validate Button Clear for Total Deposit with Button Deposit Amount on Regular Member Deposit

--- tests/oms/internalMember/memberDeposit.spec.ts
+++ tests/oms/internalMember/memberDeposit.spec.ts
@@ -48,7 +48,7 @@ test.describe.serial("Internal Member", () => {
             await safeTest(async ({}) => {
                 await regularMemberDeposit.createdMemberDeposit();
                 await regularMemberDeposit.inputAdditionalInformation("TOPUP100K");
-                await regularMemberDeposit.saveAndExpectValidation({member: true, paymentMethod: true, amount: true});
+                await regularMemberDeposit.saveDeposit({member: true, paymentMethod: true, amount: true});
             }, {regularMemberDeposit}, testInfo);
         });
 
@@ -65,5 +65,20 @@ test.describe.serial("Internal Member", () => {
                 await regularMemberDeposit.clearTotalDeposit();
             }, {regularMemberDeposit}, testInfo);
         });
+
+    test("[TC_0205611] Validate Button Clear for Total Deposit with Button Deposit Amount on Regular Member Deposit",
+        {tag: tags + "@positive"}, async ({regularMemberDeposit}, testInfo) => {
+            await safeTest(async ({}) => {
+                await regularMemberDeposit.createdMemberDeposit();
+                await regularMemberDeposit.applyRegularMemberNameList();
+                await regularMemberDeposit.searchMemberList("REZA_CUSTOMER");
+                await regularMemberDeposit.selectRegularMemberNameList("REZA_CUSTOMER");
+                await regularMemberDeposit.paymentMemberCategoryType(MemberObject.CashCatMember);
+                await regularMemberDeposit.paymentMethodMember(MemberObject.CashPaymentMember);
+                await regularMemberDeposit.selectDepositBoard(MemberObject.GridMemberBoard100000, 3);
+                await regularMemberDeposit.clearTotalDeposit();
+            }, {regularMemberDeposit}, testInfo);
+        });
+
 
 });
