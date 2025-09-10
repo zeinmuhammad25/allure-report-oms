1b3fd54a0b30af226f830f42dad2097f8e888acc
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-10 09:07:04 +0700

    [ADD] logic createMemberWithdrawal

--- src/modules/oms/regularMemberWithdrawal/regularMemberWithdrawal.page.ts
+++ src/modules/oms/regularMemberWithdrawal/regularMemberWithdrawal.page.ts
@@ -1,11 +1,18 @@
 import BaseOmsPage from "../base-oms-page";
 import RegularMemberWithdrawalScenario from "./regularMemberWithdrawal.scenario";
 import Element from "../../../base/objects/Element";
+import RegularMemberWithdrawalLocator from "./regularMemberWithdrawal.locator";
 
-export default class RegularMemberWithdrawalPage extends BaseOmsPage implements RegularMemberWithdrawalScenario{
+export default class RegularMemberWithdrawalPage extends BaseOmsPage implements RegularMemberWithdrawalScenario {
     pageUrl: () => string;
 
     shouldHave(): Element[] {
         return [];
     }
+
+    async createMemberWithdrawal(): Promise<void> {
+        await this.expectVisible(RegularMemberWithdrawalLocator.btnAddWithdrawalMember);
+        await this.click(RegularMemberWithdrawalLocator.btnAddWithdrawalMember);
+    }
+
 }
