9cd6ece83f2c3a0652e97caf9b72d5329ec98758
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-10 09:17:15 +0700

    [ADD] scenario searchMemberWithdrawal

--- src/modules/oms/regularMemberWithdrawal/regularMemberWithdrawal.scenario.ts
+++ src/modules/oms/regularMemberWithdrawal/regularMemberWithdrawal.scenario.ts
@@ -2,4 +2,6 @@ import BaseScenario from "../../../base/base-scenario";
 
 export default interface RegularMemberWithdrawalScenario extends BaseScenario {
     createMemberWithdrawal(): Promise<void>;
+
+    searchMemberWithdrawal(valueMember: string): Promise<void>;
 }
