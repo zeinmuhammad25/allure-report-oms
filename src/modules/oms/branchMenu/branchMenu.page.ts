53ac0af1b7ac61cf986221a12b8497f1a02ddbc4
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-12 10:20:55 +0700

    [ADD] filterCategoryBranchMenu and searchMenuInCategory logic

--- src/modules/oms/branchMenu/branchMenu.page.ts
+++ src/modules/oms/branchMenu/branchMenu.page.ts
@@ -1,11 +1,26 @@
 import BaseOmsPage from "../base-oms-page";
 import BranchMenuScenario from "./branchMenu.scenario";
 import Element from "../../../base/objects/Element";
+import BranchMenuLocator from "./branchMenu.locator";
 
-export default class BranchMenuPage extends BaseOmsPage implements BranchMenuScenario{
+
+export default class BranchMenuPage extends BaseOmsPage implements BranchMenuScenario {
     pageUrl: () => string;
 
     shouldHave(): Element[] {
         return [];
     }
+
+    async filterCategoryBranchMenu(filterName: string): Promise<void> {
+        await this.expectVisible(BranchMenuLocator.btnFilterCategory(filterName));
+        await this.click(BranchMenuLocator.btnFilterCategory(filterName));
+    }
+
+    async searchMenuInCategory(value: string): Promise<void> {
+        await this.expectVisible(BranchMenuLocator.fieldSearchBranchMenu);
+        await this.click(BranchMenuLocator.fieldSearchBranchMenu);
+        await this.fill(BranchMenuLocator.fieldSearchBranchMenu, value);
+        await this.click(BranchMenuLocator.escapeKeyboardBranchMenu);
+    }
+
 }
