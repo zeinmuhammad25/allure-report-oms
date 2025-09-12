ba0ea1ec056b7ea418ea37ac2f76b6a6d058e3e4
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-12 10:21:08 +0700

    [ADD] filterCategoryBranchMenu and searchMenuInCategory scenario

--- src/modules/oms/branchMenu/branchMenu.scenario.ts
+++ src/modules/oms/branchMenu/branchMenu.scenario.ts
@@ -2,4 +2,7 @@ import BaseScenario from "../../../base/base-scenario";
 
 export default interface BranchMenuScenario extends BaseScenario {
 
+    filterCategoryBranchMenu(filterName: string): Promise<void>;
+
+    searchMenuInCategory(value: string): Promise<void>;
 }
