b409fefe39aa1f82eb306fa10015f33ae7eeffde
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-15 17:20:37 +0700

    [ADD][TC_0205634] Validate tampilan halaman branch menu yang memiliki penambahan button filter

---
+++ tests/oms/branchMenu/branchMenu.spec.ts
@@ -0,0 +1,45 @@
+import {test} from "../injection";
+import {safeTest} from "../../../src/base/utils/safeTest";
+
+test.setTimeout(100000);
+test.describe.serial("Branch Menu", () => {
+    const tags = "@smokeTest @oms @BranchMenu";
+
+    test.beforeEach(async ({terminalID, signPin, sideNavBar, tableList}) => {
+        const testWithAuthentication = [
+            "[TC_0205634] Validate tampilan halaman branch menu yang memiliki penambahan button filter"
+        ];
+
+        if (testWithAuthentication.includes(test.info().title)) {
+            await terminalID.goHere();
+            await terminalID.performTerminalID();
+            await signPin.inputPinByTouch("22");
+            await signPin.validateShowStarCash("20.000");
+            await signPin.storeAuthState();
+        }
+        await tableList.goHere();
+        await signPin.closePopUpAlert();
+        await sideNavBar.gotoPageBranchMenu();
+    });
+
+    test.afterEach(async ({}) => {
+
+    });
+
+    test("[TC_0205634] Validate tampilan halaman branch menu yang memiliki penambahan button filter",
+        {tag: tags + "@positive"}, async ({branchMenu}, testInfo) => {
+            await safeTest(async ({}) => {
+                await branchMenu.filterCategoryBranchMenu("All Menu");
+                await branchMenu.selectMenuCategory("Anggur");
+                await branchMenu.validationMenu("[21+] Anggur Ketan Hitam OT 620ml", "name");
+                await branchMenu.filterCategoryBranchMenu("Sold Out Menu");
+                await branchMenu.selectMenuCategory("A1 Ready To Sell");
+                await branchMenu.validationMenu("New York Cheese Cake Dus", "short");
+                await branchMenu.filterCategoryBranchMenu("Limit Quantity Menu");
+                await branchMenu.selectMenuCategory("Makanan Apri");
+                await branchMenu.validationMenu("For Testing", "sub");
+            }, {branchMenu}, testInfo);
+        });
+
+
+});
