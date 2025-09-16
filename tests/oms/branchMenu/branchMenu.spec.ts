075895aff911c3ebe0c3affcee34a88a9643e9b8
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-16 14:38:45 +0700

    [ADD][TC_0205655] Validate perubahan status menu dari Limited Qty menjadi tidak Limited Qty tanpa melakukan 'Save' ketika filtering sedang berjalan

--- tests/oms/branchMenu/branchMenu.spec.ts
+++ tests/oms/branchMenu/branchMenu.spec.ts
@@ -326,5 +326,18 @@ test.describe.serial("Branch Menu", () => {
             }, {branchMenu, sideNavBar}, testInfo);
         });
 
+    test("[TC_0205655] Validate perubahan status menu dari Limited Qty menjadi tidak Limited Qty tanpa melakukan 'Save' ketika filtering sedang berjalan",
+        {tag: tags + "@positive"}, async ({branchMenu, sideNavBar}, testInfo) => {
+            await safeTest(async ({}) => {
+                await branchMenu.selectMenuCategory("Makanan Apri");
+                await branchMenu.butonShowQty("2",1,);
+                await branchMenu.inputQty("0");
+                await sideNavBar.gotoPageTools();
+                await sideNavBar.gotoPageBranchMenu();
+                await branchMenu.selectMenuCategory("Makanan Apri");
+                await branchMenu.validationMenu("Bebek Madu Pedas Bakar", "short");
+            }, {branchMenu, sideNavBar}, testInfo);
+        });
+
 
 });
