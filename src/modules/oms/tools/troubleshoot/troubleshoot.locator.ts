3e1335a886727738e7b7d0ab74cbd8316ad5e9c3
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-17 15:00:28 +0700

    [MOD] TROUBLESHOOT lOCATOR

--- src/modules/oms/tools/troubleshoot/troubleshoot.locator.ts
+++ src/modules/oms/tools/troubleshoot/troubleshoot.locator.ts
@@ -1,5 +1,19 @@
 import BaseLocator from "../../../../base/base-locator";
 
 export default class TroubleshootLocator extends BaseLocator {
-    static testPrintButton: string = "//mat-tab-body//button/span[normalize-space()='Test Print']";
+    static testPrintButton: string = "//mat-tab-body//button//span[normalize-space()='Test Print']";
+    static selectAllStation: string = "//label[@for='mat-checkbox-36-input']//span[@class='mat-checkbox-label']";
+    static selectStation = (stationName: string): string =>
+        `//span//div[contains(text(),'${stationName}')]`;
+    static openDrawer: string = "//mat-tab-body//button//span[normalize-space()='Open Drawer']";
+    static testPrintMenuAndBill: string = "//mat-tab-body//button[normalize-space()='Test Print Menu/Bill']";
+    static dropDownFormTestPrint = (index: number): string =>
+        `(//div[@class='mat-select-arrow'])[${index}]`;
+    static selectValueDropDown = (value: string): string =>
+        `//span[@class='mat-option-text'][normalize-space()='${value}']`;
+    static selectStationInForm = (stationName: string): string =>
+        `//app-printer-test-menu-bill//div[contains(text(),'${stationName}')]`;
+    static btnFormTestPrintBill = (stationName: "Test Print Bill"|"Test Print Menu"|"Cancel"): string =>
+        `//app-printer-test-menu-bill//div[contains(text(),'${stationName}')]`;
+
 }
