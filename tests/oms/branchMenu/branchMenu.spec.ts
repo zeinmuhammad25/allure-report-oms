2601d187835bce2d7dafa2ddfe81083ab97a5513
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-17 11:01:08 +0700

    [ADD][TC_0205679] Verify that User can search by Menu Short Name with valid input

--- tests/oms/branchMenu/branchMenu.spec.ts
+++ tests/oms/branchMenu/branchMenu.spec.ts
@@ -606,5 +606,15 @@ test.describe.serial("Branch Menu", () => {
             }, {branchMenu}, testInfo);
         });
 
+    test("[TC_0205679] Verify that User can search by Menu Short Name with valid input",
+        {tag: tags + "@positive"}, async ({branchMenu}, testInfo) => {
+            await safeTest(async ({}) => {
+                await branchMenu.selectMenuCategory("AT MENU CATEGORY");
+                await branchMenu.searchMenuInCategory("AT MENU PAKET MAHAL");
+                await branchMenu.validationMenu("AT MENU PAKET MAHAL","short");
+            }, {branchMenu}, testInfo);
+        });
+
+
 
 });
