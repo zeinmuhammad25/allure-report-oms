ec3ae2c92111fbca78ffd359e51a4dbcc1e293c6
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-19 14:26:11 +0700

    [ADD] closePopUp LOCATOR

--- src/modules/oms/tools/troubleshoot/troubleshoot.locator.ts
+++ src/modules/oms/tools/troubleshoot/troubleshoot.locator.ts
@@ -2,6 +2,7 @@ import BaseLocator from "../../../../base/base-locator";
 
 export default class TroubleshootLocator extends BaseLocator {
     static testPrintButton: string = "//mat-tab-body//button//span[normalize-space()='Test Print']";
+    static closePopUp: string = "//span[normalize-space()='Ok']";
     static selectAllStation: string = "//label[@for='mat-checkbox-36-input']//span[@class='mat-checkbox-label']";
     static selectStation = (stationName: string): string =>
         `//span//div[contains(text(),'${stationName}')]`;
