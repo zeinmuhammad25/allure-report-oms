c1a27af56ef5d1641ce3d369d2dc416e26994cc2
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-19 17:32:42 +0700

    [ADD] [TC_0205695] Validate Logic when User can Select QR Code Scanner Mode in Application Setting sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -98,4 +98,13 @@ test.describe.serial("Tools", () => {
             }, {tools, applicationSetting}, testInfo);
         });
 
+    test("[TC_0205695] Validate Logic when User can Select QR Code Scanner Mode in Application Setting sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, applicationSetting}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.ApplicationSetting);
+                await applicationSetting.userSetQrCodeScanner("Device Camera");
+                await applicationSetting.saveSetting();
+            }, {tools, applicationSetting}, testInfo);
+        });
+
 });
