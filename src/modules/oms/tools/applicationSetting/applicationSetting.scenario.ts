a8e37cce2c690ea11ddcc769c6b56c228f6803e0
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-19 16:55:43 +0700

    [MOD] SCENARIO APPLICATION SETTING

--- src/modules/oms/tools/applicationSetting/applicationSetting.scenario.ts
+++ src/modules/oms/tools/applicationSetting/applicationSetting.scenario.ts
@@ -1,14 +1,47 @@
 import BaseScenario from "../../../../base/base-scenario";
 
 export default interface ApplicationSettingScenario extends BaseScenario {
-    userSetAllSetting(): Promise<void>;
 
-    userSetStation(): Promise<void>;
+    userSetStation(station: string): Promise<void>;
 
-    userSetWaringTime(): Promise<void>;
+    userSetWaringTime(first: string, second: string): Promise<void>;
 
-    userSetSalesMode(): Promise<void>;
+    userSetSalesMode(dineIn: string, quickService): Promise<void>;
 
-    userSetSelfOrderServer(): Promise<void>;
+    userSetSelfOrderServer(station: string): Promise<void>;
+
+    saveSetting(): Promise<void>;
+
+    userFirstWaringTime(first: string): Promise<void>;
+
+    userSecondWaringTime(second: string): Promise<void>;
+
+    userDineInSalesMode(dineIn: string): Promise<void>;
+
+    userQuickServiceSalesMode(dineIn: string, quickService: string): Promise<void>;
+
+    userSalesModeforQuickService(quickService: string): Promise<void>;
+
+    userSetDirectServing(): Promise<void>;
+
+    userHideNotesSetDirectServing(): Promise<void>;
+
+    setCustomerDisplay(option: string): Promise<void>;
+
+    proceedMonitorDisplay(): Promise<void>;
+
+    cancelMonitorDisplay(): Promise<void>;
+
+    inputLengthPoleDisplay(length: string): Promise<void>;
+
+    inputPortPoleDisplay(length: string): Promise<void>;
+
+    userSetSelfOrderServerStation(station: string): Promise<void>;
+
+    UserShowOnScreenKeyboard(): Promise<void>;
+
+    userSetIntegratedScale(status: string): Promise<void>;
+
+    UserShowInfoOnTable(): Promise<void>;
 
 }
