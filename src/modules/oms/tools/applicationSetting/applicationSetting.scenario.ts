7bc8f0be5dd1448354b5746485f03fb9c8c17fb3
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-22 15:19:57 +0700

    [MOD] SCENARIO FOR APPLICATION SETTING

--- src/modules/oms/tools/applicationSetting/applicationSetting.scenario.ts
+++ src/modules/oms/tools/applicationSetting/applicationSetting.scenario.ts
@@ -8,17 +8,29 @@ export default interface ApplicationSettingScenario extends BaseScenario {
 
     userSetSalesMode(dineIn: string, quickService): Promise<void>;
 
-    userSetSelfOrderServer(station: string): Promise<void>;
+    userSetSelfOrderServer(): Promise<void>;
+
+    dropDownSelfOrderServer(): Promise<void>;
 
     saveSetting(): Promise<void>;
 
-    userFirstWaringTime(first: string): Promise<void>;
+    userFirstWaringTime(
+        o?: { append?: boolean; delCount?: number; delFrom?: "start" | "end"; delSub?: number },
+        first?: number | string
+    ): Promise<void>;
 
-    userSecondWaringTime(second: string): Promise<void>;
+    userSecondWaringTime(
+        o?: { append?: boolean; delCount?: number; delFrom?: "start" | "end"; delSub?: number },
+        second?: number | string
+    ): Promise<void>;
 
-    userDineInSalesMode(dineIn: string): Promise<void>;
+    showDropDownDineISalesMode(): Promise<void>;
 
-    userQuickServiceSalesMode(dineIn: string, quickService: string): Promise<void>;
+    userDineInSalesMode(dineIn: string | string[]): Promise<void>;
+
+    showDropDownQuickServiceSalesMode(): Promise<void>;
+
+    userQuickServiceSalesMode(quickService: string | string[]): Promise<void>;
 
     userSalesModeforQuickService(quickService: string): Promise<void>;
 
@@ -32,16 +44,30 @@ export default interface ApplicationSettingScenario extends BaseScenario {
 
     cancelMonitorDisplay(): Promise<void>;
 
-    inputLengthPoleDisplay(length: string): Promise<void>;
+    inputLengthPoleDisplay(
+        o?: { append?: boolean; delCount?: number; delFrom?: "start" | "end"; delSub?: number },
+        length?: number | string
+    ): Promise<void>;
 
-    inputPortPoleDisplay(length: string): Promise<void>;
+    inputPortPoleDisplay(
+        o?: { append?: boolean; delCount?: number; delFrom?: "start" | "end"; delSub?: number },
+        port?: number | string
+    ): Promise<void>;
 
     userSetSelfOrderServerStation(station: string): Promise<void>;
+
+    userOffSelfOrderServerStation(): Promise<void>;
+
+    cancelUserOffSelfOrderServerStation(): Promise<void>;
 
     UserShowOnScreenKeyboard(): Promise<void>;
 
     userSetIntegratedScale(status: string): Promise<void>;
 
     UserShowInfoOnTable(): Promise<void>;
+
+    userSetQrCodeScanner(mode: string): Promise<void>;
+
+    closePopUpApplicationSetting(): Promise<void>;
 
 }
