d718fc8cd754b914f06f48fb44a6b0448a258d76
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-22 16:40:36 +0700

    [MOD]LOCATOR SYNC

--- src/modules/oms/tools/synchronizeData/synchronizeData.locator.ts
+++ src/modules/oms/tools/synchronizeData/synchronizeData.locator.ts
@@ -6,9 +6,10 @@ export default class SynchronizeDataLocator extends BaseLocator {
     static getLocatorSynchronize = (actionSync: SynchronizeDataObject): string => ` //div[normalize-space()='${actionSync}']`;
     static buttonUpdateSetting: string = "//span[normalize-space()='Update Setting']";
     static buttonSynchronize: string = "//span[normalize-space()='Synchronize']";
-    static buttonSelectAll: string = "//label[@for='mat-checkbox-6-input']//span[@class='mat-checkbox-label']";
-    static buttonCloseSyncPopup: string = "//span[normalize-space()='Ok']";
+    static buttonSelectAll: string = "(//span[@class='mat-checkbox-label'])[2]";
+    static buttonCloseSyncPopup: string = "//span[normalize-space()='Close']";
     static buttonIUnderstandCancelAuto: string = "//span[normalize-space()='I Understand']";
     static buttonCancelAutoCancel: string = "//span[normalize-space()='Cancel']";
+    static buttonCloseAfterUpdate: string = "//span[normalize-space()='Ok']";
 
 }
