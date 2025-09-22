2df3a330cb079d5791aec8c4515a66c7b2f29e4c
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-22 15:17:09 +0700

    [MOD] LOCATOR FOR APPLICATION SETING

--- src/modules/oms/tools/applicationSetting/applicationSetting.locator.ts
+++ src/modules/oms/tools/applicationSetting/applicationSetting.locator.ts
@@ -1,32 +1,40 @@
 import BaseLocator from "../../../../base/base-locator";
 
 export default class ApplicationSettingLocator extends BaseLocator {
-
+    static closePopUp: string = "//span[normalize-space()='Ok']";
     static escapeKeyBoard: string = "//h5[normalize-space()='Tools']";
     static closeAfterSet: string = "//div[@class='cdk-overlay-backdrop cdk-overlay-transparent-backdrop cdk-overlay-backdrop-showing']";
     static buttonApplicationSetting: string = "//div[contains(text(),'Application Setting')]";
     static defaultStationDropdown: string = `#mat-select-0`;
     static selectDefaultStation = (SelectStation: string): string => `//span[@class='mat-option-text'][normalize-space()='${SelectStation}']`;
+    static qrCodeDropdown: string = `(//div[@class='mat-select-arrow-wrapper'])[2]`;
     static selectQrCodeScannerMode = (SelectQrOption: string): string => `//span[@class='mat-option-text'][normalize-space()='${SelectQrOption}']`;
     static inputTimeFirstWarning: string = "//input[@placeholder='e.g. 60']";
     static inputTimeSecondWaring: string = "//input[@placeholder='e.g. 90']";
     static showDineInMode: string = "(//div[@class='mat-select-arrow-wrapper'])[3]";
-    static selectSalesModeForDineIn = (SelectDineInSalesMode: string): string => `//span[normalize-space()='${SelectDineInSalesMode}']`;
+    static selectSalesModeForDineIn = (SelectDineInSalesMode: string): string =>
+        `//div[contains(@class,'mat-select-panel')]//mat-option[.//span[normalize-space()='${SelectDineInSalesMode}']]`;
     static showQuickServiceMode: string = "(//div[@class='mat-select-arrow-wrapper'])[4]";
-    static selectSalesModeForQuickService = (SelectQuickServiceSalesMode: string): string => `//span[normalize-space()='${SelectQuickServiceSalesMode}']`;
+    static selectSalesModeForQuickService = (SelectQuickServiceSalesMode: string): string =>
+        `//div[contains(@class,'mat-select-panel')]//mat-option[.//span[normalize-space()='${SelectQuickServiceSalesMode}']]`;
     static checkboxDirectServing: string = "(//div[@class='mat-checkbox-inner-container mat-checkbox-inner-container-no-side-margin'])[1]";
     static showDefaultQsMode: string = "(//div[@class='mat-select-arrow-wrapper'])[5]";
-    static selectSalesModeForDefaultQS = (SelectDineInSalesMode: string): string => `//span[normalize-space()='${SelectDineInSalesMode}']`;
+    static selectSalesModeForDefaultQS = (SelectDineInSalesMode: string): string =>
+        `//div[contains(@class,'mat-select-panel')]//mat-option[.//span[normalize-space()='${SelectDineInSalesMode}']]`;
     static toggleHidePendingNotes: string = "//span[@class='mat-slide-toggle-content']";
     static checkboxShowCustomerDisplay: string = "(//div[@class='mat-select-arrow-wrapper'])[6]";
     static selectedOffShowCustomerDisplay: string = "(//span[@class='mat-option-text'][normalize-space()='OFF'])[2]";
     static selectedShowCustomerDisplay = (display: string): string => `//span[normalize-space()='${display}']`;
-    static CustomerDisplayLength: string = `//input[@class='form-control text-right ng-pristine ng-valid ng-touched']`;
-    static CustomerDisplayport: string = `//input[@name='poleDisplayPort']`;
+    static CustomerDisplayLength: string = `//div[label[normalize-space()='Pole Display Length']]//app-number-input//input[@matkeyboard='numpad']`;
+    static CustomerDisplayport: string = `//div[label[normalize-space()='Pole Display Port']]//input[@name='poleDisplayPort']`;
     static displayMonitorProceed: string = `//span[normalize-space()='Proceed']`;
     static displayMonitorCancel: string = `//span[normalize-space()='Close']`;
-    static checkboxSelfOrderServer: string = "(//div[@class='mat-checkbox-inner-container mat-checkbox-inner-container-no-side-margin'])[2]";
-    static selectESBOrderPrinterStation = (SelectEsoStation: string): string => `//span[normalize-space()='${SelectEsoStation}']`;
+    static checkboxSelfOrderServer: string = "(//label//div[contains(@class,'mat-checkbox-inner-container')])[2]";
+    static dropDownSelfOrderServer: string = "(//div[@class='mat-select-arrow-wrapper'])[7]";
+    static selectESBOrderPrinterStation = (SelectEsoStation: string): string =>
+        `//div[contains(@class,'mat-select-panel')]//mat-option[.//span[normalize-space()='${SelectEsoStation}']]`;
+    static turnOffServer: string = "//button[.//span[normalize-space()='Turn Off Server']]";
+    static cancelTurnOffServer: string = "//button[.//span[normalize-space()='Cancel']]";
     static checkboxShowOnScreenKeyboard: string = "(//div[@class='mat-checkbox-inner-container mat-checkbox-inner-container-no-side-margin'])[3]";
     static ShowOnIntegratedScale: string = "(//div[@class='mat-select-arrow-wrapper'])[8]";
     static selectIntegratedScale = (SelectIntegratedScale: string): string => `//span[@class='mat-option-text'][normalize-space()='${SelectIntegratedScale}']`;
