803afb554034e830894447a9ba5771962cef0ecf
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-22 15:48:43 +0700

    [MOD]LOCATOR SYNC

--- src/modules/oms/tools/synchronizeData/synchronizeData.locator.ts
+++ src/modules/oms/tools/synchronizeData/synchronizeData.locator.ts
@@ -8,5 +8,8 @@ export default class SynchronizeDataLocator extends BaseLocator {
     static buttonSynchronize: string = "//span[normalize-space()='Synchronize']";
     static buttonSelectAll: string = "//label[@for='mat-checkbox-6-input']//span[@class='mat-checkbox-label']";
     static buttonCloseSyncPopup: string = "//span[normalize-space()='Close']";
+    static buttonIUnderstandCancelAuto: string = "//span[normalize-space()='I Understand']";
+    static buttonCancelAutoCancel: string = "//span[normalize-space()='Cancel']";
+    static closePopUp: string = "//span[normalize-space()='Ok']";
 
 }
