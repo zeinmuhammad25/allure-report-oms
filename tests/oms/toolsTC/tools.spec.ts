82d85cfe9c121306dba9cedb10f0fda46d354da1
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-22 16:44:55 +0700

    [ADD][TC_0205726] Validate Logic when User can Select User only in Synchronize Data sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -404,5 +404,13 @@ test.describe.serial("Tools", () => {
             }, {tools, synchronizeData}, testInfo);
         });
 
+    test("[TC_0205726] Validate Logic when User can Select User only in Synchronize Data sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, synchronizeData}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.SynchronizeData);
+                await synchronizeData.synchronizeDataUser();
+                await synchronizeData.closePopUpAfterSync();
+            }, {tools, synchronizeData}, testInfo);
+        });
 
 });
