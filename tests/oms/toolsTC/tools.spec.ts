e1145e450045ffc15dab94a0c2dac7f5822a38ec
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-23 11:12:53 +0700

    [ADD][TC_0205747] Validate Logic when User can select Expired Status All Promotions in Promotion List sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -618,6 +618,17 @@ test.describe.serial("Tools", () => {
             }, {tools, promotionListTools}, testInfo);
         });
 
+    test("[TC_0205747] Validate Logic when User can select Expired Status All Promotions in Promotion List sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, promotionListTools}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.PromotionList);
+                await promotionListTools.selectPromoCategoryFilter("ALL PROMOTIONS");
+                await promotionListTools.showDropDown();
+                await promotionListTools.setStatus("Expired");
+                await promotionListTools.dataValidation("SEPTEMBER SPECIAL DISCOUNT 50%");
+            }, {tools, promotionListTools}, testInfo);
+        });
+
 
 
 });
