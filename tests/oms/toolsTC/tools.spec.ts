11765aa19982d23b8f0d3e971a26ad8e451e233f
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-23 15:56:14 +0700

    [ADD][TC_0205781] Validate Logic when User can sort Descending Event Subject in Branch Event List sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -1040,4 +1040,18 @@ test.describe.serial("Tools", () => {
             }, {tools, branchEventList}, testInfo);
         });
 
+    test("[TC_0205781] Validate Logic when User can sort Ascending Event Subject in Branch Event List sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, branchEventList}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.BranchEventList);
+                await branchEventList.clickButtonFilterDate();
+                await branchEventList.selectMonthAndYear("Choose month and year");
+                await branchEventList.datePickerYear("2025");
+                await branchEventList.datePickerMonth("SEP");
+                await branchEventList.datePickerDate("1");
+                await branchEventList.shortingDescBranchEventList("Event Subject");
+                await branchEventList.dataValidation("Push Data");
+            }, {tools, branchEventList}, testInfo);
+        });
+
 });
