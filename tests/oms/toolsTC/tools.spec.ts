26f38874c9b925ddbb11109a6eb83e69b9e2ae35
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-23 16:37:28 +0700

    [ADD][TC_0205787] Validate Logic when User can navigate to the Previous Page in Branch Event List sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -1128,4 +1128,21 @@ test.describe.serial("Tools", () => {
             }, {tools, branchEventList}, testInfo);
         });
 
+    test("[TC_0205787] Validate Logic when User can navigate to the Previous Page in Branch Event List sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, branchEventList}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.BranchEventList);
+                await branchEventList.clickButtonFilterDate();
+                await branchEventList.selectMonthAndYear("Choose month and year");
+                await branchEventList.datePickerYear("2025");
+                await branchEventList.datePickerMonth("SEP");
+                await branchEventList.datePickerDate("1");
+                await branchEventList.depositPagination("next");
+                await branchEventList.depositPagination("next");
+                await branchEventList.depositPagination("previous");
+                await branchEventList.depositPagination("previous");
+                await branchEventList.dataValidation("DEV1RIZAL");
+            }, {tools, branchEventList}, testInfo);
+        });
+
 });
