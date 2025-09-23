cc2c279d530ed97eac4dfa46e80016c7fdfcb037
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-23 15:19:35 +0700

    [ADD] [TC_0205773] Validate Logic when User can input Valid Ref Number in Branch Event List sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -918,5 +918,21 @@ test.describe.serial("Tools", () => {
             }, {tools}, testInfo);
         });
 
+    test("[TC_0205773] Validate Logic when User can input Valid Ref Number in Branch Event List sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, branchEventList}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.BranchEventList);
+                await branchEventList.clickButtonFilterDate();
+                await branchEventList.selectMonthAndYear("Choose month and year");
+                await branchEventList.datePickerYear("2025");
+                await branchEventList.datePickerMonth("SEP");
+                await branchEventList.datePickerDate("1");
+                await branchEventList.searchRefNumber("Saacs175645036369");
+                await branchEventList.dataValidation("Saacs175645036369");
+                await branchEventList.clickDetailBranchEvent("Saacs175645036369", 1);
+                await branchEventList.closeDetailBranchEvent();
+            }, {tools}, testInfo);
+        });
+
 
 });
