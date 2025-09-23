2ff20271952a09114acc9816f58e700ae793b8c9
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-23 11:53:24 +0700

    [ADD][TC_0205762] Validate Logic when User can Sort Ascending Status All Promotions list in Promotion List sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -779,4 +779,15 @@ test.describe.serial("Tools", () => {
             }, {tools, promotionListTools}, testInfo);
         });
 
+    test("[TC_0205762] Validate Logic when User can Sort Ascending Status All Promotions list in Promotion List sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, promotionListTools}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.PromotionList);
+                await promotionListTools.selectPromoCategoryFilter("ALL PROMOTIONS");
+                await promotionListTools.shortingAscPromotionList("Status");
+                await promotionListTools.dataValidation("FREE BUKAN SF");
+            }, {tools, promotionListTools}, testInfo);
+        });
+
+
 });
