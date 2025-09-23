97bf99ee78cb41da74fb6605f65eba2618e11db4
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-23 11:28:04 +0700

    [ADD][TC_0205750] Validate Logic when User can select > 1 Status All Promotions in Promotion List sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -651,5 +651,15 @@ test.describe.serial("Tools", () => {
             }, {tools, promotionListTools}, testInfo);
         });
 
+    test("[TC_0205750] Validate Logic when User can select > 1 Status All Promotions in Promotion List sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, promotionListTools}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.PromotionList);
+                await promotionListTools.selectPromoCategoryFilter("ALL PROMOTIONS");
+                await promotionListTools.showDropDown();
+                await promotionListTools.setStatus(["Upcoming","Ongoing"]);
+                await promotionListTools.dataValidation("SEPTEMBER SPECIAL DISCOUNT 50%");
+            }, {tools, promotionListTools}, testInfo);
+        });
 
 });
