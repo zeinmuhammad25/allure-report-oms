390678003930db0b094522acc11f797da553cc9c
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-23 16:35:11 +0700

    [ADD][TC_0205786] Validate Logic when User can navigate to the Next Page in Branch Event List sub-tab menu on Tools

--- tests/oms/toolsTC/tools.spec.ts
+++ tests/oms/toolsTC/tools.spec.ts
@@ -1113,4 +1113,19 @@ test.describe.serial("Tools", () => {
             }, {tools, branchEventList}, testInfo);
         });
 
+    test("[TC_0205786] Validate Logic when User can navigate to the Next Page in Branch Event List sub-tab menu on Tools",
+        {tag: tags + "@positive"}, async ({tools, branchEventList}, testInfo) => {
+            await safeTest(async ({}) => {
+                await tools.selectTab(ToolsTabs.BranchEventList);
+                await branchEventList.clickButtonFilterDate();
+                await branchEventList.selectMonthAndYear("Choose month and year");
+                await branchEventList.datePickerYear("2025");
+                await branchEventList.datePickerMonth("SEP");
+                await branchEventList.datePickerDate("1");
+                await branchEventList.depositPagination("next");
+                await branchEventList.depositPagination("next");
+                await branchEventList.dataValidation("Push Data");
+            }, {tools, branchEventList}, testInfo);
+        });
+
 });
