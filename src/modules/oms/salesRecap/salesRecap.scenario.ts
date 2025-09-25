b2b65cd24bcd8e3a710681ab2bc90cc877a27d2c
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-25 09:13:09 +0700

    [ADD] searchTransactionBillNumberSalesOverView scenario

--- src/modules/oms/salesRecap/salesRecap.scenario.ts
+++ src/modules/oms/salesRecap/salesRecap.scenario.ts
@@ -2,4 +2,6 @@ import BaseScenario from "../../../base/base-scenario";
 
 export default interface SalesRecapScenario extends BaseScenario {
     salesRecapTab(tabSection: string): Promise<void>;
+
+    searchTransactionBillNumberSalesOverView(value: string): Promise<void>;
 }
