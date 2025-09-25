6d44d4bed182852d1caae57195752f0790b4369d
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-25 08:38:35 +0700

    [ADD] salesRecapTab Logic

--- src/modules/oms/salesRecap/salesRecap.page.ts
+++ src/modules/oms/salesRecap/salesRecap.page.ts
@@ -1,6 +1,7 @@
 import BaseOmsPage from "../base-oms-page";
 import Element from "../../../base/objects/Element";
 import SalesRecapScenario from "./salesRecap.scenario";
+import SalesRecapLocator from "./salesRecap.locator";
 
 export default class SalesRecapPage extends BaseOmsPage implements SalesRecapScenario {
     pageUrl: () => string;
@@ -8,4 +9,10 @@ export default class SalesRecapPage extends BaseOmsPage implements SalesRecapSce
     shouldHave(): Element[] {
         return [];
     }
+
+    async salesRecapTab(tabSection: string): Promise<void> {
+        await this.expectVisible(SalesRecapLocator.salesRecapTabSection(tabSection));
+        await this.click(SalesRecapLocator.salesRecapTabSection(tabSection));
+    }
+
 }
