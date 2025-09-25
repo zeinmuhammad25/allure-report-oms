965c2d039eccf7bc76d96606a188d8e8e55db72f
reza fadhilah <reza.fadhilah@esb.co.id>
2025-09-25 09:23:07 +0700

    [ADD] selectMonthAndYear scenario

--- src/modules/oms/salesRecap/salesRecap.scenario.ts
+++ src/modules/oms/salesRecap/salesRecap.scenario.ts
@@ -3,9 +3,12 @@ import BaseScenario from "../../../base/base-scenario";
 export default interface SalesRecapScenario extends BaseScenario {
     salesRecapTab(tabSection: string): Promise<void>;
 
+    clickFilterDate(): Promise<void>;
+
+    selectMonthAndYear(side: "left" | "right", nav: "prev" | "next"): Promise<void>;
+
     searchTransactionBillNumberSalesOverView(value: string): Promise<void>;
 
     searchMemberCustomerSalesOverView(value: string): Promise<void>;
 
-    clickFilterDate(): Promise<void>;
 }
