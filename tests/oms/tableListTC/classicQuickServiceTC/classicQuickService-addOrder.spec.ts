dc9387409ea32ba2c8e2f8a16fa9d31a6a57b3ec
nadia <nadiadplestari@esb.co.id>
2025-10-07 23:46:05 +0700

    [ADD][TCAT_OMS_CQSBO_0047] Validate Logic When User Able To Edit Qty Menu Extra Special Price

--- tests/oms/tableListTC/classicQuickServiceTC/classicQuickService-addOrder.spec.ts
+++ tests/oms/tableListTC/classicQuickServiceTC/classicQuickService-addOrder.spec.ts
@@ -990,4 +990,17 @@ test.describe.serial("Quick Service Classic Add Order", () => {
             }, {quickServiceList, bookOrderClassic, orderClassic, addOrderV2, paymentV2}, testInfo);
         });
 
+    test("[TCAT_OMS_CQSBO_0047] Validate Logic When User Able To Edit Qty Menu Extra Special Price",
+        {tag: tag + "@positive"}, async ({quickServiceList, bookOrderClassic, orderClassic, addOrderV2, paymentV2}, testInfo) => {
+            await safeTest(async ({quickServiceList, bookOrderClassic, orderClassic, addOrderV2, paymentV2}) => {
+                await makeOrder("AT EXCLUSIVE", bookOrderClassic, quickServiceList);
+                await selectMenuPaketSpecialPrice(orderClassic, addOrderV2, 2);
+                await selectMenuExtra(addOrderV2,5);
+                await addOrderV2.addToCartMenuDetailPackage();
+                await new Promise(resolve => setTimeout(resolve, 1000));
+                await orderClassic.saveOrder();
+                await paymentCashFull(paymentV2);
+            }, {quickServiceList, bookOrderClassic, orderClassic, addOrderV2, paymentV2}, testInfo);
+        });
+
 });
