5564b03e85609a59b59a40e3a292d8d67b742258
nadia <nadiadplestari@esb.co.id>
2025-10-09 11:23:56 +0700

    [MOD] Remove unused imports

--- tests/oms/tableListTC/classicQuickServiceTC/classicQuickService-addOrder.spec.ts
+++ tests/oms/tableListTC/classicQuickServiceTC/classicQuickService-addOrder.spec.ts
@@ -1,16 +1,12 @@
 import {test} from "../../injection";
 import MenuList from "../../../../src/modules/oms/objects/menuList";
-import OrderScenario from "../../../../src/modules/oms/tableList/order/order.scenario";
 import OrderClassicScenario from "../../../../src/modules/oms/tableList/order/orderClassic.scenario";
 import QuickServiceListScenario from "../../../../src/modules/oms/tableList/quickServiceList/quickServiceList.scenario";
-import BookOrderScenario from "../../../../src/modules/oms/tableList/components/bookOrder/bookOrder.scenario";
 import BookOrderClassicScenario from "../../../../src/modules/oms/tableList/components/bookOrderClassic/bookOrderClassic.scenario";
 import AddOrderV2Scenario from "../../../../src/modules/oms/tableList/order/components/addOrderV2/addOrderV2.scenario";
 import PaymentV2Scenario from "../../../../src/modules/oms/tableList/paymentV2/paymentV2.scenario";
 import PaymentList from "../../../../src/modules/oms/objects/paymentList";
 import {safeTest} from "../../../../src/base/utils/safeTest";
-import {ToolsTabs} from "../../../../src/modules/oms/tools/ToolsTabs";
-import SignPinLocator from "../../../../src/modules/oms/signPin/signPin.locator";
 
 test.setTimeout(600000);
 test.describe.serial("Quick Service Classic Add Order", () => {
