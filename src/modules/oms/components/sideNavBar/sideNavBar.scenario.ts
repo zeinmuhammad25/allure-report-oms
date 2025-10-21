b94ea9829763697fbf31c55cfef8fede95373a8a
nadia <nadiadplestari@esb.co.id>
2025-10-21 17:01:31 +0700

    [ADD] selectSalesMode function in sideNavBar Scenario Object

--- src/modules/oms/components/sideNavBar/sideNavBar.scenario.ts
+++ src/modules/oms/components/sideNavBar/sideNavBar.scenario.ts
@@ -33,4 +33,6 @@ export default interface SideNavBarScenario extends BaseScenario {
     gotoPageStation(): Promise<void>;
 
     selectStation(stationName: string): Promise<void>;
+
+    selectSalesMode(salesMode: string): Promise<void>;
 }
