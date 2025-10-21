67191779a770a6efe791ba9febce54cd2025bb9f
nadia <nadiadplestari@esb.co.id>
2025-10-21 16:27:39 +0700

    [ADD][TCAT_OMS_CQSTM_0016] Validate logic POS when user edit Sales Mode within the Order Page after order menu

--- tests/oms/tableListTC/classicQuickServiceTC/classicQuickService-editOrder.spec.ts
+++ tests/oms/tableListTC/classicQuickServiceTC/classicQuickService-editOrder.spec.ts
@@ -70,6 +70,18 @@ test.describe.serial("Quick Service Classic Edit Order", () => {
             }, {bookOrderClassic, orderClassic, quickServiceList}, testInfo);
         });
 
+    test("[TCAT_OMS_CQSTM_0016] Validate logic POS when user edit Sales Mode within the Order Page after order menu",
+        {tag: tag + "@positive"}, async ({bookOrderClassic, orderClassic, quickServiceList}, testInfo) => {
+            await safeTest(async ({bookOrderClassic, orderClassic, quickServiceList}) => {
+                await makeOrder("AT INCLUSIVE", bookOrderClassic, quickServiceList);
+                await selectMenuBiasa(orderClassic, 2);
+                await orderClassic.editSalesMode("AT INCLUSIVE", orderClassic);
+                await bookOrderClassic.selectSalesMode("AT EXCLUSIVE", bookOrderClassic);
+                await orderClassic.applySalesMode(orderClassic);
+                await orderClassic.confirmationClose("Yes");
+            }, {bookOrderClassic, orderClassic, quickServiceList}, testInfo);
+        });
+
 
 
 });
